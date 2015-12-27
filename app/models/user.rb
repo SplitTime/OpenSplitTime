1e1d99ed63f4f81b6be27a498cd5c25abfd7f02b
Randall Oveson <randall.oveson@domo.com>
2015-12-27 00:53:27 +0000

    rails_apps_composer: add roles to a User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    if User.count == 0
+      self.role ||= :admin
+    else
+      self.role ||= :user
+    end
+  end
 
   def self.create_with_omniauth(auth)
     create! do |user|

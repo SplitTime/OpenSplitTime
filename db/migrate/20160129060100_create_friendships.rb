bbefe2fb56eb9869ccb7e2f4d358a41563089eb2
Mark Oveson <mark.oveson@gmail.com>
2016-01-29 01:31:21 -0700

    add explicit foreign keys to friendship table

--- db/migrate/20160129060100_create_friendships.rb
+++ db/migrate/20160129060100_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :participant, index: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :participant, index: true, foreign_key: true
       t.integer :type
 
       t.timestamps null: false

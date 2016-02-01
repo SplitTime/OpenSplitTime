ad86c8dbc3dc8eae2a35ba92a7de35894f45c714
Mark Oveson <mark.oveson@gmail.com>
2016-01-31 17:52:02 -0700

    Change ownership relations to belongs_to

--- db/migrate/20160131195951_create_ownerships.rb
+++ db/migrate/20160131195951_create_ownerships.rb
@@ -1,8 +1,8 @@
 class CreateOwnerships < ActiveRecord::Migration
   def change
     create_table :ownerships do |t|
-      t.integer :user_id, index: true, foreign_key: true
-      t.integer :race_id, index: true, foreign_key: true
+      t.belongs_to :user_id, index: true, foreign_key: true
+      t.belongs_to :race_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end

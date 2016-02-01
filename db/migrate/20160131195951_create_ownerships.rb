ab8142c8bba3324e8f0c18685e303449b11fed54
Mark Oveson <mark.oveson@gmail.com>
2016-01-31 17:59:02 -0700

    Fix ownership relations (sorry for multiple commits...)

--- db/migrate/20160131195951_create_ownerships.rb
+++ db/migrate/20160131195951_create_ownerships.rb
@@ -1,8 +1,8 @@
 class CreateOwnerships < ActiveRecord::Migration
   def change
     create_table :ownerships do |t|
-      t.belongs_to :user_id, index: true, foreign_key: true
-      t.belongs_to :race_id, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :race, index: true, foreign_key: true
 
       t.timestamps null: false
     end

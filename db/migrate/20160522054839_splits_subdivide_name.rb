d9da4e41adba09ed9bf8c909bc0259989cf11cb5
Mark Oveson <mark.oveson@gmail.com>
2016-05-24 02:21:06 -0600

    Fix split subdivide migration.

--- db/migrate/20160522054839_splits_subdivide_name.rb
+++ db/migrate/20160522054839_splits_subdivide_name.rb
@@ -2,23 +2,15 @@ class SplitsSubdivideName < ActiveRecord::Migration
   def self.up
     add_column :splits, :base_name, :string
     add_column :splits, :name_extension, :string
-    Split.all.each do |split|
-      base_name = split.name.split.reject { |x| (x.downcase == 'in') | (x.downcase == 'out') }.join(' ')
-      name_extension = split.name.gsub(base_name, '').strip
-      name_extension = name_extension.present? ? name_extension : nil
-      split.update(base_name: base_name, name_extension: name_extension)
-    end
+    execute("UPDATE splits SET base_name = ARRAY_TO_STRING(array_remove(array_remove(STRING_TO_ARRAY(name, ' '), 'In'), 'Out'), ' ')")
+    execute("UPDATE splits SET name_extension = trim(replace(name, base_name, ''))")
+    execute("UPDATE splits SET name_extension = NULLIF(name_extension, '')")
     remove_column :splits, :name
   end
 
   def self.down
     add_column :splits, :name, :string
-    Split.all.each do |split|
-      split.update(name: [split.base_name, split.name_extension]
-                             .map { |x| x.present? ? x : nil }
-                             .compact
-                             .join(' '))
-    end
+    execute("UPDATE splits SET name = (ARRAY_TO_STRING(ARRAY[base_name, name_extension], ' '))")
     remove_column :splits, :base_name
     remove_column :splits, :name_extension
   end

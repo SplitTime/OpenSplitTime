693d4aba77c32215094dd885399e1051655f1145
Mark Oveson <mark.oveson@gmail.com>
2016-06-09 12:35:06 -0600

    Improve documentation for enpoints in live/events_controller.

--- app/controllers/live/events_controller.rb
+++ app/controllers/live/events_controller.rb
@@ -17,6 +17,11 @@ class Live::EventsController < Live::BaseController
   end
 
   def get_event_data
+
+    # This endpoint requires only an event_id, which is passed via the URL as params[:id]
+    # It returns a json containing eventId, eventName, and detailed split info
+    # for all splits associated with the event.
+
     authorize @event
     render partial: 'event_data.json.ruby'
   end
@@ -24,18 +29,18 @@ class Live::EventsController < Live::BaseController
 # This endpoint gets called when the admin enters a "bib" number in the live_entry UI.
 #
   def get_effort
+
     # Here look up the effort and populate the json array with data
-    # needed for front end processing. The assumption is that this endpoint
-    # will take the splitId, bibNumber and eventId used to populate the following fields:
-    # Name, Last Reported, Split From, and Time Spent
+    # needed for front end processing. This endpoint requires an event_id, which is passed
+    # via the URL as params[:id], and a bibNumber. It returns a json containing {success (boolean),
+    # effortId, name (effort.full_name), lastReportedSplitId, lastReportedBitkey, reportText (a string
+    # containing descriptive info about the last reported place and time), dropped (boolean), and
+    # finished (boolean)}
     #
     # Split From and Time Spent fields are not populated until Time In and Time Out fields are entered
-    # Get bib number like this: params[:bibNumber]
-    # Event ID is within the url param
     #
-    # If the lookup fails here (bibNumber or eventId is incorrect), return { success: false }
-    # lastReportedSplitTime comes from the splits table for this "effort"
-    # estimatedTime range
+    # Returns { success: false } if the lookup fails here (bibNumber or eventId is not found).
+
     authorize @event
     render partial: 'effort_data.json.ruby'
   end
@@ -45,16 +50,20 @@ class Live::EventsController < Live::BaseController
     # params must include effortId, splitId, lastReportedSplitId, lastReportedBitkey, and timeIn (military time)
     # This endpoint returns a hash of three elements: {success (boolean),
     # timeFromLastReported (formatted string "hh:mm"), and timeFromStartIn (seconds) }
+    #
+    # Returns {success: false} if the effortId is not found or time_from_last otherwise cannot be calculated
 
     authorize @event
     render partial: 'time_from_last.json.ruby'
-   end
+  end
 
   def get_time_spent
 
     # params must include effortId, splitId, timeFromStartIn (seconds from start), timeOut (military time)
     # This returns a hash of three elements: {success (boolean), time_in_aid (number of minutes),
     # and time_from_start_out (seconds from start)}
+    #
+    # Returns {success: false} if effortId or splitId is not found or time_in_aid otherwise cannot be calculated
 
     authorize @event
     render partial: 'time_spent.json.ruby'
@@ -65,6 +74,8 @@ class Live::EventsController < Live::BaseController
     # params must include effortId, splitId, timeFromStartIn, timeFromStartOut
     # This returns a hash of five elements: {success (boolean), timeInExists (boolean), timeOutExists (boolean),
     # timeInStatus ('bad', 'questionable', or 'good'), and timeOutStatus ('bad', 'questionable', or 'good')}
+    #
+    # Returns {success: false} if effortId or splitId is not found
 
     authorize @event
     render partial: 'verify_times.json.ruby'

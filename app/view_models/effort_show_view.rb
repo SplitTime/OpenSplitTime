bef7b5cb6f5616a9e3380ffa014297efd36fda06
Mark Oveson <mark.oveson@gmail.com>
2016-06-12 00:36:14 -0600

    Optimize effort#show view.

--- app/view_models/effort_show_view.rb
+++ app/view_models/effort_show_view.rb
@@ -1,8 +1,9 @@
 class EffortShowView
-  attr_reader :effort, :split_rows
+  attr_reader :effort, :event, :split_rows
 
   def initialize(effort)
     @effort = effort
+    @event = effort.event
     @splits = effort.event.ordered_splits.to_a
     @split_times = effort.split_times.index_by(&:bitkey_hash)
     @split_rows = []
@@ -18,9 +19,10 @@ class EffortShowView
   attr_reader :splits, :split_times
 
   def create_split_rows
+    start_time = event.start_time + effort.start_offset
     prior_time = 0
     splits.each do |split|
-      split_row = SplitRow.new(split, related_split_times(split), prior_time)
+      split_row = SplitRow.new(split, related_split_times(split), prior_time, start_time)
       split_rows << split_row
       prior_time = split_row.times_from_start.compact.last if split_row.times_from_start.compact.present?
     end

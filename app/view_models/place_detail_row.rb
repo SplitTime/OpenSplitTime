9912adf06b41f0a4eb6f832a11e4f001b96f9229
Mark Oveson <mark.oveson@gmail.com>
2016-06-29 12:04:24 -0600

    Add custom titles to efforts mini table popovers in place_detail view.

--- app/view_models/place_detail_row.rb
+++ app/view_models/place_detail_row.rb
@@ -7,8 +7,10 @@ class PlaceDetailRow
   # split_times should be an array having size == split.sub_split_bitkey_hashes.size,
   # with nil values where no corresponding split_time exists
 
-  def initialize(split, split_times, places, efforts)
+  def initialize(effort, split, previous_split, split_times, places, efforts)
+    @effort = effort
     @split = split
+    @previous_split = previous_split
     @split_times = split_times
     @split_place_in = places[:split_place_in]
     @split_place_out = places[:split_place_out]
@@ -67,12 +69,40 @@ class PlaceDetailRow
     together_in_aid ? together_in_aid.map(&:id) : nil
   end
 
+  def passed_segment_table_title
+    "#{effort_name} passed #{persons(passed_segment_count)} between #{previous_split.base_name} and #{split.base_name}"
+  end
+
+  def passed_in_aid_table_title
+    "#{effort_name} passed #{persons(passed_in_aid_count)} in aid at #{split.base_name}"
+  end
+
+  def passed_by_segment_table_title
+    "#{effort_name} was passed by #{persons(passed_by_segment_count)} between #{previous_split.base_name} and #{split.base_name}"
+  end
+
+  def passed_by_in_aid_table_title
+    "#{effort_name} was passed by #{persons(passed_by_in_aid_count)} while in aid at #{split.base_name}"
+  end
+
+  def together_in_aid_table_title
+    "#{effort_name} was in #{split.base_name} with #{persons(together_in_aid_count)}"
+  end
+
+  def persons(number)
+    number == 1 ? "#{number} person" : "#{number} people"
+  end
+
   def split_id
     split.id
   end
 
   private
 
-  attr_reader :split
+  attr_reader :effort, :split, :previous_split
+
+  def effort_name
+    effort.full_name
+  end
 
 end

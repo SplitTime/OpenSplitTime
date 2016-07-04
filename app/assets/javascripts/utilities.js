be89bf5268a38380824deb245238560ebe78a017
Abram Early <abram.early@gmail.com>
2016-07-04 16:46:07 -0600

    Static popover can be light or dark.

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -1,50 +1,50 @@
 (function ($) {
 
-	var utilities = {
-		isMobileSafari: function () {
-		    return navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)
-		}
-	};
+    var utilities = {
+        isMobileSafari: function () {
+            return navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)
+        }
+    };
 
-	var effortsPopover = {
-		init: function () {
-			$('[data-toggle="efforts-popover"]')
-				.attr('tabindex', '0')
-				.attr('role', 'button')
-				.data('ajax', null)
-				.popover({
-					'html': 'append',
-					'trigger': 'focus'
-				}).on('show.bs.popover', effortsPopover.onShowPopover);
-			if ( utilities.isMobileSafari() ) {
-				$( 'body' ).css( 'cursor', 'pointer' );
-			}
-		},
-		onShowPopover: function (e) {
-			$self = $(this);
-			var ajax = $self.data('ajax');
-			if ( !ajax || typeof ajax.status == 'undefined' ) {
-				var $popover = $self.data('bs.popover');
-				$popover.tip().addClass('efforts-popover');
-				var data = {
-					effortIds: $self.data('effort-ids')
-				};
-				$self.data('ajax', $.post('/efforts/mini_table/', data)
-					.done(function (response) {
-						$popover.options.content = $(response);
-						$popover.show();
-					}).always(function () {
-						$self.data('ajax', null);
-					})
-				);
-				e.preventDefault();
-			}
-		}
-	};
+    var effortsPopover = {
+        init: function () {
+            $('[data-toggle="efforts-popover"]')
+                .attr('tabindex', '0')
+                .attr('role', 'button')
+                .data('ajax', null)
+                .popover({
+                    'html': 'append',
+                    'trigger': 'focus'
+                }).on('show.bs.popover', effortsPopover.onShowPopover);
+            if ( utilities.isMobileSafari() ) {
+                $( 'body' ).css( 'cursor', 'pointer' );
+            }
+        },
+        onShowPopover: function (e) {
+            $self = $(this);
+            var ajax = $self.data('ajax');
+            if ( !ajax || typeof ajax.status == 'undefined' ) {
+                var $popover = $self.data('bs.popover');
+                $popover.tip().addClass('efforts-popover');
+                var data = {
+                    effortIds: $self.data('effort-ids')
+                };
+                $self.data('ajax', $.post('/efforts/mini_table/', data)
+                    .done(function (response) {
+                        $popover.options.content = $(response);
+                        $popover.show();
+                    }).always(function () {
+                        $self.data('ajax', null);
+                    })
+                );
+                e.preventDefault();
+            }
+        }
+    };
 
     var staticPopover = {
         init: function () {
-            $('[data-toggle="static-popover"]')
+            $('[data-toggle="static-popover"],[data-toggle="static-popover-dark"]')
                 .attr('tabindex', '0')
                 .attr('role', 'button')
                 .popover({
@@ -58,12 +58,14 @@
         },
         onShowPopover: function (e) {
             var $popover = $(this).data('bs.popover');
-            $popover.tip().addClass('static-popover');
+            $popover.tip()
+                .addClass('static-popover')
+                .addClass($(this).data('toggle'));
         }
     };
 
-	$(document).ready(function () {
-		effortsPopover.init();
-		staticPopover.init();
-	});
+    $(document).ready(function () {
+        effortsPopover.init();
+        staticPopover.init();
+    });
 })(jQuery);

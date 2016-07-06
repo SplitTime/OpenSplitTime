8b5e72fae856636b821680c6eaef053dc91d8b54
winterpk <winterpk@voltage.com>
2016-07-06 15:20:17 -0600

    Reverted to the custom parallax

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,32 @@
+(function($) {
+	homepage = {
+		$parallaxDivs:null,
+		lastScrollTop:null,
+		isMobile:function() {
+			return ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) );
+		},
+		init:function() {
+			homepage.$parallaxDivs = $('.homepage .first, .homepage .third, .homepage .fifth');
+			if ( ! homepage.isMobile() ) {
+				window.requestAnimationFrame( homepage.parallax );	
+			}
+		},
+		parallax:function() {
+			var scrollTop = $(window).scrollTop();
+
+			if ( !homepage.lastScrollTop || homepage.lastScrollTop !== scrollTop ) {
+				homepage.lastScrollTop = scrollTop;
+
+				homepage.$parallaxDivs.each(function( index ){
+					var $this = $(this);
+					var bgTopPosition = Math.floor( -($this.offset().top - scrollTop) * .3 - 30 );
+					$this.css('background-position', 'center ' + bgTopPosition + 'px');
+				});
+			}
+			window.requestAnimationFrame( homepage.parallax );
+		}
+	}
+	$(document).ready(function() {
+		homepage.init();
+	});
+})(jQuery);

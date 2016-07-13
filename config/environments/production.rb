961cd41d8e70270f62c24a3232e07f5292667026
Mark Oveson <mark.oveson@gmail.com>
2016-07-12 23:44:47 -0600

    Change mailer protocol from https to http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'opensplittime.org', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'opensplittime.org', :protocol => 'http'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false

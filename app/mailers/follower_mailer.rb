a357d406dfe4dda69cd13e93920676d8db40475a
Mark Oveson <mark.oveson@gmail.com>
2016-09-20 11:25:08 -0600

    Change live_effort_email to class method.

--- app/mailers/follower_mailer.rb
+++ app/mailers/follower_mailer.rb
@@ -2,15 +2,12 @@ class FollowerMailer < ApplicationMailer
 
   helper :application
 
-  def live_effort_email(follower, split_times)
+  def self.live_effort_email(follower, split_times)
     @follower = follower
     @split_times = split_times
     @effort = split_times.first.effort
+    puts "Sending email to #{follower.full_name}."
     mail(to: @follower.email, subject: "Update for #{@effort.full_name} at #{@effort.event_name}")
-  end
-
-  def interest_new_event
-
   end
 
 end

7312b4b4b9cc950e404076110f8434ed8fdd57a4
Mark Oveson <mark.oveson@gmail.com>
2016-09-21 23:15:07 -0600

    Change follower mail delivery to background task.

--- app/services/follower_mailer_service.rb
+++ app/services/follower_mailer_service.rb
@@ -3,7 +3,7 @@ class FollowerMailerService
   def self.send_live_effort_mail(participant_id, split_time_ids)
     live_effort_mail_data = LiveEffortMailData.new(participant_id, split_time_ids)
     live_effort_mail_data.followers.each do |follower|
-      FollowerMailer.live_effort_email(follower, live_effort_mail_data.effort_data).deliver_now
+      FollowerMailer.live_effort_email(follower, live_effort_mail_data.effort_data).deliver_later
     end
   end
 

23e5313ea9e3c3b0ec5d7ff6c4f7957ac83c1e9d
Mark Oveson <mark.oveson@gmail.com>
2016-09-22 18:30:36 -0600

    Update FollowerMailerJob for new LiveEffortMailData objects.

--- app/jobs/follower_mailer_job.rb
+++ app/jobs/follower_mailer_job.rb
@@ -1,12 +1,13 @@
+# Not currently working. Sidekiq seems not to handle the nested jobs. Use FollowerMailerService instead.
+
 class FollowerMailerJob < ActiveJob::Base
 
-  queue_as :default
+  queue_as :default # Perhaps this should be 'mailers'
 
   def perform(participant_id, split_time_ids)
-    split_times = SplitTime.find(split_time_ids)
-    participant = Participant.find(participant_id)
-    participant.followers.each do |follower|
-      FollowerMailer.live_effort_email(follower, split_times).deliver_later
+    live_effort_mail_data = LiveEffortMailData.new(participant_id, split_time_ids)
+    live_effort_mail_data.followers.each do |follower|
+      FollowerMailer.live_effort_email(follower, live_effort_mail_data.effort_data).deliver_later
     end
   end
 

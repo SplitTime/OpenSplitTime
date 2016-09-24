08f76785b8874227dd08027def7a3f1b43af4ffd
Mark Oveson <mark.oveson@gmail.com>
2016-09-24 12:45:29 -0600

    fix bug in live effort email

--- app/view_models/live_effort_mail_data.rb
+++ app/view_models/live_effort_mail_data.rb
@@ -13,14 +13,6 @@ class LiveEffortMailData
     @effort_data ||= get_effort_data
   end
 
-  def effort_id
-    effort.id
-  end
-
-  def event_id
-    effort.event.id
-  end
-
   private
 
   attr_reader :effort
@@ -31,7 +23,9 @@ class LiveEffortMailData
     {full_name: full_name,
      event_name: event_name,
      dropped_split_id: dropped_split_id,
-     split_times_data: split_times_data}
+     split_times_data: split_times_data,
+     effort_id: effort.id,
+     event_id: effort.event.id}
   end
 
   def split_times_data

92655f68080f23b2d34ad86172f4226459b6bd14
Mark Oveson <mark.oveson@gmail.com>
2016-11-06 16:27:24 -0700

    Remove OID initializer (no longer necessary because of change in Participant::approximate_ages_today.

--- config/initializers/add_custom_oid_types_to_active_record.rb
+++
@@ -1,9 +0,0 @@
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
-  def initialize_type_map_with_postgres_oids mapping
-    initialize_type_map_without_postgres_oids mapping
-    register_class_with_limit mapping, 'participants',
-                              ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Integer
-  end
-
-  alias_method_chain :initialize_type_map, :postgres_oids
-end

32a73a03d25f4e0ca7b31614ea8a242d39f20b57
Mark Oveson <mark.oveson@gmail.com>
2016-11-06 04:44:07 -0700

    Refactor Participant model; add spec for Participant#pull_data_from_effort.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -14,10 +14,10 @@ class Participant < ActiveRecord::Base
 
   attr_accessor :suggested_match
 
-  scope :with_age_and_effort_count, -> { select("participants.*, COUNT(efforts.id) as effort_count, ROUND(AVG((extract(epoch from(current_date - events.start_time))/60/60/24/365.25) + efforts.age)) as participant_age")
-                                             .joins("LEFT OUTER JOIN efforts ON (efforts.participant_id = participants.id)")
-                                             .joins("LEFT OUTER JOIN events ON (events.id = efforts.event_id)")
-                                             .group("participants.id") }
+  scope :with_age_and_effort_count, -> { select('participants.*, COUNT(efforts.id) as effort_count, ROUND(AVG((extract(epoch from(current_date - events.start_time))/60/60/24/365.25) + efforts.age)) as participant_age')
+                                             .joins('LEFT OUTER JOIN efforts ON (efforts.participant_id = participants.id)')
+                                             .joins('LEFT OUTER JOIN events ON (events.id = efforts.event_id)')
+                                             .group('participants.id') }
   scope :ordered_by_name, -> { order(:last_name, :first_name) }
 
 
@@ -27,36 +27,39 @@ class Participant < ActiveRecord::Base
             format: {with: VALID_EMAIL_REGEX}
 
 
-  # Search functions specific to Participant
-
   def self.search(param)
     return none if param.blank? || (param.length < 3)
     flexible_search(param)
   end
 
-  def approximate_age_today
-    average = efforts.joins(:event).average("((extract(epoch from(current_date - events.start_time))/60/60/24/365.25) + efforts.age)").to_f
-    average == 0 ? nil : average
-  end
-
-  def self.approximate_ages_today # Returns a hash of {participant_id => approximate age}
-    joins(:efforts => :event)
-        .group("participants.id")
-        .average("((extract(epoch from(current_date - events.start_time))/60/60/24/365.25) + efforts.age)")
-        .transform_values(&:to_f)
-  end
-
   def self.age_matches(age_param)
     return none unless age_param.is_a?(Numeric)
     threshold = 2 # Allow for some inaccuracy in reporting, rounding errors, etc.
-    exact_age_hash = exact_ages_today
-    approximate_age_hash = approximate_ages_today
-    age_hash = approximate_age_hash.merge(exact_age_hash)
-    age_hash.reject! { |_, age| (age - age_param).abs > threshold }
+    age_hash = approximate_ages_today.merge(exact_ages_today)
+                   .reject { |_, age| (age - age_param).abs > threshold }
     self.where(id: age_hash.keys)
   end
 
-  # Methods for determining if a user has claimed a participant
+  def self.columns_to_pull_from_model
+    id = ['id']
+    foreign_keys = Participant.column_names.find_all { |x| x.include?('_id') }
+    stamps = Participant.column_names.find_all { |x| x.include?('_at') | x.include?('_by') }
+    geographic = %w(country_code state_code city)
+    (column_names - (id + foreign_keys + stamps + geographic)).map &:to_sym
+  end
+
+  SQL = {ages_from_events: '((extract(epoch from(current_date - events.start_time))/60/60/24/365.25) + efforts.age)'}
+
+  def self.approximate_ages_today # Returns a hash of {participant_id => approximate age}
+    joins(:efforts => :event).group('participants.id').average(SQL[:ages_from_events]).transform_values(&:to_f)
+  end
+
+  private_class_method :approximate_ages_today
+
+  def approximate_age_today
+    average = efforts.joins(:event).average(SQL[:ages_from_events]).to_f
+    average == 0 ? nil : average
+  end
 
   def unclaimed?
     claimant.nil?
@@ -66,42 +69,45 @@ class Participant < ActiveRecord::Base
     claimant.present?
   end
 
-  # Methods related to matching and merging efforts with participants
-
-  def pull_data_from_effort(effort)
-    resolve_country(effort)
-    resolve_state_and_city(effort)
-    participant_attributes = Participant.columns_to_pull_from_model
-    participant_attributes.each do |attribute|
-      assign_attributes({attribute => effort[attribute]}) if self[attribute].blank?
-    end
-    if save
-      effort.participant ||= self
-      effort.save
-      effort.id
-    else
-      nil
-    end
+  def add_follower(user)
+    followers << user
   end
+
+  def remove_follower(user)
+    followers.delete(user)
+  end
+
+  # Methods related to matching and merging efforts with participants
 
   def most_likely_duplicate
     possible_matching_participants.first
   end
 
-  def merge_with(target)
-    @target_participant = Participant.find(target.id)
-    resolve_country(@target_participant)
-    resolve_state_and_city(@target_participant)
-    Participant.columns_to_pull_from_model.each do |attribute|
-      assign_attributes({attribute => @target_participant[attribute]}) if self[attribute].blank?
-    end
+  def pull_data_from_effort(effort)
+    pull_attributes(effort)
     if save
-      efforts << @target_participant.efforts
-      @target_participant.efforts = []
-      @target_participant.destroy
-      @target_participant.id
-    else
-      nil
+      effort.participant ||= self
+      effort.save
+    end
+  end
+
+  def merge_with(target)
+    target_participant = Participant.find(target.id)
+    pull_attributes(target_participant)
+    if save
+      efforts << target_participant.efforts
+      target_participant.efforts.destroy_all
+      target_participant.destroy
+    end
+  end
+
+  private
+
+  def pull_attributes(target)
+    resolve_country(target)
+    resolve_state_and_city(target)
+    Participant.columns_to_pull_from_model.each do |attribute|
+      assign_attributes({attribute => target[attribute]}) if self[attribute].blank?
     end
   end
 
@@ -126,24 +132,6 @@ class Participant < ActiveRecord::Base
     elsif (state_code == target.state_code) && city.blank?
       assign_attributes(city: target.city)
     end
-  end
-
-  def add_follower(user)
-    followers << user
-  end
-
-  def remove_follower(user)
-    followers.delete(user)
-  end
-
-  private
-
-  def self.columns_to_pull_from_model
-    id = ["id"]
-    foreign_keys = Participant.column_names.find_all { |x| x.include?("_id") }
-    stamps = Participant.column_names.find_all { |x| x.include?("_at") | x.include?("_by") }
-    geographic = ["country_code", "state_code", "city"]
-    (column_names - (id + foreign_keys + stamps + geographic)).map &:to_sym
   end
 
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+require 'rails_helper'
 
 # t.string   "first_name"
 # t.string   "last_name"
@@ -18,7 +18,7 @@ RSpec.describe Participant, type: :model do
   it { is_expected.to strip_attribute(:state_code).collapse_spaces }
   it { is_expected.to strip_attribute(:country_code).collapse_spaces }
 
-  it "should be valid when created with a first_name, a last_name, and a gender" do
+  it 'should be valid when created with a first_name, a last_name, and a gender' do
     participant = Participant.create!(first_name: 'Johnny', last_name: 'Appleseed', gender: 'male')
 
     expect(Participant.all.count).to eq(1)
@@ -28,25 +28,25 @@ RSpec.describe Participant, type: :model do
     expect(participant).to be_valid
   end
 
-  it "should be invalid without a first_name" do
+  it 'should be invalid without a first_name' do
     participant = Participant.new(first_name: nil, last_name: 'Appleseed', gender: 'male')
     expect(participant).not_to be_valid
     expect(participant.errors[:first_name]).to include("can't be blank")
   end
 
-  it "should be invalid without a last_name" do
+  it 'should be invalid without a last_name' do
     participant = Participant.new(first_name: 'Johnny', last_name: nil, gender: 'male')
     expect(participant).not_to be_valid
     expect(participant.errors[:last_name]).to include("can't be blank")
   end
 
-  it "should be invalid without a gender" do
+  it 'should be invalid without a gender' do
     participant = Participant.new(first_name: 'Johnny', last_name: 'Appleseed', gender: nil)
     expect(participant).not_to be_valid
     expect(participant.errors[:gender]).to include("can't be blank")
   end
 
-  it "should reject invalid email" do
+  it 'should reject invalid email' do
     participant1 = Participant.new(first_name: 'Johnny', last_name: 'Appleseed', gender: 'male', email: 'johnny@appleseed')
     participant2 = Participant.new(first_name: 'Johnny', last_name: 'Appleseed', gender: 'male', email: 'appleseed.com')
     participant3 = Participant.new(first_name: 'Johnny', last_name: 'Appleseed', gender: 'male', email: 'johnny@.com')
@@ -75,6 +75,71 @@ RSpec.describe Participant, type: :model do
     it 'should not accept country data from target when state does not exist in country of target' do
       @participant2.merge_with(@participant5)
       expect(@participant2.country_code).to be_nil
+    end
+
+  end
+
+  describe 'pull_data_from_effort' do
+    let(:course) { Course.create!(name: 'Test Course 100') }
+    let(:event) { Event.create!(course: course, name: 'Test Event', start_time: '2012-08-08 05:00:00') }
+
+    it 'should pull all effort data into corresponding empty fields' do
+      participant = Participant.new
+      effort = Effort.create!(event: event, bib_number: 99, city: 'Vancouver', birthdate: '1978-08-08',
+                                    state_code: 'BC', country_code: 'CA', age: 50,
+                                    first_name: 'Jen', last_name: 'Huckster', gender: 'female')
+      participant.pull_data_from_effort(effort)
+      expect(participant.first_name).to eq('Jen')
+      expect(participant.last_name).to eq('Huckster')
+      expect(participant.gender).to eq('female')
+      expect(participant.birthdate).to eq(Date.new(1978, 8, 8))
+      expect(participant.country_code).to eq('CA')
+      expect(participant.state_code).to eq('BC')
+    end
+
+    it 'should not pull effort data into corresponding populated fields' do
+      participant = Participant.new(birthdate: '1978-01-01', country_code: 'US',
+                                    first_name: 'Jennifer', last_name: 'Huckster', gender: 'female')
+      effort = Effort.create!(event: event, bib_number: 99, city: 'Vancouver', birthdate: '1978-08-08',
+                              state_code: 'BC', country_code: 'CA', age: 50,
+                              first_name: 'Jen', last_name: 'Huckster', gender: 'female')
+      participant.pull_data_from_effort(effort)
+      expect(participant.first_name).to eq('Jennifer')
+      expect(participant.last_name).to eq('Huckster')
+      expect(participant.gender).to eq('female')
+      expect(participant.birthdate).to eq(Date.new(1978, 1, 1))
+      expect(participant.country_code).to eq('US')
+    end
+
+    it 'upon successful save should associate the participant with the pulled effort' do
+      participant = Participant.new
+      effort = Effort.create!(event: event, bib_number: 99, city: 'Vancouver', birthdate: '1978-08-08',
+                              state_code: 'BC', country_code: 'CA', age: 50,
+                              first_name: 'Jen', last_name: 'Huckster', gender: 'female')
+      participant.pull_data_from_effort(effort)
+      expect(effort.participant).to eq(participant)
+    end
+
+    it 'should not pull state_code if country_code is different' do
+      participant = Participant.new(birthdate: '1978-01-01', country_code: 'US',
+                                    first_name: 'Jennifer', last_name: 'Huckster', gender: 'female')
+      effort = Effort.create!(event: event, bib_number: 99, city: 'Vancouver', birthdate: '1978-08-08',
+                              state_code: 'BC', country_code: 'CA', age: 50,
+                              first_name: 'Jen', last_name: 'Huckster', gender: 'female')
+      participant.pull_data_from_effort(effort)
+      expect(participant.state_code).to be_nil
+    end
+
+    it 'should return false if participant does not save' do
+      participant = Participant.new
+      effort = Effort.new(event: event, first_name: nil, last_name: nil, gender: 'female')
+      expect(participant.pull_data_from_effort(effort)).to be_falsey
+    end
+
+    it 'should return true if participant saves' do
+      participant = Participant.new
+      effort = Effort.new(event: event, first_name: 'Jen', last_name: 'Huckster', gender: 'female')
+      expect(participant.pull_data_from_effort(effort)).to be_truthy
     end
 
   end

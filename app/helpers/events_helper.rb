6bc009c16ec0cf9ee7ef7692430e0a9fd0868457
Mark Oveson <mark.oveson@gmail.com>
2016-11-07 14:57:44 -0700

    Refactor EventsHelper and remove unused code.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,24 +1,11 @@
 module EventsHelper
 
-  def make_event_split_id_array(event_id, splits)
-    event_split_id_array = []
-    splits.each do |split|
-      @event_split = AidStation.find_by(event_id: event_id, split_id: split.id)
-      event_split_id_array << @event_split.id
-    end
-    event_split_id_array
-  end
-
-  def make_suggested_match_id_hash(efforts)
-    id_hash = {}
-    efforts.each do |effort|
-      id_hash[effort.id] = effort.suggested_match.id if effort.suggested_match
-    end
-    id_hash
+  def suggested_match_id_hash(efforts)
+    efforts.select(&:suggested_match).map { |effort| [effort.id, effort.suggested_match.id] }.to_h
   end
 
   def suggested_match_count(efforts)
-    make_suggested_match_id_hash(efforts).count
+    suggested_match_id_hash(efforts).count
   end
 
   def data_status(status_int)

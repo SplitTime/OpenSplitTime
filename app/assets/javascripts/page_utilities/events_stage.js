869ffa76817278edbf2c20fc93656c8948c8c0c1
Mark Oveson <mark.oveson@gmail.com>
2016-11-08 15:16:46 -0700

    Disable file import buttons in event stage (admin) view until a file has been selected.

---
+++ app/assets/javascripts/page_utilities/events_stage.js
@@ -0,0 +1,14 @@
+$('.events.stage').ready(function () {
+        $('#file-splits').on("change", function () {
+            $('#submit-splits').prop('disabled', !$(this).val());
+        });
+
+        $('#file-efforts-with-times').on("change", function () {
+            $('#submit-efforts-with-times').prop('disabled', !$(this).val());
+        });
+
+        $('#file-efforts-without-times').on("change", function () {
+            $('#submit-efforts-without-times').prop('disabled', !$(this).val());
+        });
+    }
+);

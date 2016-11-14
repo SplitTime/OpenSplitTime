1c6a79a78f0c3b504634b5a73542974c56543cd8
Mark Oveson <mark.oveson@gmail.com>
2016-11-14 11:17:56 -0700

    Remove obsolete set_effort_ages method from dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,16 +5,4 @@ class Admin::DashboardController < Admin::BaseController
   def dashboard
     authorize :dashboard, :show?
   end
-
-  def set_effort_ages
-    efforts = Effort.where.not(birthdate: nil).where(age: nil)
-    if efforts.count > 0
-      update_count = efforts.reset_effort_ages
-      flash[:success] = "Set ages for #{update_count} efforts."
-    else
-      flash[:warning] = "No efforts contained birthdates without ages."
-    end
-    redirect_to admin_root_path
-  end
-
 end

ad8d5b7349456d646418a34b534c538a401f79af
moveson <mark.oveson@gmail.com>
2016-11-29 21:18:20 -0700

    Refactor LiveEffortMailData to accept flexible arguments and streamline code; write LifeEffortMailData spec.

--- app/services/follower_mailer_service.rb
+++ app/services/follower_mailer_service.rb
@@ -2,7 +2,7 @@ class FollowerMailerService
 
   def self.send_live_effort_mail(participant_id, split_time_ids)
     return unless participant_id.present?
-    live_effort_mail_data = LiveEffortMailData.new(participant_id, split_time_ids)
+    live_effort_mail_data = LiveEffortMailData.new(participant_id: participant_id, split_time_ids: split_time_ids)
     live_effort_mail_data.followers.each do |follower|
       FollowerMailer.live_effort_email(follower, live_effort_mail_data.effort_data).deliver_later
     end

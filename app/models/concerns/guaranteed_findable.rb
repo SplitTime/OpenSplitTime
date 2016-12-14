74137c1a31571b3a06367108bf01cb2eada78d2a
moveson <mark.oveson@gmail.com>
2016-12-13 23:31:38 -0700

    Add #null_record?, #real_record?, and #real_presence methods to GuaranteedFindable module.

--- app/models/concerns/guaranteed_findable.rb
+++ app/models/concerns/guaranteed_findable.rb
@@ -3,6 +3,18 @@ module GuaranteedFindable
 
   # Requires .null_record class method on incorporating class
 
+  def null_record?
+    self == self.class.null_record
+  end
+
+  def real_record?
+    !null_record?
+  end
+
+  def real_presence
+    self if real_record?
+  end
+
   module ClassMethods
     def find_guaranteed(args)
       find_by(args) || null_record

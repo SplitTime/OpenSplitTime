f7cd8216ec9b6f6c4182f9d5c32c2168b437892b
moveson <mark.oveson@gmail.com>
2017-01-05 12:00:03 -0700

    Optimize TimeCluster.

--- app/view_models/time_cluster.rb
+++ app/view_models/time_cluster.rb
@@ -3,7 +3,7 @@ class TimeCluster
   attr_reader :drop_display
   delegate :finish?, to: :split
 
-  def initialize(split, split_times, prior_time, start_time, drop_display = nil)
+  def initialize(split, split_times, prior_time, start_time, drop_display = false)
     @split = split
     @split_times = split_times
     @prior_time = prior_time
@@ -12,29 +12,28 @@ class TimeCluster
   end
 
   def segment_time
-    return nil unless (prior_time && (times_from_start.compact.count > 0))
-    times_from_start.compact.first - prior_time
+    @segment_time ||=
+        times_from_start.compact.first - prior_time if (prior_time && (times_from_start.compact.count > 0))
   end
 
   def time_in_aid
-    return nil unless times_from_start.compact.count > 1
-    times_from_start.compact.last - times_from_start.compact.first
+    @time_in_aid ||=
+        times_from_start.compact.last - times_from_start.compact.first if times_from_start.compact.count > 1
   end
 
   def times_from_start
-    split_times.map { |st| st ? st.time_from_start : nil }
+    @times_from_start ||= split_times.map { |st| st.try(:time_from_start) }
   end
 
   def days_and_times
-    times_from_start.map { |time| time ? start_time + time.seconds : nil }
+    @days_and_times ||= times_from_start.map { |time| time && (start_time + time.seconds) }
   end
 
   def time_data_statuses
-    split_times.map { |st| st ? st.data_status : nil }
+    @time_data_statuses ||= split_times.map { |st| st.try(:data_status) }
   end
 
   private
 
   attr_reader :split, :split_times, :start_time, :prior_time
-
 end

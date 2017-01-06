1c8007f4b9dc92dd758658b19ecb2cfcd8c379c0
moveson <mark.oveson@gmail.com>
2017-01-06 02:15:42 -0700

    Update ImportEffortsWithoutTimesJob to call EffortImporter with an args hash.

--- app/jobs/import_efforts_without_times_job.rb
+++ app/jobs/import_efforts_without_times_job.rb
@@ -3,7 +3,7 @@ class ImportEffortsWithoutTimesJob < ActiveJob::Base
   queue_as :default
 
   def perform(file_url, event, user_id)
-    importer = EffortImporter.new(file_url, event, user_id)
+    importer = EffortImporter.new(file_path: file_url, event: event, current_user_id: user_id)
     importer.effort_import_without_times
     importer.effort_import_report
   end

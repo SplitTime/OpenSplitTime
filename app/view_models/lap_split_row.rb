1e2ff87a92b70c3e2205d87d55f53d7c57287cb6
moveson <mark.oveson@gmail.com>
2017-01-27 22:39:56 -0700

    Rename SplitRow to LapSplitRow; add LapSplitRow#lap and change LapSplitRow#name so it returns split.name if args[:show_laps] is false and lap_split.name otherwise; add tests for LapSplitRow#name.

--- app/view_models/lap_split_row.rb
+++ app/view_models/lap_split_row.rb
@@ -1,6 +1,7 @@
-class SplitRow
+class LapSplitRow
 
-  delegate :id, :name, :distance_from_start, :kind, :start?, :intermediate?, :finish?, to: :split
+  delegate :distance_from_start, to: :lap_split
+  delegate :id, :kind, :start?, :intermediate?, :finish?, to: :split
   delegate :segment_time, :time_in_aid, :times_from_start, :days_and_times, :time_data_statuses,
            :split_time_ids, to: :time_cluster
 
@@ -11,22 +12,30 @@ class SplitRow
     ArgsValidator.validate(params: args,
                            required: [:split_times, :start_time],
                            required_alternatives: [:lap_split, :split],
-                           exclusive: [:lap_split, :split, :split_times, :prior_time, :start_time],
+                           exclusive: [:lap_split, :split, :split_times, :prior_time, :start_time, :show_laps],
                            deprecated: {split: :lap_split},
                            class: self.class)
     @lap_split = args[:lap_split]
-    @arg_split = args[:split]
     @split_times = args[:split_times]
     @prior_time = args[:prior_time]
     @start_time = args[:start_time]
+    @show_laps = args[:show_laps]
+  end
+
+  def name
+    @show_laps ? name_with_lap : name_without_lap
   end
 
   def time_cluster
     @time_cluster ||= TimeCluster.new(split, split_times, prior_time, start_time)
   end
 
+  def lap
+    lap_split.lap
+  end
+
   def split
-    @split ||= arg_split || lap_split.split
+    lap_split.split
   end
 
   def split_id
@@ -47,5 +56,13 @@ class SplitRow
 
   private
 
-  attr_reader :lap_split, :arg_split, :split_times, :prior_time, :start_time
+  attr_reader :lap_split, :split_times, :prior_time, :start_time
+
+  def name_without_lap
+    split.name
+  end
+
+  def name_with_lap
+    lap_split.name
+  end
 end

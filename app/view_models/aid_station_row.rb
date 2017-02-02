f3e830f9d0e6d72d0de469551a477c826c153e0b
Mark Oveson <mark.oveson@gmail.com>
2017-02-02 08:37:10 -0700

    Continue refactoring AidStationsDisplay, AidStationDetail, and AidStationRow.

--- app/view_models/aid_station_row.rb
+++ app/view_models/aid_station_row.rb
@@ -1,9 +1,8 @@
-class AidStationDetail
+class AidStationRow
 
   attr_reader :aid_station
   delegate :course, :race, to: :event
-  delegate :event, :split, :split_id, :open_time, :close_time, :status, :captain_name, :comms_crew_names,
-           :comms_frequencies, :current_issues, to: :aid_station
+  delegate :event, :split_id, to: :aid_station
   delegate :expected_day_and_time, :prior_valid_display_data, :next_valid_display_data, to: :live_event
 
   EFFORT_CATEGORIES = [:started, :recorded_in, :recorded_out, :dropped_here, :in_aid, :missed, :expected]
@@ -51,39 +50,6 @@ class AidStationDetail
     efforts.select { |effort| ordered_split_ids.index(effort.final_split_id) > ordered_split_ids.index(split_id) }
   end
 
-  def efforts_recorded_out
-    @efforts_recorded_out ||=
-        efforts_started.select { |effort| indexed_split_times[effort.id].map(&:bitkey).include?(SubSplit::OUT_BITKEY) }
-  end
-
-  def efforts_dropped_here
-    @efforts_dropped_here ||= efforts_dropped.select { |effort| effort.dropped_split_id == split_id }
-  end
-
-  def efforts_in_aid
-    @efforts_in_aid ||= efforts_recorded_in - efforts_recorded_out
-  end
-
-  def efforts_missed
-    @efforts_missed ||= efforts_not_recorded.select { |effort| recorded_later(effort) }
-  end
-
-  def efforts_expected
-    @efforts_expected ||= efforts_not_recorded - efforts_missed - efforts_dropped
-  end
-
-  EFFORT_CATEGORIES.each do |category|
-    define_method("efforts_#{category}_count") do
-      method("efforts_#{category}").call.size
-    end
-  end
-
-  EFFORT_CATEGORIES.each do |category|
-    define_method("efforts_#{category}_ids") do
-      method("efforts_#{category}").call.map(&:id)
-    end
-  end
-
   EFFORT_CATEGORIES.each do |category|
     define_method("efforts_#{category}_table_title") do
       count = method("efforts_#{category}_count").call
@@ -95,52 +61,13 @@ class AidStationDetail
     split.base_name
   end
 
-  def event_name
-    event.name
-  end
-
-  def course_name
-    course.name
-  end
-
-  def race_name
-    race && race.name
-  end
-
-  def recorded_in_day_and_time(effort)
-    recorded_day_and_time(effort, sub_split_in)
-  end
-
-  def recorded_out_day_and_time(effort)
-    recorded_day_and_time(effort, sub_split_out)
-  end
-
   private
 
   attr_reader :event_data
-  delegate :sub_split_in, :sub_split_out, to: :split
-  delegate :ordered_split_ids, :efforts_started, :efforts_dropped, :efforts_finished,
-           :efforts_in_progress, to: :event_data
+  delegate :ordered_split_ids, :efforts_dropped, to: :event_data
 
   def split_times
     event.split_times.recorded_at_aid(aid_station.id)
-  end
-
-  def efforts_not_recorded
-    @efforts_not_recorded ||= efforts_started - efforts_recorded_in - efforts_recorded_out
-  end
-
-  def start_split_id
-    ordered_split_ids.first
-  end
-
-  def recorded_later(effort)
-    ordered_split_ids.index(effort.final_split_id) > ordered_split_ids.index(split_id)
-  end
-
-  def recorded_day_and_time(effort, sub_split)
-    split_time = indexed_split_times[effort.id].index_by(&:sub_split)[sub_split]
-    split_time && effort.start_time + split_time.time_from_start
   end
 
   def persons(number)
--- app/view_models/aid_stations_display.rb
+++ app/view_models/aid_stations_display.rb
@@ -1,48 +1,21 @@
-class AidStationsDisplay
+class AidStationsDisplay < LiveEventFramework
 
-  attr_reader :live_event
   delegate :start_time, :course, :race, to: :event
-  delegate :captain_name, :comms_crew_names, :comms_frequencies, to: :aid_station
 
-  def initialize(event)
-    @event = event
-    @live_event = LiveEvent.new(@event)
-    @aid_stations = @event.aid_stations.ordered.to_a[1..-1]
-    @split_times_by_split = split_times.group_by(&:split_id)
-    @split_times_by_split.default = []
+  def post_initialize(args)
+    nil
   end
 
   def aid_station_rows
     @aid_station_rows ||=
-        aid_stations.map { |aid_station| AidStationDetail.new(aid_station: aid_station,
-                                                              live_event: live_event,
-                                                              indexed_split_times: split_times_by_effort(aid_station)) }
+        aid_stations.map { |aid_station| AidStationRow.new(aid_station: aid_station,
+                                                           event_data: self) }
   end
 
-  def split_times_by_effort(aid_station)
-    hash = split_times_by_split[aid_station.split_id].group_by(&:effort_id)
-    hash.default = []
-    hash
-  end
-
-  def efforts_started_count
-    efforts_started.size
-  end
-
-  def efforts_finished_count
-    efforts_finished.size
-  end
-
-  def efforts_dropped_count
-    efforts_dropped.size
-  end
-
-  def efforts_in_progress_count
-    efforts_in_progress.size
-  end
-
-  def event_name
-    event.name
+  EFFORT_CATEGORIES.each do |category|
+    define_method("efforts_#{category}_ids") do
+      efforts.select { |effort| effort.send("#{category}?") }.map(&:id)
+    end
   end
 
   def course_name
@@ -70,8 +43,9 @@ class AidStationsDisplay
 
   private
 
-  attr_accessor :efforts_started
-  attr_reader :event, :aid_stations, :split_times_by_split
-  delegate :ordered_splits, :ordered_split_ids, :split_times, :efforts, :efforts_started,
-           :efforts_finished, :efforts_dropped, :efforts_in_progress, to: :live_event
+  attr_reader :event
+
+  def aid_stations
+    @aid_stations ||= event.aid_stations.ordered.to_a[1..-1]
+  end
 end

fb4751650be1834d970b1addf7ce829eafd42e98
Mark Oveson <mark.oveson@gmail.com>
2017-02-06 04:07:54 -0700

    Add Live::EventsHelper#display_progress_time_only.

--- app/helpers/live/events_helper.rb
+++ app/helpers/live/events_helper.rb
@@ -5,9 +5,16 @@ module Live::EventsHelper
 
     if split_name && day_and_time
       "#{split_name} at #{l(day_and_time, format: :day_and_military) }"
+    elsif split_name
+      "#{split_name} at [unknown time]"
     else
       '[None]'
     end
+  end
+
+  def display_progress_time_only(args)
+    day_and_time = args[:day_and_time]
+    day_and_time ? l(day_and_time, format: :day_and_military) : '[unknown time]'
   end
 
   def join_days_and_times(days_and_times)

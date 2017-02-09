ed6b759171350dcb782f0c5b33ef0c3e28308141
moveson <mark.oveson@gmail.com>
2017-02-09 16:54:34 -0700

    Thank-you message and OpenSplitTime indications in email subject and body.

--- app/mailers/follower_mailer.rb
+++ app/mailers/follower_mailer.rb
@@ -4,7 +4,7 @@ class FollowerMailer < ApplicationMailer
     @follower = follower
     @effort_data = effort_data
     puts "Sending email to #{@follower.full_name}."
-    mail(to: @follower.email, subject: "Update for #{@effort_data[:full_name]} at #{@effort_data[:event_name]}")
+    mail(to: @follower.email, subject: "Update for #{@effort_data[:full_name]} at #{@effort_data[:event_name]} from OpenSplitTime")
   end
 
 end

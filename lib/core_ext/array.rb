c4f49f94f26cbabf5addbdd64807022ee3f430f9
moveson <mark.oveson@gmail.com>
2017-02-19 07:49:25 -0700

    Add Array#included_before? and #included_after? with tests.

--- lib/core_ext/array.rb
+++ lib/core_ext/array.rb
@@ -1,11 +1,21 @@
 class Array
-  def elements_before(element)
-    i = index(element)
+  def elements_before(index_element)
+    i = index(index_element)
     i ? self[0, i] : []
   end
 
-  def elements_after(element)
-    i = index(element)
+  def elements_after(index_element)
+    i = index(index_element)
     i ? self[(i + 1)..-1] : []
+  end
+
+  def included_before?(index_element, subject_element)
+    i = index(index_element)
+    i ? self[0, i].include?(subject_element) : false
+  end
+
+  def included_after?(index_element, subject_element)
+    i = index(index_element)
+    i ? self[(i + 1)..-1].include?(subject_element) : false
   end
 end
--- spec/lib/core_ext/array_spec.rb
+++ spec/lib/core_ext/array_spec.rb
@@ -64,4 +64,120 @@ describe Array do
       expect(sub).to eq([])
     end
   end
+
+  describe '#included_before?' do
+    it 'returns true if the subject element is included in the array indexed prior to the index element' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'ferret'
+      subject_element = 'bird'
+      expect(array.included_before?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'returns false if the subject element is not included in the array indexed prior to the index element' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'ferret'
+      subject_element = 'coyote'
+      expect(array.included_before?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'returns false if the index element is not found in the array' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'buffalo'
+      subject_element = 'bird'
+      expect(array.included_before?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'returns false if the subject element is not found in the array' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'sheep'
+      subject_element = 'buffalo'
+      expect(array.included_before?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'works as expected when nil is provided as the index element and the subject element appears before' do
+      array = [1, 2, 3, nil, 5]
+      index_element = nil
+      subject_element = 2
+      expect(array.included_before?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'works as expected when nil is provided as the index element and the subject element does not appear before' do
+      array = [1, 2, 3, nil, 5]
+      index_element = nil
+      subject_element = 5
+      expect(array.included_before?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'works as expected when nil is provided as the subject element and appears before' do
+      array = [1, nil, 3, 4, 5]
+      index_element = 4
+      subject_element = nil
+      expect(array.included_before?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'works as expected when nil is provided as the subject element and does not appear before' do
+      array = [1, 2, 3, 4, 5]
+      index_element = 4
+      subject_element = nil
+      expect(array.included_before?(index_element, subject_element)).to eq(false)
+    end
+  end
+
+  describe '#included_after?' do
+    it 'returns true if the subject element is included in the array indexed after the index element' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'bird'
+      subject_element = 'ferret'
+      expect(array.included_after?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'returns false if the subject element is not included in the array indexed after the index element' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'bird'
+      subject_element = 'cat'
+      expect(array.included_after?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'returns false if the index element is not found in the array' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'buffalo'
+      subject_element = 'bird'
+      expect(array.included_after?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'returns false if the subject element is not found in the array' do
+      array = %w(cat bird sheep ferret coyote)
+      index_element = 'sheep'
+      subject_element = 'buffalo'
+      expect(array.included_after?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'works as expected when nil is provided as the index element and the subject element appears after' do
+      array = [1, nil, 3, 4, 5]
+      index_element = nil
+      subject_element = 4
+      expect(array.included_after?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'works as expected when nil is provided as the index element and the subject element does not appear after' do
+      array = [1, 2, 3, nil, 5]
+      index_element = nil
+      subject_element = 2
+      expect(array.included_after?(index_element, subject_element)).to eq(false)
+    end
+
+    it 'works as expected when nil is provided as the subject element and appears after' do
+      array = [1, 2, 3, nil, 5]
+      index_element = 2
+      subject_element = nil
+      expect(array.included_after?(index_element, subject_element)).to eq(true)
+    end
+
+    it 'works as expected when nil is provided as the subject element and does not appear after' do
+      array = [1, 2, 3, 4, 5]
+      index_element = 4
+      subject_element = nil
+      expect(array.included_after?(index_element, subject_element)).to eq(false)
+    end
+  end
 end

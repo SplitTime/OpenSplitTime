c1e202844a69531794b7567d4d216a6d748866c5
moveson <mark.oveson@gmail.com>
2017-02-19 13:46:11 -0700

    Update split_row nomenclature to lap_split_row or simply row in SplitTimesHelper and plan_detail partial.

--- app/helpers/split_times_helper.rb
+++ app/helpers/split_times_helper.rb
@@ -1,10 +1,10 @@
 module SplitTimesHelper
 
-  def composite_time(split_row)
+  def composite_time(lap_split_row)
     time_array = []
-    (0...split_row.times_from_start.size).each do |i|
-      time = split_row.times_from_start[i]
-      data_status = split_row.time_data_statuses[i]
+    (0...lap_split_row.times_from_start.size).each do |i|
+      time = lap_split_row.times_from_start[i]
+      data_status = lap_split_row.time_data_statuses[i]
       if time
         element = case data_status
                     when 'bad'
@@ -22,11 +22,11 @@ module SplitTimesHelper
     time_array.join(' / ')
   end
 
-  def composite_time_zzs(split_row)
+  def composite_time_zzs(lap_split_row)
     time_array = []
-    (0...split_row.times_from_start.size).each do |i|
-      time = split_row.times_from_start[i]
-      data_status = split_row.time_data_statuses[i]
+    (0...lap_split_row.times_from_start.size).each do |i|
+      time = lap_split_row.times_from_start[i]
+      data_status = lap_split_row.time_data_statuses[i]
       if time
         element = case data_status
                     when 'bad'

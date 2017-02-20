4e19c59f7457db7c9cd9f39c91b01fc7d2d5dbf8
moveson <mark.oveson@gmail.com>
2017-02-19 20:57:03 -0700

    Remove debugging message from FollowerMailer#live_effort_email.

--- app/mailers/follower_mailer.rb
+++ app/mailers/follower_mailer.rb
@@ -4,7 +4,6 @@ class FollowerMailer < ApplicationMailer
   def live_effort_email(follower, effort_data)
     @follower = follower
     @effort_data = effort_data
-    puts "Sending email to #{@follower.full_name}."
     mail(to: @follower.email, subject: "Update for #{@effort_data[:full_name]} at #{@effort_data[:event_name]} from OpenSplitTime")
   end
 end

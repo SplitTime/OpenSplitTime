0dd2a8344a6b4d03c90e794a15e213a3aec9d2d6
moveson <mark.oveson@gmail.com>
2017-02-19 19:48:51 -0700

    Remove Effort#dropped_split_id and Effort#dropped_lap from DroppedAttributesSetter; remove EffortShowView#dropped_split_id.

--- app/services/dropped_attributes_setter.rb
+++ app/services/dropped_attributes_setter.rb
@@ -19,22 +19,13 @@ class DroppedAttributesSetter
 
   def set_attributes
     efforts.each do |effort|
-      unless effort.finished?
-        effort.assign_attributes(dropped_split_id: effort.split_times.last.split_id,
-                                 dropped_lap: effort.split_times.last.lap)
-      end
       effort.split_times.each { |st| st.assign_attributes(stopped_here: false) }
       effort.split_times.last.assign_attributes(stopped_here: true)
       changed_split_times << effort.split_times.select(&:changed?)
     end
   end
 
-  def changed_efforts
-    efforts.select(&:changed?)
-  end
-
   def save_changes
-    reports << BulkUpdateService.update_attributes(:efforts, changed_effort_attributes)
     reports << BulkUpdateService.update_attributes(:split_times, changed_split_time_attributes)
   end
 
@@ -48,10 +39,5 @@ class DroppedAttributesSetter
 
   def changed_split_time_attributes
     changed_split_times.flatten.map { |st| [st.id, {stopped_here: st.stopped_here}] }
-  end
-
-  def changed_effort_attributes
-    changed_efforts.map { |effort| [effort.id, {dropped_split_id: effort.dropped_split_id,
-                                                dropped_lap: effort.dropped_lap}] }.to_h
   end
 end
--- app/view_models/effort_show_view.rb
+++ app/view_models/effort_show_view.rb
@@ -2,7 +2,7 @@ class EffortShowView
 
   attr_reader :effort, :lap_split_rows
   delegate :full_name, :event_name, :participant, :bib_number, :gender, :split_times,
-           :finish_status, :report_url, :beacon_url, :dropped_split_id, :start_time, :photo_url,
+           :finish_status, :report_url, :beacon_url, :start_time, :photo_url,
            :overall_place, :gender_place, :started?, :finished?, :dropped?, :in_progress?, to: :effort
   delegate :simple?, to: :event
 

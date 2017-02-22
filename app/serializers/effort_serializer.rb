837577ea20d3826d75e8138293179a7c32dc8ae9
moveson <mark.oveson@gmail.com>
2017-02-22 06:43:17 -0700

    Add split_times relationship to EffortSerializer; add link to EventSerializer.

--- app/serializers/effort_serializer.rb
+++ app/serializers/effort_serializer.rb
@@ -1,4 +1,6 @@
 class EffortSerializer < BaseSerializer
   attributes :id, :event_id, :participant_id, :bib_number, :first_name, :last_name, :full_name, :gender,
              :birthdate, :age, :city, :state_code, :country_code
+
+  has_many :split_times
 end

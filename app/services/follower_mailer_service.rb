c99dd6190232a846561ae5ea5320a86a69f09f5e
Mark Oveson <mark.oveson@gmail.com>
2017-02-28 02:33:37 -0700

    Update FollowerMailerService to accept keyword arguments and support multiple laps.

--- app/services/follower_mailer_service.rb
+++ app/services/follower_mailer_service.rb
@@ -1,7 +1,15 @@
 class FollowerMailerService
-  def self.send_live_effort_mail(participant_id, split_time_ids)
+  def self.send_live_effort_mail(args)
+    ArgsValidator.validate(params: args,
+                           required: [:participant_id, :split_time_ids],
+                           exclusive: [:participant_id, :split_time_ids, :multi_lap],
+                           class: self)
+    participant_id = args[:participant_id]
+    split_time_ids = args[:split_time_ids]
+    multi_lap = args[:multi_lap]
     return unless participant_id.present? && split_time_ids.compact.present?
-    live_effort_mail_data = LiveEffortMailData.new(participant_id: participant_id, split_time_ids: split_time_ids)
+    live_effort_mail_data =
+        LiveEffortMailData.new(participant_id: participant_id, split_time_ids: split_time_ids, multi_lap: multi_lap)
     live_effort_mail_data.followers.each do |follower|
       FollowerMailer.live_effort_email(follower, live_effort_mail_data.effort_data).deliver_later
     end

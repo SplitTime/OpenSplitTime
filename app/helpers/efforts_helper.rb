444c4f2a9ac87a5a9cfa2389680bb372dfd20103
moveson <mark.oveson@gmail.com>
2017-03-03 12:08:17 -0700

    Move data status tag logic out of efforts_list partial and into EffortsHelper.

---
+++ app/helpers/efforts_helper.rb
@@ -0,0 +1,12 @@
+module EffortsHelper
+
+  def data_status_tag(effort_row)
+    if effort_row.bad?
+      tag('tr', class: "text-danger")
+    elsif effort_row.questionable?
+      tag('tr', class: "text-warning")
+    else
+      tag('tr')
+    end
+  end
+end

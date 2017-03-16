a1649ffcc71bc9ba9d5586258ae41a030819937e
moveson <mark.oveson@gmail.com>
2017-03-16 17:58:13 -0600

    Rename subscription_spec file; update Subscription tests so as not to create new records.

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -8,12 +8,11 @@ RSpec.describe Subscription, type: :model do
   it 'should be valid when created with a user_id, a participant_id, and a kind' do
     user = User.create!(first_name: 'Test', last_name: 'User', role: :user, email: 'user@example.com', password: 'password')
     participant = Participant.create!(first_name: 'Freddy', last_name: 'Fast', gender: 'male')
-    subscription = Subscription.create!(user_id: user.id, participant_id: participant.id)
+    subscription = Subscription.new(user_id: user.id, participant_id: participant.id, protocol: 'email')
 
-    expect(Subscription.all.count).to eq(1)
     expect(subscription.user_id).to eq(user.id)
     expect(subscription.participant_id).to eq(participant.id)
-    expect(subscription.kind).to eq('casual')
+    expect(subscription.protocol).to eq('email')
     expect(subscription).to be_valid
   end
 

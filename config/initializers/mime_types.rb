2a20513b77dd02dbaa99a49cc4e1682e9da88583
moveson <mark.oveson@gmail.com>
2017-03-28 16:00:59 -0600

    Add Mime::Type for jsonapi.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,4 +3,5 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "application/xls", :xls
+Mime::Type.register 'application/xls', :xls
+Mime::Type.register 'application/vnd.api+json', :jsonapi

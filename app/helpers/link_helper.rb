9bd6cbc11a80bfc847bf6d08dbb0d12a85cb9021
moveson <mark.oveson@gmail.com>
2017-04-04 23:27:41 -0600

    Update LinkHelper.reversed_sort_params to allow :asc or :desc default options.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,10 +1,14 @@
 module LinkHelper
 
-  def reversed_sort_param(field)
-    (params[:sort][field.to_sym] == :desc) ? "#{field}" : "-#{field}"
+  def reversed_sort_param(field, default = :asc)
+    if default == :desc
+      (params[:sort][field] == :desc) ? "#{field}" : "-#{field}"
+    else
+      (params[:sort][field] == :asc) ? "-#{field}" : "#{field}"
+    end
   end
 
   def toggled_sort_param(field_1, field_2)
-    params[:sort].keys.include?(field_1.to_sym) ? field_2.to_s : field_1.to_s
+    params[:sort].keys.include?(field_1.to_s) ? field_2.to_s : field_1.to_s
   end
 end

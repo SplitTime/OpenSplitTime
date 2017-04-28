0cb1d0532657a753b39cf037c7edf2cb67455d75
moveson <mark.oveson@gmail.com>
2017-04-28 17:17:32 -0600

    Add phone, email, and url attributes to EffortSerializer.

--- app/serializers/effort_serializer.rb
+++ app/serializers/effort_serializer.rb
@@ -1,6 +1,6 @@
 class EffortSerializer < BaseSerializer
   attributes :id, :event_id, :participant_id, :bib_number, :first_name, :last_name, :full_name, :gender,
-             :birthdate, :age, :city, :state_code, :country_code
+             :birthdate, :age, :city, :state_code, :country_code, :phone, :email, :beacon_url, :photo_url, :report_url
   link(:self) { api_v1_effort_path(object) }
 
   has_many :split_times, if: :split_times_loaded?

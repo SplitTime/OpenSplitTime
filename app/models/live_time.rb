3f7ff6da2c02147f64d6c67291870bdddcfe8627
moveson <mark.oveson@gmail.com>
2017-05-07 09:05:52 -0600

    Refine source enum.

--- app/models/live_time.rb
+++ app/models/live_time.rb
@@ -1,8 +1,7 @@
 class LiveTime < ActiveRecord::Base
-  enum source: [:internal, :api]
+  enum source: [:internal, :generic_api]
   include Auditable
 
   belongs_to :event
   belongs_to :split
-
 end

47cb6e82392b7c9261f0f412f0259dc163e4d3cf
moveson <mark.oveson@gmail.com>
2017-05-09 13:29:26 -0600

    Add BucketStoreService#read; extract logic specific to Amazon S3 to new S3FileManager class.

--- app/services/bucket_store_service.rb
+++ app/services/bucket_store_service.rb
@@ -6,17 +6,37 @@ class BucketStoreService
     if Rails.env.test?
       file.path
     elsif file_size_ok(file)
-      obj = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET']).object("#{directory}/#{id}/#{file.original_filename}")
-      obj.upload_file(file.path, acl: 'public-read')
-      warn "Uploaded file available at #{obj.public_url}"
-      obj.public_url
+      key = "#{directory}/#{id}/#{file.original_filename}"
+      public_url = S3FileManager.public_upload(key, file)
+      public_url
     else
       errors.add(:bucket_store_service, "Import file must be less than #{MAX_FILESIZE / 1024 / 1024} MB")
       false
     end
   end
 
+  def self.read(path_or_url)
+    case
+    when uri?(path_or_url)
+      key = URI.parse(url).path[1..-1]
+      S3FileManager.read(key)
+    when local_path?(path_or_url)
+      full_path = "#{Rails.root}#{path_or_url}"
+      File.new(full_path)
+    else # Assume it is an s3 key
+      S3FileManager.read(path_or_url)
+    end
+  end
+
   def self.file_size_ok(file)
     file.size <= MAX_FILESIZE
+  end
+
+  def self.uri?(string)
+    string.start_with?('http://', 'https://')
+  end
+
+  def self.local_path?(string)
+    string.start_with?('/')
   end
 end

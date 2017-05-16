04c53790598208155a36f54d67adaa9f6ada56cc
moveson <mark.oveson@gmail.com>
2017-05-16 17:27:53 -0600

    Add validations to BackgroundNotifier setup; handle zero values passed to BackgroundNotifier#progress; update spec.

--- spec/services/background_notifier_spec.rb
+++ spec/services/background_notifier_spec.rb
@@ -21,6 +21,22 @@ RSpec.describe BackgroundNotifier do
       expect { BackgroundNotifier.new(channel: channel, event: nil) }
           .to raise_error(/must include event/)
     end
+
+    context 'when current_object is larger than total_objects' do
+      let(:args) { {channel: channel, event: event, action: 'imported', resource: 'effort', current_object: 10, total_objects: 9} }
+
+      it 'raises an ArgumentError' do
+        expect { BackgroundNotifier.new(args) }.to raise_error(/less than total objects/)
+      end
+    end
+
+    context 'when current_object is negative' do
+      let(:args) { {channel: channel, event: event, action: 'imported', resource: 'effort', current_object: -1, total_objects: 10} }
+
+      it 'raises an ArgumentError' do
+        expect { BackgroundNotifier.new(args) }.to raise_error(/cannot be a negative number/)
+      end
+    end
   end
 
   describe '#publish' do
@@ -42,6 +58,18 @@ RSpec.describe BackgroundNotifier do
         notifier = BackgroundNotifier.new(args)
         expected = {message: 'Imported 10 of 50 efforts', current_object: 10, total_objects: 50,
                     progress: 20, action: 'imported', resource: 'effort'}
+        expect(Pusher).to receive(:trigger).with(channel, event, expected)
+        notifier.publish
+      end
+    end
+
+    context 'when current_object and total_objects provided are each 0' do
+      let(:args) { {channel: channel, event: event, action: 'imported', resource: 'effort', current_object: 0, total_objects: 0} }
+
+      it 'creates a message and a progress percentage of 100' do
+        notifier = BackgroundNotifier.new(args)
+        expected = {message: 'Imported 0 of 0 efforts', current_object: 0, total_objects: 0,
+                    progress: 100, action: 'imported', resource: 'effort'}
         expect(Pusher).to receive(:trigger).with(channel, event, expected)
         notifier.publish
       end

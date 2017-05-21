acd39f22ae7cbc05d24505e4c2cb4cd2dd6b940b
moveson <mark.oveson@gmail.com>
2017-05-21 14:35:41 -0600

    Update SplitParameters.permitted to allow distance, vert_gain, and vert_loss.

--- app/parameters/split_parameters.rb
+++ app/parameters/split_parameters.rb
@@ -1,10 +1,9 @@
 class SplitParameters < BaseParameters
 
   def self.permitted
-    [:id, :course_id, :split_id, :distance_from_start, :distance_in_preferred_units, :vert_gain_from_start,
-     :vert_gain_in_preferred_units, :vert_loss_from_start, :vert_loss_in_preferred_units, :kind, :base_name,
-     :description, :sub_split_bitmap, :latitude, :longitude, :elevation, :elevation_as_entered,
-     :name_extensions]
+    [:id, :course_id, :split_id, :distance_from_start, :distance, :vert_gain_from_start, :vert_gain,
+     :vert_loss_from_start, :vert_loss, :kind, :base_name, :description, :sub_split_bitmap, :latitude, :longitude,
+     :elevation, :elevation_as_entered, :name_extensions]
   end
 
   def self.csv_attributes

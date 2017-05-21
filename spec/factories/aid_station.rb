648aa6cdd02ecd76624dc6a5f6fdef63c0e4937e
Mark Oveson <mark.oveson@gmail.com>
2017-05-21 08:26:57 -0600

    Add aid_station factory and test for uniqueness of aid_station event and split.

---
+++ spec/factories/aid_station.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :aid_station do
+    event
+    split
+  end
+end
--- spec/models/aid_station_spec.rb
+++ spec/models/aid_station_spec.rb
@@ -11,28 +11,39 @@ RSpec.describe AidStation, type: :model do
   let(:wrong_event) { FactoryGirl.build_stubbed(:event, course: wrong_course, name: 'Wrong Event', start_time: '2012-08-08 05:00:00') }
   let(:wrong_split) { FactoryGirl.build_stubbed(:split, course: wrong_course, base_name: 'Wrong Outbound', distance_from_start: 50000) }
 
-  it 'should be valid with an event and a split' do
+  it 'is valid with an event and a split' do
     aid_station = AidStation.new(event: event, split: split)
     expect(aid_station).to be_valid
   end
 
-  it 'should be invalid without an event' do
+  it 'is invalid without an event' do
     aid_station = AidStation.new(split: split)
     expect(aid_station).not_to be_valid
     expect(aid_station.errors[:event_id]).to include("can't be blank")
   end
 
-  it 'should be invalid without a split' do
+  it 'is invalid without a split' do
     aid_station = AidStation.new(event: event)
     expect(aid_station).not_to be_valid
     expect(aid_station.errors[:split_id]).to include("can't be blank")
   end
 
-  it 'should be invalid if event course and split course are inconsistent' do
+  it 'is invalid if event course and split course are inconsistent' do
     aid_station = AidStation.new(event: event, split: wrong_split)
     expect(aid_station).not_to be_valid
     expect(aid_station.errors[:split_id]).to include("event's course is not the same as split's course")
     expect(aid_station.errors[:event_id]).to include("event's course is not the same as split's course")
   end
 
+  context 'when an aid_station with the same event and split already exists' do
+    let(:existing_course) { create(:course) }
+    let(:existing_event) { create(:event, course: existing_course) }
+    let(:existing_split) { create(:split, course: existing_course) }
+
+    it 'does not allow for duplicate records with the same course and split' do
+      create(:aid_station, event: existing_event, split: existing_split)
+      aid_station = build(:aid_station, event: existing_event, split: existing_split)
+      expect(aid_station).not_to be_valid
+    end
+  end
 end

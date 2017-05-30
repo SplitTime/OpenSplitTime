31df27d3a2b502e3177f1e37c5cea08bd4aeb5db
Mark Oveson <mark.oveson@gmail.com>
2017-05-29 21:45:24 -0600

    Add constructor to DataImport::Importer and convert class methods to instance methods; add tests to DataImport::Loader spec.

--- app/services/data_import/importer.rb
+++ app/services/data_import/importer.rb
@@ -1,7 +1,21 @@
 module DataImport
   class Importer
 
-    def self.import(file_path, source, options = {})
+    REPORT_ARRAYS = [:valid_records, :invalid_records, :destroyed_records, :discarded_records, :errors]
+    attr_reader *REPORT_ARRAYS
+
+    def initialize(file_path, source, options = {})
+      @file_path = file_path
+      @source = source
+      @options = options
+      @valid_records = []
+      @invalid_records = []
+      @destroyed_records = []
+      @discarded_records = []
+      @errors = []
+    end
+
+    def import
       case source
       when :race_result
         import_with(file_path, RaceResult::ReadStrategy, RaceResult::ParseStrategy, RaceResult::TransformStrategy, options)
@@ -12,23 +26,30 @@ module DataImport
       end
     end
 
-    def self.import_with(file_path, read_strategy, parse_strategy, transform_strategy, options)
+    private
+
+    attr_reader :file_path, :source, :options
+
+    def import_with(file_path, read_strategy, parse_strategy, transform_strategy, options)
       reader = DataImport::Reader.new(file_path, read_strategy)
       raw_data = reader.read_file
-      return reader.errors if reader.errors.present?
+      self.errors += reader.errors and return if reader.errors.present?
 
       parser = DataImport::Parser.new(raw_data, parse_strategy, options)
       parsed_structs = parser.parse
-      return parser.errors if parser.errors.present?
+      self.errors += parser.errors and return if parser.errors.present?
 
       transformer = DataImport::Transformer.new(parsed_structs, transform_strategy, options)
       proto_records = transformer.transform
-      return transformer.errors if transformer.errors.present?
+      self.errors += transformer.errors and return if transformer.errors.present?
 
       proto_record_groups = options[:strict] ? proto_records : proto_records.map { |record| [record] }
       proto_record_groups.each do |proto_record_group|
         loader = DataImport::Loader.new(proto_record_group, options)
         loader.load_records
+        REPORT_ARRAYS.each do |group|
+          loader.send(group).each { |element| send(group) << element }
+        end
       end
     end
   end
--- spec/services/data_import/loader_spec.rb
+++ spec/services/data_import/loader_spec.rb
@@ -5,67 +5,123 @@ RSpec.describe DataImport::Loader do
   let(:splits) { event.ordered_splits }
   let(:split_ids) { splits.ids }
 
-  let(:transformed_data) { [
+  let(:valid_proto_records) { [
       ProtoRecord.new(record_type: :effort, age: '39', gender: 'male', bib_number: '5',
                       first_name: 'Jatest', last_name: 'Schtest', event_id: event.id, concealed: true,
-                      children: [ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[0], bitkey: 1, time_from_start: 0.0),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[1], bitkey: 1, time_from_start: 2581.36),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[2], bitkey: 1, time_from_start: 6308.86),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[3], bitkey: 1, time_from_start: 9463.56),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[4], bitkey: 1, time_from_start: 13571.37),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[5], bitkey: 1, time_from_start: 16655.3),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[6], bitkey: 1, time_from_start: 17736.45)]),
+                      children: [ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[0], sub_split_bitkey: 1, time_from_start: 0.0),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[1], sub_split_bitkey: 1, time_from_start: 2581.36),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[2], sub_split_bitkey: 1, time_from_start: 6308.86),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[3], sub_split_bitkey: 1, time_from_start: 9463.56),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[4], sub_split_bitkey: 1, time_from_start: 13571.37),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[5], sub_split_bitkey: 1, time_from_start: 16655.3),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[6], sub_split_bitkey: 1, time_from_start: 17736.45)]),
       ProtoRecord.new(record_type: :effort, age: '31', gender: 'female', bib_number: '661',
                       first_name: 'Castest', last_name: 'Pertest', event_id: event.id, concealed: true,
-                      children: [ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[0], bitkey: 1, time_from_start: 0.0),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[1], bitkey: 1, time_from_start: 4916.63),
-                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[2], bitkey: 1, time_from_start: 14398.48),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], bitkey: 1, time_from_start: nil)]),
+                      children: [ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[0], sub_split_bitkey: 1, time_from_start: 0.0),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[1], sub_split_bitkey: 1, time_from_start: 4916.63),
+                                 ProtoRecord.new(record_type: :split_time, lap: 1, split_id: split_ids[2], sub_split_bitkey: 1, time_from_start: 14398.48),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], sub_split_bitkey: 1, time_from_start: nil)]),
       ProtoRecord.new(record_type: :effort, age: '35', gender: 'female', bib_number: '633',
                       first_name: 'Mictest', last_name: 'Hintest', event_id: event.id, concealed: true,
-                      children: [ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[0], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[1], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[2], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], bitkey: 1, time_from_start: nil)]),
+                      children: [ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[0], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[1], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[2], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], sub_split_bitkey: 1, time_from_start: nil)])
+  ] }
+
+  let(:invalid_proto_record) { [
       ProtoRecord.new(record_type: :effort, age: '0', gender: '', bib_number: '62',
                       first_name: 'N.n.', last_name: '62', event_id: event.id, concealed: true,
-                      children: [ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[0], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[1], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[2], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], bitkey: 1, time_from_start: nil),
-                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], bitkey: 1, time_from_start: nil)])
-  ]}
+                      children: [ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[0], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[1], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[2], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[3], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[4], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[5], sub_split_bitkey: 1, time_from_start: nil),
+                                 ProtoRecord.new(record_type: :split_time, record_action: :destroy, lap: 1, split_id: split_ids[6], sub_split_bitkey: 1, time_from_start: nil)])
+  ] }
+  let(:all_proto_records) { valid_proto_records + invalid_proto_record }
   let(:options) { {event: event} }
-  subject { DataImport::Loader.new(transformed_data, options) }
 
   describe '#load_records' do
-    it 'creates new records of the parent class and accurately saves attributes' do
-      efforts = Effort.all
-      expect(efforts.size).to eq(0)
-      subject.load_records
-      expect(efforts.size).to eq(3)
-      expect(efforts.map(&:first_name)).to eq(%w(Jatest Castest Mictest))
-      expect(efforts.map(&:bib_number)).to eq([5, 661, 633])
-      expect(efforts.map(&:gender)).to eq(%w(male female female))
-      expect(efforts.map(&:event_id)).to eq([event.id] * efforts.size)
+    context 'when all provided records are valid and none previously exists' do
+      subject { DataImport::Loader.new(valid_proto_records, options) }
+
+      it 'creates new records of the parent class and accurately saves attributes' do
+        efforts = Effort.all
+        expect(efforts.size).to eq(0)
+        subject.load_records
+        expect(efforts.size).to eq(3)
+        expect(efforts.map(&:first_name)).to eq(%w(Jatest Castest Mictest))
+        expect(efforts.map(&:bib_number)).to eq([5, 661, 633])
+        expect(efforts.map(&:gender)).to eq(%w(male female female))
+        expect(efforts.map(&:event_id)).to eq([event.id] * efforts.size)
+      end
+
+      it 'accurately saves new child records and accurately saves attributes' do
+        split_times = SplitTime.all
+        expect(split_times.size).to eq(0)
+        subject.load_records
+        expect(split_times.size).to eq(10)
+        expect(split_times.map(&:split_id)).to eq(split_ids.cycle.first(split_times.size))
+        expect(split_times.map(&:time_from_start)).to eq([0.0, 2581.36, 6308.86, 9463.56, 13571.37, 16655.3, 17736.45, 0.0, 4916.63, 14398.48])
+        expect(split_times.map(&:effort_id)).to eq([Effort.first.id] * 7 + [Effort.second.id] * 3)
+      end
+
+      it 'returns saved records in the valid_records array' do
+        subject.load_records
+        expect(subject.valid_records.size).to eq(13)
+      end
     end
 
-    it 'accurately saves new child records and accurately saves attributes' do
-      split_times = SplitTime.all
-      expect(split_times.size).to eq(0)
-      subject.load_records
-      expect(split_times.size).to eq(10)
-      expect(split_times.map(&:split_id)).to eq(split_ids.cycle.first(split_times.size))
-      expect(split_times.map(&:time_from_start)).to eq([0.0, 2581.36, 6308.86, 9463.56, 13571.37, 16655.3, 17736.45, 0.0, 4916.63, 14398.48])
-      expect(split_times.map(&:effort_id)).to eq([Effort.first.id] * 7 + [Effort.second.id] * 3)
+    context 'when one or more records exists (as determined by a unique key defined in a Parameters class)' do
+      let(:first_child) { valid_proto_records.first.children.first }
+      let(:second_child) { valid_proto_records.first.children.second }
+
+      before do
+        existing_effort = create(:effort, event: event, bib_number: valid_proto_records.first[:bib_number])
+        create(:split_time, effort: existing_effort, lap: first_child[:lap], split_id: first_child[:split_id],
+               bitkey: first_child[:sub_split_bitkey], time_from_start: 0)
+        create(:split_time, effort: existing_effort, lap: second_child[:lap], split_id: second_child[:split_id],
+               bitkey: second_child[:sub_split_bitkey], time_from_start: 1000)
+      end
+
+      subject { DataImport::Loader.new(valid_proto_records, options) }
+
+      it 'updates existing records and creates new records for non-existing records' do
+        expect(Effort.all.size).to eq(1)
+        expect(SplitTime.all.size).to eq(2)
+        subject.load_records
+        expect(Effort.all.size).to eq(3)
+        expect(SplitTime.all.size).to eq(10)
+        expect(Effort.first.split_times.pluck(:time_from_start))
+            .to eq([0.0, 2581.36, 6308.86, 9463.56, 13571.37, 16655.3, 17736.45])
+      end
+    end
+
+    context 'when any provided record is invalid' do
+      subject { DataImport::Loader.new(all_proto_records, options) }
+
+      it 'does not create any records of the parent or child class' do
+        subject.load_records
+        expect(Effort.all.size).to eq(0)
+        expect(SplitTime.all.size).to eq(0)
+      end
+
+      it 'includes invalid records and errors in the invalid_records and errors attributes' do
+        subject.load_records
+        expect(subject.invalid_records.size).to eq(1)
+        expect(subject.invalid_records.first.first_name).to eq('N.n.')
+        expect(subject.errors.size).to eq(1)
+        expect(subject.errors.first[:title]).to match(/Effort could not be saved/)
+        expect(subject.errors.first[:detail][:messages]).to include("Gender can't be blank")
+      end
     end
   end
 end

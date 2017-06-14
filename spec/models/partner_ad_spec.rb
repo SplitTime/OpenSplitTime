a3e55665c0c619f1a0125536fc23d139644ca7d4
moveson <mark.oveson@gmail.com>
2017-06-13 21:37:50 -0600

    Update PartnerAd spec and factory.

--- spec/models/partner_ad_spec.rb
+++ spec/models/partner_ad_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-# t.integer  "event_id"
-# t.string   "image"
-# t.string   "link"
-# t.integer  "weight"
+# t.integer  "event_id",                        null: false
+# t.string   "link",                            null: false
+# t.integer  "weight",              default: 1, null: false
+# t.datetime "created_at",                      null: false
+# t.datetime "updated_at",                      null: false
+# t.string   "banner_file_name"
+# t.string   "banner_content_type"
+# t.integer  "banner_file_size"
+# t.datetime "banner_updated_at"
 
 RSpec.describe PartnerAd, type: :model do
-  it { is_expected.to strip_attribute(:image).collapse_spaces }
   it { is_expected.to strip_attribute(:link).collapse_spaces }
 
   describe '#initialize' do
-    it 'initializes with an event_id, an image URL, a link URL, and a weight' do
+    it 'initializes with an event_id, a link URL, and a weight' do
       expect { create(:partner_ad) }.not_to raise_error
     end
   end

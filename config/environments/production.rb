ddf7552d7ac374ceff221c319fa40acfa3c5a91d
moveson <mark.oveson@gmail.com>
2017-06-13 21:50:43 -0600

    Fix bug in config/environments/production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
           bucket: ENV['S3_BUCKET'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-          s3_region: ENV['us-west-2'],
+          s3_region: ENV['AWS_REGION'],
           s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
       }
   }

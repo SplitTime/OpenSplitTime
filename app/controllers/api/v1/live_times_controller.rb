9521c0881b437aa8c1b8c349b256f02864f54a2b
Mark Oveson <mark.oveson@gmail.com>
2017-07-05 07:40:35 -0600

    Paginate Api::V1::LiveTimesController#pull; update spec.

--- app/controllers/api/v1/live_times_controller.rb
+++ app/controllers/api/v1/live_times_controller.rb
@@ -5,8 +5,10 @@ class Api::V1::LiveTimesController < ApiController
     event = Event.friendly.find(params[:staging_id])
     authorize event
 
-    live_times = event.live_times.unconsidered.order(:split_id, :bib_number, :bitkey)
-    render json: live_times
+    force_pull = false
+    scoped_live_times = force_pull ? event.live_times.unmatched : event.live_times.unconsidered
+    live_times = scoped_live_times.order(:split_id, :bib_number, :bitkey)
+    paginate json: live_times
     live_times.update_all(pulled_by: current_user.id, pulled_at: Time.current)
   end
 end

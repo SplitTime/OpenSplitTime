387245a4aa02f535372a4aa5751f46650e4252c3
Mark Oveson <mark.oveson@gmail.com>
2017-07-18 21:05:44 -0600

    Fix bug in LiveEffortData so that for "not found" bib numbers, #response_row returns the bib number instead of nil.

--- app/view_models/live_effort_data.rb
+++ app/view_models/live_effort_data.rb
@@ -34,7 +34,7 @@ class LiveEffortData
      split_name: subject_split.base_name,
      split_distance: subject_lap_split.distance_from_start,
      effort_id: effort.id,
-     bib_number: effort.bib_number,
+     bib_number: effort.bib_number || params[:bib_number],
      effort_name: effort_name,
      dropped_here: stopped_here?,
      live_time_id_in: params[:live_time_id_in],
@@ -88,7 +88,7 @@ class LiveEffortData
   end
 
   def effort_name
-    effort&.full_name.presence || (params[:bib_number].present? ? 'Bib number not located' : 'n/a')
+    effort&.full_name.presence || (params[:bib_number].present? ? '[Bib not found]' : 'n/a')
   end
 
   def stopped_here?

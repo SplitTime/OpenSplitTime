3904d414f4faa35e614ed7022991ea2fbe4e0431
moveson <mark.oveson@gmail.com>
2017-07-21 11:02:23 -0600

    Add ability to change auto_live_times from events/_form partial; update EventParameters to include auto_live_times.

--- app/parameters/event_parameters.rb
+++ app/parameters/event_parameters.rb
@@ -2,7 +2,7 @@ class EventParameters < BaseParameters
 
   def self.permitted
     [:id, :course_id, :organization_id, :name, :start_time, :concealed,
-     :available_live, :beacon_url, :laps_required, :staging_id]
+     :available_live, :beacon_url, :laps_required, :staging_id, :auto_live_times]
   end
 
   def self.permitted_query

7c73fd8cc7353337aab91df5d9eb289135acb7eb
moveson <mark.oveson@gmail.com>
2017-07-24 17:34:15 -0600

    Change EffortSplitData from an OpenStruct to a class and add #<=> method with spec.

--- app/models/effort_split_data.rb
+++ app/models/effort_split_data.rb
@@ -1,1 +1,14 @@
-EffortSplitData = OpenStruct
+class EffortSplitData
+  attr_reader :effort_slug, :lap_name, :split_name, :days_and_times
+
+  def initialize(args)
+    @effort_slug = args[:effort_slug]
+    @lap_name = args[:lap_name]
+    @split_name = args[:split_name]
+    @days_and_times = args[:days_and_times]
+  end
+
+  def <=>(other)
+    (days_and_times.compact.first&.to_i || 0) <=> (other.days_and_times.compact.first&.to_i || 0)
+  end
+end
---
+++ spec/models/effort_split_data_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe EffortSplitData, type: :model do
+  subject { EffortSplitData.new(days_and_times: days_and_times) }
+  let(:other) { EffortSplitData.new(days_and_times: days_and_times_other) }
+
+  describe '#<=>' do
+    let(:effort_split_data_objects) { [subject, other] }
+
+    context 'when the other object contains a later first day_and_time' do
+      let(:days_and_times) { [DateTime.parse('2017-07-01 06:00:00'), DateTime.parse('2017-07-01 06:30:00')] }
+      let(:days_and_times_other) { [DateTime.parse('2017-07-01 06:15:00'), DateTime.parse('2017-07-01 06:45:00')] }
+
+      it 'sorts effort_split_data objects based on the first available time contained in days_and_times' do
+        expect(effort_split_data_objects.sort).to eq([subject, other])
+      end
+    end
+
+    context 'when the other object contains an earlier first day_and_time' do
+      let(:days_and_times) { [DateTime.parse('2017-07-01 06:00:00'), DateTime.parse('2017-07-01 06:30:00')] }
+      let(:days_and_times_other) { [DateTime.parse('2017-07-01 05:15:00'), DateTime.parse('2017-07-01 06:45:00')] }
+
+      it 'sorts effort_split_data objects based on the first available time contained in days_and_times' do
+        expect(effort_split_data_objects.sort).to eq([other, subject])
+      end
+    end
+
+    context 'when the other object contains no first day_and_time but an earlier second day_and_time' do
+      let(:days_and_times) { [DateTime.parse('2017-07-01 06:00:00'), DateTime.parse('2017-07-01 06:30:00')] }
+      let(:days_and_times_other) { [nil, DateTime.parse('2017-07-01 05:45:00')] }
+
+      it 'sorts effort_split_data objects based on the first available time contained in days_and_times' do
+        expect(effort_split_data_objects.sort).to eq([other, subject])
+      end
+    end
+
+    context 'when the other object contains no day_and_time' do
+      let(:days_and_times) { [DateTime.parse('2017-07-01 06:00:00'), DateTime.parse('2017-07-01 06:30:00')] }
+      let(:days_and_times_other) { [nil, nil] }
+
+      it 'sorts substituting zero for the empty array' do
+        expect(effort_split_data_objects.sort).to eq([other, subject])
+      end
+    end
+  end
+end

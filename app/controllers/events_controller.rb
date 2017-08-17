b39e3b99cb9ee5d10a131001cb66dcd07614325e
moveson <mark.oveson@gmail.com>
2017-08-17 14:58:08 -0600

    Remove extraneous referral code in EventsController#destroy.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -56,8 +56,7 @@ class EventsController < ApplicationController
     authorize @event
     @event.destroy
 
-    session[:return_to] = params[:referrer_path] if params[:referrer_path]
-    redirect_to session.delete(:return_to) || events_path
+    redirect_to events_path
   end
 
   def find_problem_effort

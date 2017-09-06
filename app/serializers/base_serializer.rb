1d916193566b22def0b9fd8e1398bb393a08e5ab
moveson <mark.oveson@gmail.com>
2017-09-05 20:07:12 -0600

    Make email, phone, and birthdate attributes conditional in EffortSerializer and PersonSerializer, such that they show only if the current user is properly authorized.

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -3,4 +3,8 @@ class BaseSerializer < ActiveModel::Serializer
   def editable
     Pundit.policy!(current_user, object).update?
   end
+
+  def show_personal_info?
+    scope.authorized_to_edit?(object)
+  end
 end
--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,5 +1,7 @@
 class PersonSerializer < BaseSerializer
   attributes :id, :first_name, :last_name, :full_name, :gender, :current_age, :city, :state_code, :country_code
+  %i[phone email birthdate].each { |att| attribute att, if: :show_personal_info? }
+
   link(:self) { api_v1_person_path(object) }
 
   has_many :efforts

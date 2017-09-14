e095dbb7b9d6422ed759aaf0c73e5694cedc839a
moveson <mark.oveson@gmail.com>
2017-09-14 11:43:32 -0600

    Add String#phrase_in_common and spec.

--- spec/lib/core_ext/string_spec.rb
+++ spec/lib/core_ext/string_spec.rb
@@ -41,6 +41,118 @@ describe String do
     end
   end
 
+  describe '#phrase_in_common' do
+    subject { string.phrase_in_common(other) }
+
+    context 'when the two strings have one word in common at the start of the string' do
+      let(:string) { 'hello world' }
+      let(:other) { 'hello curl' }
+
+      it 'returns the common word' do
+        expect(subject).to eq('hello')
+      end
+    end
+
+    context 'when the two strings have multiple words in common at the start of the string' do
+      let(:string) { 'hello there world how are you?' }
+      let(:other) { 'hello there world what is happening?' }
+
+      it 'returns the common phrase' do
+        expect(subject).to eq('hello there world')
+      end
+    end
+
+    context 'when the two strings match but for inconsistent case' do
+      let(:string) { 'Hello World' }
+      let(:other) { 'hello world how are you'}
+      
+      it 'matches case-insensitive and returns the result using capitalization of the subject string' do
+        expect(subject).to eq('Hello World')
+      end
+    end
+
+    context 'when the two strings include a matching phrase at the end of the string' do
+      let(:string) { 'Race of the Century' }
+      let(:other) { 'Kids Race of the Century'}
+
+      it 'returns the matching phrase' do
+        expect(subject).to eq('Race of the Century')
+      end
+    end
+
+    context 'when the two strings include a matching phrase in the middle of the string' do
+      let(:string) { 'Adult Race of the Century Get Ready' }
+      let(:other) { 'Kids Race of the Century They Are So Cute'}
+
+      it 'returns the matching phrase' do
+        expect(subject).to eq('Race of the Century')
+      end
+    end
+
+    context 'when the two strings include numbers' do
+      let(:string) { '2017 Rattlesnake Ramble' }
+      let(:other) { '2017 Rattlesnake Ramble Kids Race'}
+
+      it 'includes the numbers' do
+        expect(subject).to eq('2017 Rattlesnake Ramble')
+      end
+    end
+
+    context 'when the two strings include distance designations that differ' do
+      let(:string) { '2017 Double Dirty 30 100K' }
+      let(:other) { '2017 Double Dirty 30 55K'}
+
+      it 'returns the expected result' do
+        expect(subject).to eq('2017 Double Dirty 30')
+      end
+    end
+
+    context 'when the longest matching phrase is in the middle between shorter matching phrases' do
+      let(:string) { 'Hello world, please come to my house next week' }
+      let(:other) { 'Hello friends, please come to my party next week' }
+
+      it 'returns the longest matching phrase' do
+        expect(subject).to eq('please come to my')
+      end
+    end
+
+    context 'when the two strings have no words in common' do
+      let(:string) { 'hello' }
+      let(:other) { 'world' }
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'when self is an empty string' do
+      let(:string) { '' }
+      let(:other) { 'hello world' }
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'when other is an empty string' do
+      let(:string) { 'hello world' }
+      let(:other) { '' }
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'when both subject and other are empty strings' do
+      let(:string) { '' }
+      let(:other) { '' }
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+  end
+
   describe '#uuid?' do
     it 'returns true when the object is a valid UUID v4' do
       expect(SecureRandom.uuid.uuid?).to eq(true)

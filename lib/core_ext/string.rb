d6122927693b611057eaf0f299f9752a56e86d53
moveson <mark.oveson@gmail.com>
2017-09-15 01:47:47 -0600

    Replace String#longest_common_phrase (formerly String#phrase_in_common) with String.longest_common_phrase (which has the ability to compare several strings); update spec.

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -17,12 +17,11 @@ class String
     (self =~ /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i).present?
   end
 
-  def longest_common_phrase(other)
-    n = [size, other&.size || 0].min
+  def self.longest_common_phrase(strings)
+    n = strings.min_by { |string| string.split.size }.size
     while n > 0 do
-      other_words = other.downcase.split.each_cons(n)
-      string_words = split.each_cons(n)
-      matching_words = string_words.find { |cons_words| other_words.include?(cons_words.map(&:downcase)) }
+      cons_word_sets = strings.map { |string| string.downcase.split.each_cons(n).to_a }
+      matching_words = cons_word_sets.reduce(:&).first
       return matching_words&.join(' ') if matching_words
       n -= 1
     end

682c656954cf504bf85cb4c24f64b06d8c686440
moveson <mark.oveson@gmail.com>
2017-09-16 16:37:08 -0600

    Extract CoursesHelper.link_to_best_efforts_gender from courses/best_efforts view.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,11 @@
 module CoursesHelper
+  def link_to_best_efforts_gender(view_object, gender)
+    link_to gender.titlecase, best_efforts_course_path(view_object,
+                                                 split1: view_object.begin_id,
+                                                 split2: view_object.end_id,
+                                                 'filter[gender]' => gender,
+                                                 'filter[search]' => view_object.search_text),
+            disabled: view_object.gender_text == gender,
+            class: 'btn btn-sm btn-primary'
+  end
 end

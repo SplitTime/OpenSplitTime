de1df6298d8777e023e2850195b3113873d8fb70
moveson <mark.oveson@gmail.com>
2017-09-20 16:51:02 -0600

    Replace concealed, available_live, auto_live_times, organization_id, and organization methods to Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -48,28 +48,28 @@ class Event < ApplicationRecord
     where('start_time < ?', Time.now).order(start_time: :desc).first
   end
 
-  # def concealed
-  #   event_group&.concealed
-  # end
-  # alias_method :concealed?, :concealed
-  #
-  # def available_live
-  #   event_group&.available_live
-  # end
-  # alias_method :available_live?, :available_live
-  #
-  # def auto_live_times
-  #   event_group&.auto_live_times
-  # end
-  # alias_method :auto_live_times?, :auto_live_times
-  #
-  # def organization
-  #   event_group&.organization
-  # end
-  #
-  # def organization_id
-  #   event_group&.organization_id
-  # end
+  def concealed
+    event_group&.concealed
+  end
+  alias_method :concealed?, :concealed
+
+  def available_live
+    event_group&.available_live
+  end
+  alias_method :available_live?, :available_live
+
+  def auto_live_times
+    event_group&.auto_live_times
+  end
+  alias_method :auto_live_times?, :auto_live_times
+
+  def organization
+    event_group&.organization
+  end
+
+  def organization_id
+    event_group&.organization_id
+  end
 
   def events_within_group
     event_group&.events

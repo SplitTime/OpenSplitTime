0fa747838b0e3d00bf7285876175c0b0fb5232cb
moveson <mark.oveson@gmail.com>
2017-10-12 06:34:02 -0600

    Update spec for user_visits_organizations_index; add spec for user_visits_event_index and user_searches_for_event.

---
+++ spec/features/user_searches_for_event_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+feature 'User searches for an event' do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+  let!(:visible_event_1) { create(:event, event_group: visible_event_group) }
+  let!(:visible_event_2) { create(:event, event_group: visible_event_group) }
+  let!(:concealed_event) { create(:event, event_group: concealed_event_group) }
+  let(:visible_event_group) { create(:event_group, concealed: false) }
+  let(:concealed_event_group) { create(:event_group, concealed: true) }
+
+  scenario 'The user is a visitor searching for a visible event' do
+    visit events_path
+    fill_in 'Event name', with: visible_event_1.name
+    click_button 'Find an event'
+
+    expect(page).to have_content(visible_event_1.name)
+    expect(page).not_to have_content(visible_event_2.name)
+    expect(page).not_to have_content(concealed_event.name)
+  end
+
+  scenario 'The user is a visitor searching for a concealed event' do
+    visit events_path
+    fill_in 'Event name', with: concealed_event.name
+    click_button 'Find an event'
+
+    expect(page).not_to have_content(visible_event_1.name)
+    expect(page).not_to have_content(visible_event_2.name)
+    expect(page).not_to have_content(concealed_event.name)
+  end
+
+  scenario 'The user is a user searching for a visible event' do
+    login_as user, scope: :user
+
+    visit events_path
+    fill_in 'Event name', with: visible_event_1.name
+    click_button 'Find an event'
+
+    expect(page).to have_content(visible_event_1.name)
+    expect(page).not_to have_content(visible_event_2.name)
+    expect(page).not_to have_content(concealed_event.name)
+  end
+
+  scenario 'The user is a user searching for a concealed event that is not visible to the user' do
+    login_as user, scope: :user
+
+    visit events_path
+    fill_in 'Event name', with: concealed_event.name
+    click_button 'Find an event'
+
+    expect(page).not_to have_content(visible_event_1.name)
+    expect(page).not_to have_content(visible_event_2.name)
+    expect(page).not_to have_content(concealed_event.name)
+  end
+
+  scenario 'The user is a user searching for a concealed event that is visible to the user' do
+    login_as admin, scope: :user
+
+    visit events_path
+    fill_in 'Event name', with: concealed_event.name
+    click_button 'Find an event'
+
+    expect(page).not_to have_content(visible_event_1.name)
+    expect(page).not_to have_content(visible_event_2.name)
+    expect(page).to have_content(concealed_event.name)
+  end
+end
--- spec/features/user_visits_organizations_index_spec.rb
+++ spec/features/user_visits_organizations_index_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 feature 'User visits organizations index' do
   let(:user) { create(:user) }
   let(:owner) { create(:user) }
+  let(:steward) { create(:user) }
   let(:admin) { create(:admin) }
   let!(:visible_organization) { create(:organization, concealed: false) }
   let!(:concealed_organization) { create(:organization, concealed: true, created_by: owner.id) }
+  before do
+    concealed_organization.stewards << steward
+  end
 
   scenario 'The user is a visitor' do
     visit organizations_path
@@ -33,8 +37,17 @@ feature 'User visits organizations index' do
     expect(page).to have_content(concealed_organization.name)
   end
 
+  scenario 'The user is a non-admin user that is a steward of the concealed organization' do
+    login_as steward, scope: :user
+    visit organizations_path
+
+    expect(page).to have_content('Organizations')
+    expect(page).to have_content(visible_organization.name)
+    expect(page).to have_content(concealed_organization.name)
+  end
+
   scenario 'The user is an admin user' do
-    login_as create(:admin), scope: :user
+    login_as admin, scope: :user
     visit organizations_path
 
     expect(page).to have_content('Organizations')

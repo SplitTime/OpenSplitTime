ae78f06e5b1109064e87a64b5556b93c990d1005
Mark Oveson <mark.oveson@gmail.com>
2017-10-15 21:56:51 -0600

    Credit to virtuouscode.com for database_cleaner config file.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,3 +1,5 @@
+# As suggested by http://www.virtuouscode.com/2012/08/31/configuring-database_cleaner-with-rails-rspec-capybara-and-selenium/
+
 RSpec.configure do |config|
 
   config.before(:suite) do

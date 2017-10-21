35eccac277a393ad7c9a9ad13d7e1b12d22679d0
moveson <mark.oveson@gmail.com>
2017-10-21 10:47:55 -0600

    Add live_entry_flow feature test.

---
+++ spec/support/wait_for_css.rb
@@ -0,0 +1,12 @@
+# https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
+# https://coderwall.com/p/aklybw/wait-for-ajax-with-capybara-2-0
+
+module WaitForCSS
+  def wait_for_css
+    sleep(0.5)
+  end
+end
+
+RSpec.configure do |config|
+  config.include WaitForCSS, type: :feature
+end

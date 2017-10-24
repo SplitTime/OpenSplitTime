9bf3cb2393ae1582c1e043873f1c293990b15f20
moveson <mark.oveson@gmail.com>
2017-10-24 02:51:00 -0600

    Add database migrations to test-setup script.

--- db/migrate/20171024075557_add_pg_search_dmetaphone_support_functions.rb
+++ db/migrate/20171024075557_add_pg_search_dmetaphone_support_functions.rb
@@ -1,18 +1,18 @@
 class AddPgSearchDmetaphoneSupportFunctions < ActiveRecord::Migration[5.1]
   def self.up
     say_with_time("Adding support functions for pg_search :dmetaphone") do
-      execute <<-'SQL'
-CREATE OR REPLACE FUNCTION pg_search_dmetaphone(text) RETURNS text LANGUAGE SQL IMMUTABLE STRICT AS $function$
-  SELECT array_to_string(ARRAY(SELECT dmetaphone(unnest(regexp_split_to_array($1, E'\\s+')))), ' ')
-$function$;
+      execute <<~'SQL'
+        CREATE OR REPLACE FUNCTION pg_search_dmetaphone(text) RETURNS text LANGUAGE SQL IMMUTABLE STRICT AS $function$
+          SELECT array_to_string(ARRAY(SELECT dmetaphone(unnest(regexp_split_to_array($1, E'\\s+')))), ' ')
+        $function$;
       SQL
     end
   end
 
   def self.down
     say_with_time("Dropping support functions for pg_search :dmetaphone") do
-      execute <<-'SQL'
-DROP FUNCTION pg_search_dmetaphone(text);
+      execute <<~'SQL'
+        DROP FUNCTION pg_search_dmetaphone(text);
       SQL
     end
   end

33cdcbc030b04d31926776e0558cc23ea61312cb
moveson <mark.oveson@gmail.com>
2017-10-26 19:39:08 -0600

    Tweak BirthdateValidator to reject birthdate == Date.today; update spec.

--- app/models/concerns/birthdate_validator.rb
+++ app/models/concerns/birthdate_validator.rb
@@ -3,8 +3,8 @@ class BirthdateValidator < ActiveModel::Validator
     if record.birthdate.present? && (record.birthdate < '1900-01-01'.to_date)
       record.errors.add(:birthdate, "can't be before 1900")
     end
-    if record.birthdate.present? && (record.birthdate > Date.today)
-      record.errors.add(:birthdate, "can't be in the future")
+    if record.birthdate.present? && (record.birthdate >= Date.today)
+      record.errors.add(:birthdate, "can't be today or in the future")
     end
   end
 end

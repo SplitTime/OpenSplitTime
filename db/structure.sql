1c1c2af0ae7fbd327967cf3a62750411e86ea477
Mark Oveson <mark.oveson@gmail.com>
2017-10-26 23:51:33 -0600

    Add structure.sql containing only function pg_search_metaphone; update app.json.

---
+++ db/structure.sql
@@ -0,0 +1,9 @@
+--
+-- Name: pg_search_dmetaphone(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION pg_search_dmetaphone(text) RETURNS text
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $_$
+  SELECT array_to_string(ARRAY(SELECT dmetaphone(unnest(regexp_split_to_array($1, E'\\s+')))), ' ')
+$_$;

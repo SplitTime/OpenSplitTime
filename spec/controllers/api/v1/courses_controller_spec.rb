896e5354573a9cbc37bef89a64d3fc61401652be
moveson <mark.oveson@gmail.com>
2018-02-28 13:58:34 -0700

    Add JWT testing to Api::V1::CoursesController.

--- spec/controllers/api/v1/courses_controller_spec.rb
+++ spec/controllers/api/v1/courses_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CoursesController do
-  login_admin
-
   let(:course) { create(:course) }
 
   describe '#index' do
+    subject(:make_request) { get :index, params: params }
+    let(:params) { {} }
+
     before do
       create(:course, name: 'Bravo', description: 'Fabulous')
       create(:course, name: 'Charlie', description: 'Beautiful')
@@ -13,119 +14,179 @@ RSpec.describe Api::V1::CoursesController do
       create(:course, name: 'Delta', description: 'Gorgeous')
     end
 
-    it 'returns a successful 200 response' do
-      get :index
-      expect(response.status).to eq(200)
-    end
+    via_login_and_jwt do
+      it 'returns a successful 200 response' do
+        make_request
+        expect(response.status).to eq(200)
+      end
 
-    it 'returns each course that the current_user is authorized to edit' do
-      get :index
-      expect(response.status).to eq(200)
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data'].size).to eq(4)
-      expect(parsed_response['data'].map { |item| item['id'].to_i }).to eq(Course.all.map(&:id))
-    end
+      it 'returns each course that the current_user is authorized to edit' do
+        make_request
+        expect(response.status).to eq(200)
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['data'].size).to eq(4)
+        expect(parsed_response['data'].map { |item| item['id'].to_i }).to eq(Course.all.map(&:id))
+      end
 
-    it 'sorts properly in ascending order based on a provided sort parameter' do
-      expected = %w(Alpha Bravo Charlie Delta)
-      get :index, params: {sort: 'name'}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
-    end
+      context 'when a sort parameter is provided' do
+        let(:params) { {sort: 'name'} }
 
-    it 'sorts properly in descending order based on a provided sort parameter with a minus sign' do
-      expected = %w(Delta Charlie Bravo Alpha)
-      get :index, params: {sort: '-name'}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
-    end
+        it 'sorts properly in ascending order based on the parameter' do
+          make_request
+          expected = %w(Alpha Bravo Charlie Delta)
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
+        end
+      end
 
-    it 'sorts properly on multiple fields' do
-      expected = %w(Alpha Charlie Bravo Delta)
-      get :index, params: {sort: 'description,name'}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
+      context 'when a negative sort parameter is provided' do
+        let(:params) { {sort: '-name'} }
+
+        it 'sorts properly in descending order based on the parameter' do
+          make_request
+          expected = %w(Delta Charlie Bravo Alpha)
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
+        end
+      end
+
+      context 'when multiple sort parameters are provided' do
+        let(:params) { {sort: 'description,name'} }
+
+        it 'sorts properly on multiple fields' do
+          make_request
+          expected = %w(Alpha Charlie Bravo Delta)
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['data'].map { |item| item.dig('attributes', 'name') }).to eq(expected)
+        end
+      end
     end
   end
 
   describe '#show' do
-    it 'returns a successful 200 response' do
-      get :show, params: {id: course}
-      expect(response.status).to eq(200)
-    end
+    subject(:make_request) { get :show, params: params }
 
-    it 'returns data of a single course' do
-      get :show, params: {id: course}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data']['id'].to_i).to eq(course.id)
-      expect(response.body).to be_jsonapi_response_for('courses')
-    end
+    via_login_and_jwt do
+      context 'when an existing course.id is provided' do
+        let(:params) { {id: course} }
 
-    it 'returns an error if the course does not exist' do
-      get :show, params: {id: 0}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['errors']).to include(/not found/)
-      expect(response.status).to eq(404)
+        it 'returns a successful 200 response' do
+          make_request
+          expect(response.status).to eq(200)
+        end
+
+        it 'returns data of a single course' do
+          make_request
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['data']['id'].to_i).to eq(course.id)
+          expect(response.body).to be_jsonapi_response_for('courses')
+        end
+      end
+
+      context 'if the course does not exist' do
+        let(:params) { {id: 0} }
+
+        it 'returns an error' do
+          make_request
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['errors']).to include(/not found/)
+          expect(response.status).to eq(404)
+        end
+      end
     end
   end
 
   describe '#create' do
-    it 'returns a successful json response' do
-      post :create, params: {data: {type: 'courses', attributes: {name: 'Test Course'}}}
-      expect(response.body).to be_jsonapi_response_for('courses')
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['data']['id']).not_to be_nil
-      expect(response.status).to eq(201)
-    end
+    subject(:make_request) { post :create, params: params }
 
-    it 'creates a course record' do
-      expect(Course.all.count).to eq(0)
-      post :create, params: {data: {type: 'courses', attributes: {name: 'Test Course'}}}
-      expect(Course.all.count).to eq(1)
+    via_login_and_jwt do
+      context 'when provided data is valid' do
+        let(:params) { {data: {type: 'courses', attributes: {name: 'Test Course'}}} }
+
+        it 'returns a successful json response' do
+          make_request
+          expect(response.body).to be_jsonapi_response_for('courses')
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['data']['id']).not_to be_nil
+          expect(response.status).to eq(201)
+        end
+
+        it 'creates a course record' do
+          expect(Course.all.count).to eq(0)
+          post :create, params: {data: {type: 'courses', attributes: {name: 'Test Course'}}}
+          expect(Course.all.count).to eq(1)
+        end
+      end
     end
   end
 
   describe '#update' do
+    subject(:make_request) { put :update, params: params }
+    let(:params) { {id: course_id, data: {type: 'courses', attributes: attributes}} }
     let(:attributes) { {name: 'Updated Course Name'} }
 
-    it 'returns a successful json response' do
-      put :update, params: {id: course, data: {type: 'courses', attributes: attributes}}
-      expect(response.body).to be_jsonapi_response_for('courses')
-      expect(response.status).to eq(200)
-    end
+    via_login_and_jwt do
+      context 'when the course exists' do
+        let(:course_id) { course.id }
 
-    it 'updates the specified fields' do
-      put :update, params: {id: course, data: {type: 'courses', attributes: attributes}}
-      course.reload
-      expect(course.name).to eq(attributes[:name])
-    end
+        it 'returns a successful json response' do
+          make_request
+          expect(response.body).to be_jsonapi_response_for('courses')
+          expect(response.status).to eq(200)
+        end
 
-    it 'returns an error if the course does not exist' do
-      put :update, params: {id: 0, data: {type: 'courses', attributes: attributes}}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['errors']).to include(/not found/)
-      expect(response.status).to eq(404)
+        it 'updates the specified fields' do
+          make_request
+          course.reload
+          expect(course.name).to eq(attributes[:name])
+        end
+      end
+
+      context 'when the course does not exist' do
+        let(:course_id) { 0 }
+
+        it 'returns an error if the course does not exist' do
+          make_request
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['errors']).to include(/not found/)
+          expect(response.status).to eq(404)
+        end
+      end
     end
   end
 
   describe '#destroy' do
-    it 'returns a successful json response' do
-      delete :destroy, params: {id: course}
-      expect(response.status).to eq(200)
-    end
+    subject(:make_request) { delete :destroy, params: params }
+    let(:params) { {id: course_id} }
+    let(:course) { create(:course) }
 
-    it 'destroys the course record' do
-      test_course = course
-      expect(Course.all.count).to eq(1)
-      delete :destroy, params: {id: test_course}
-      expect(Course.all.count).to eq(0)
-    end
+    via_login_and_jwt do
+      context 'when the record exists' do
+        let(:course_id) { course.id }
 
-    it 'returns an error if the course does not exist' do
-      delete :destroy, params: {id: 0}
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['errors']).to include(/not found/)
-      expect(response.status).to eq(404)
+        it 'returns a successful json response' do
+          make_request
+          expect(response.status).to eq(200)
+        end
+
+        it 'destroys the course record' do
+          course
+          expect(Course.all.count).to eq(1)
+          make_request
+          expect(Course.all.count).to eq(0)
+        end
+      end
+
+      context 'when the record does not exist' do
+        let(:course_id) { 0 }
+
+        it 'returns an error if the course does not exist' do
+          make_request
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['errors']).to include(/not found/)
+          expect(response.status).to eq(404)
+        end
+      end
     end
   end
 end
---
+++ spec/support/request_spec_helpers.rb
@@ -0,0 +1,55 @@
+module RequestSpecHelpers
+  include Warden::Test::Helpers
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def via_login_and_jwt(&block)
+    %w(login jwt).each do |strategy|
+      context "with #{strategy} strategy" do
+        let(:request_spec_admin) { FactoryBot.create(:admin) }
+
+        case strategy
+        when 'login'
+          login_as_admin
+        when 'jwt'
+          add_jwt_headers
+        else
+          raise RuntimeError, "Strategy #{strategy} is not recognized"
+        end
+        module_eval(&block)
+      end
+    end
+  end
+
+  def add_jwt_headers
+    before { request.headers.merge!(headers) }
+    let(:headers) { {"Authorization" => "bearer #{token}"} }
+    let(:token) { JsonWebToken.encode(sub: request_spec_admin.id) }
+  end
+
+  def login_as_admin
+    before { sign_in request_spec_admin }
+    after { sign_out request_spec_admin }
+  end
+
+  def sign_in(resource)
+    login_as(resource, scope: warden_scope(resource))
+  end
+
+  def sign_out(resource)
+    logout(warden_scope(resource))
+  end
+
+  private
+
+  def warden_scope(resource)
+    resource.class.name.underscore.to_sym
+  end
+
+  RSpec.configure do |config|
+    config.extend self, type: :controller
+  end
+end

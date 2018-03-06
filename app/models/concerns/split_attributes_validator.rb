e17f9e6653a1e3ae54b9b9ed19a591489d096c06
Mark Oveson <mark.oveson@gmail.com>
2018-03-06 08:34:21 -0700

    Clean up SplitAttributesValidator.

--- app/models/concerns/split_attributes_validator.rb
+++ app/models/concerns/split_attributes_validator.rb
@@ -1,14 +1,17 @@
 class SplitAttributesValidator < ActiveModel::Validator
   def validate(split)
-    validate_vert(split)
-    validate_distance(split)
-    validate_location(split)
-    validate_order(split) if split.intermediate?
+    @split = split
+    validate_vert
+    validate_distance
+    validate_location
+    validate_order if split.intermediate?
   end
 
   private
 
-  def validate_vert(split)
+  attr_reader :split
+
+  def validate_vert
     if split.start?
       split.errors.add(:vert_gain_from_start, 'for the start split must be 0') unless split.vert_gain_from_start.nil? || split.vert_gain_from_start.zero?
       split.errors.add(:vert_loss_from_start, 'for the start split must be 0') unless split.vert_loss_from_start.nil? || split.vert_loss_from_start.zero?
@@ -18,7 +21,7 @@ class SplitAttributesValidator < ActiveModel::Validator
     end
   end
 
-  def validate_distance(split)
+  def validate_distance
     if split.start?
       split.errors.add(:distance_from_start, 'for the start split must be 0') unless split.distance_from_start&.zero?
     else
@@ -26,13 +29,13 @@ class SplitAttributesValidator < ActiveModel::Validator
     end
   end
 
-  def validate_location(split)
+  def validate_location
     split.errors.add(:elevation, 'must be between -1000 and 10,000 meters') unless split.elevation.nil? || split.elevation.between?(-1000, 10_000)
     split.errors.add(:latitude, 'must be between -90 and 90') unless split.latitude.nil? || split.latitude.between?(-90, 90)
     split.errors.add(:longitude, 'must be between -180 and 180') unless split.longitude.nil? || split.longitude.between?(-180, 180)
   end
 
-  def validate_order(split)
+  def validate_order
     finish_split = split.course&.finish_split
     finish_split_distance = finish_split&.distance_from_start
     if split.distance_from_start && finish_split_distance && (split.distance_from_start >= finish_split_distance)

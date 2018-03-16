148b9eb4842d9e47c5f3e71cb1de428bf2d7274e
Mark Oveson <mark.oveson@gmail.com>
2018-03-15 21:41:19 -0600

    Remove leftover split_kind attribute from DataEntryNode; fix VCR in system tests.

--- app/models/data_entry_node.rb
+++ app/models/data_entry_node.rb
@@ -5,7 +5,7 @@ class DataEntryNode
 
   DISTANCE_THRESHOLD = 100 # Distance (in meters) below which split locations are deemed equivalent
   NODE_ATTRIBUTES = [:split_name, :sub_split_kind, :label, :latitude, :longitude,
-                     :min_distance_from_start, :split_kind, :event_split_ids, :event_aid_station_ids].freeze
+                     :min_distance_from_start, :event_split_ids, :event_aid_station_ids].freeze
 
   attr_reader *NODE_ATTRIBUTES
 
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,4 +1,5 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr'
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 end

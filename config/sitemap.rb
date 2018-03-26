278e378a2b04d64d8b33cc1486d349619f5c9982
moveson <mark.oveson@gmail.com>
2018-03-26 09:07:31 -0600

    Update sitemap config file to include event_groups and to remove concealed resources.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,24 +5,29 @@ SitemapGenerator::Sitemap.public_path = 'tmp/sitemaps/'
 SitemapGenerator::Sitemap.create do
 
   add organizations_path
+  add event_groups_path
   add events_path
   add people_path
   add about_path
   add getting_started_path
 
-  Organization.find_each do |organization|
+  Organization.visible.find_each do |organization|
     add organization_path(organization), lastmod: organization.updated_at
   end
 
-  Event.find_each do |event|
+  EventGroup.visible.find_each do |event_group|
+    add event_group_path(event_group), lastmod: event_group.updated_at
+  end
+
+  Event.visible.find_each do |event|
     add event_path(event), lastmod: event.updated_at
   end
 
-  Person.find_each do |person|
+  Person.visible.find_each do |person|
     add person_path(person), lastmod: person.updated_at
   end
 
-  Effort.find_each do |effort|
+  Effort.visible.find_each do |effort|
     add person_path(effort), lastmod: effort.updated_at
   end
 end

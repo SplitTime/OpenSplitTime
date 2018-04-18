3dad29e1264688a3f8bf6c7ef344b2563d8854f0
moveson <mark.oveson@gmail.com>
2018-04-17 22:50:59 -0600

    Tune RawTimeQuery.with_relations.

--- app/queries/raw_time_query.rb
+++ app/queries/raw_time_query.rb
@@ -11,20 +11,23 @@ class RawTimeQuery < BaseQuery
            INNER JOIN existing_scope ON existing_scope.id = raw_times.id)
 
     SELECT 
-       r.*
-       , e.id AS effort_id
-       , e.event_id
-       , (SELECT split_id FROM aid_stations a
-          INNER JOIN splits s ON a.split_id = s.id
-          WHERE a.event_id = e.event_id
-          AND s.parameterized_base_name = r.parameterized_split_name
-          limit 1) AS split_id
+     r.id, r.event_group_id, r.parameterized_split_name, r.bib_number
+   , e.id AS effort_id
+   , e.event_id
+   , s.split_id
     FROM raw_times_scoped r
-    LEFT JOIN efforts e ON r.bib_number = e.bib_number::text
-       AND e.event_id IN 
-          (SELECT id 
-           FROM events 
-           WHERE events.event_group_id = r.event_group_id)
+    LEFT JOIN (
+                SELECT ef.id, ef.event_id, ef.bib_number, ev.event_group_id
+                FROM efforts ef
+                INNER JOIN events ev ON ef.event_id = ev.id
+               ) e ON r.bib_number = e.bib_number::text
+                   AND e.event_group_id = r.event_group_id
+    LEFT JOIN LATERAL (
+                SELECT a.split_id FROM aid_stations a
+                INNER JOIN splits s ON a.split_id = s.id
+                WHERE a.event_id = e.event_id
+                AND s.parameterized_base_name = r.parameterized_split_name
+                limit 1) s ON TRUE
     ORDER BY r.bib_number, r.id
     SQL
     query.squish

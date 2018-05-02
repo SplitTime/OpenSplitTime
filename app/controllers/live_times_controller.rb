4128eac1fb17424c78967e7f035a95e20d2e66ea
Mark Oveson <mark.oveson@gmail.com>
2018-05-02 01:51:39 -0600

    Report live times available when live_times are updated.

--- app/controllers/live_times_controller.rb
+++ app/controllers/live_times_controller.rb
@@ -1,10 +1,14 @@
 class LiveTimesController < ApplicationController
+  include BackgroundNotifiable
+
   before_action :set_live_time
 
   def update
     authorize @live_time
 
-    unless @live_time.update(permitted_params)
+    if @live_time.update(permitted_params)
+      report_live_times_available(@live_time.event.event_group)
+    else
       flash[:danger] = "Live time could not be updated.\n#{@live_time.errors.full_messages.join("\n")}"
     end
     redirect_to_event
@@ -24,7 +28,7 @@ class LiveTimesController < ApplicationController
   end
 
   def redirect_to_event
-      redirect_to params[:referrer_path].permit(:action, :controller, :id, :display_style, :sort, :page) ||
+    redirect_to params[:referrer_path].permit(:action, :controller, :id, :display_style, :sort, :page) ||
                     stage_event_path(@live_time.event_id, display_style: :times)
   end
 end

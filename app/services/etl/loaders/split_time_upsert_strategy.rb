ad0bc928fbcf1ad6b009db08cb53f4b988b83743
Mark Oveson <mark.oveson@gmail.com>
2018-06-02 04:09:37 -0600

    Simplify and rework ETL::Loaders::SplitTimeUpsertStrategy.

--- app/services/etl/loaders/split_time_upsert_strategy.rb
+++ app/services/etl/loaders/split_time_upsert_strategy.rb
@@ -11,16 +11,19 @@ module ETL::Loaders
     end
 
     def custom_load
-      proto_records.each do |proto_record|
-        parent_record = fetch_parent(proto_record)
-        child_records = proto_record.children.map { |child_proto_record| child_record_from_proto(child_proto_record, parent_record) }.compact
+      proto_records.each do |proto_effort|
+        effort = find_effort(proto_effort) || Effort.new(proto_effort.to_h)
 
-        child_records.each do |child_record|
-          if parent_record.persisted? && (child_record.new_record? || child_record.changed?)
-            upsert(child_record, parent_record)
+        if effort.persisted?
+          proto_effort.children.each { |proto_split_time| prepare_attributes(proto_split_time, effort) }.compact
+
+          if effort.update(proto_effort.parent_child_attributes)
+            saved_records << effort
           else
-            ignored_records << child_record
+            invalid_records << effort
           end
+        else
+          ignored_records << effort
         end
       end
     end
@@ -29,58 +32,20 @@ module ETL::Loaders
 
     attr_reader :parent_model, :child_model, :parent_key, :child_key
 
-    def fetch_parent(proto_record)
-      fetch_record(proto_record, parent_model, parent_key, parent_model)
-    end
-
-    def fetch_child(proto_record, parent_record)
-      child_scope = parent_record&.send(child_model.model_name.plural) || child_model.none
-      fetch_record(proto_record, child_model, child_key, child_scope)
-    end
-
-    def fetch_record(proto_record, model_class, unique_key, scope)
+    def find_effort(proto_record)
       attributes = proto_record.to_h
-      unique_attributes = attributes.slice(*unique_key)
-      record = unique_key_valid?(unique_key, unique_attributes) ?
-                   scope.find_or_initialize_by(unique_attributes) :
-                   model_class.new
-      record.assign_attributes(attributes)
-      record
+      unique_attributes = attributes.slice(*parent_key)
+      Effort.where(unique_attributes).includes(:split_times).first
     end
 
-    def unique_key_valid?(unique_key, unique_attributes)
-      unique_key.present? && unique_key.size == unique_attributes.size && unique_attributes.values.all?(&:present?)
-    end
+    def prepare_attributes(proto_split_time, effort)
+      time_point = TimePoint.new(proto_split_time[:lap], proto_split_time[:split_id], proto_split_time[:sub_split_bitkey])
+      existing_split_time = effort.split_times.find { |st| st.time_point == time_point }
 
-    def child_record_from_proto(proto_record, parent_record)
-      record = fetch_child(proto_record, parent_record)
-      eliminate(record) and return nil if proto_record.record_action == :destroy
-      record
-    end
-
-    def eliminate(record)
-      if record.new_record?
-        ignored_records << record
-      else
-        begin
-          destroyed_records << record if record.destroy
-        rescue ActiveRecord::ActiveRecordError => exception
-          record.errors.add(exception)
-          invalid_records << record
-        end
-      end
-    end
-
-    def upsert(child_record, parent_record)
-      add_audit_attributes(child_record)
-      save_result = child_record.new_record? ?
-                        parent_record.send(child_record.model_name.plural) << child_record :
-                        child_record.save
-      if save_result
-        saved_records << child_record
-      else
-        invalid_records << child_record
-      end
+      proto_split_time[:id] = existing_split_time.id if existing_split_time&.id
+      proto_split_time[:created_by] = current_user_id unless existing_split_time
+      proto_split_time[:updated_by] = current_user_id
+      proto_split_time[:_destroy] = true if proto_split_time.record_action == :destroy
     end
   end
 end

fdd1a2060eb7c76f292f288b8a203c05996a7951
Abram Early <abram.early@gmail.com>
2018-07-17 20:40:29 -0600

    Moved live entry tabs to dropdown builder

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -6,8 +6,8 @@ module DropdownHelper
 
     content_tag :li, class: main_active do
       a_tag = content_tag :a, class: 'dropdown-toggle', data: {toggle: 'dropdown'} do
-        active_item = items.find { |item| item[:active] }[:name]
-        string = [title, active_item].compact.join(' / ')
+        active_name = items.find { |item| item[:active] }&.dig(:name)
+        string = [title, active_name].compact.join(' / ')
         span = content_tag(:span, '', class: 'caret')
         [string, '&nbsp;', span].join.html_safe
       end
@@ -24,5 +24,34 @@ module DropdownHelper
 
       a_tag + ul_tag
     end
+  end
+
+  def live_dropdown_menu(view_object)
+    build_dropdown_menu('Live', [
+      {
+        name: 'Live Entry',
+        link: live_entry_live_event_group_path(view_object.event_group),
+        active: action_name == 'live_entry',
+        visible: true
+      },
+      {
+        name: 'Drops',
+        link: drop_list_event_path(view_object.event),
+        active: action_name == 'drop_list',
+        visible: true
+      },
+      {
+        name: 'Progress',
+        link: progress_report_live_event_path(view_object.event),
+        active: action_name == 'progress_report',
+        visible: true
+      },
+      {
+        name: 'Aid Stations',
+        link: aid_station_report_live_event_path(view_object.event),
+        active: action_name == 'aid_station_report',
+        visible: true
+      }
+    ])
   end
 end

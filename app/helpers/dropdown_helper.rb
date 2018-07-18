db497d055c94d111fe2f56b94c6833fedb5d8628
moveson <mark.oveson@gmail.com>
2018-07-17 23:15:07 -0600

    Refine DropdownHelper; combine Staging, Roster, and Group into Admin dropdown.

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -17,7 +17,7 @@ module DropdownHelper
       }
 
       concat content_tag(:ul, class: 'dropdown-menu') {
-        items.each do |item|
+        items.select { |item| item[:visible] }.each do |item|
           active = item[:active] ? 'active' : nil
           concat content_tag(:li, class: active) {
             link_to item[:name], item[:link]
@@ -27,56 +27,75 @@ module DropdownHelper
     end
   end
 
+  def admin_dropdown_menu(view_object)
+    build_dropdown_menu('Admin', [
+        {name: 'Staging',
+         link: "#{event_staging_app_path(view_object.event)}#/#{event_staging_app_page(view_object)}",
+         active: action_name == 'app',
+         visible: true},
+        {name: 'Roster',
+         link: roster_event_group_path(view_object.event_group),
+         active: action_name == 'roster',
+         visible: true},
+        {name: 'Settings',
+         link: event_group_path(view_object.event_group, force_settings: true),
+         active: controller_name == 'event_groups' && action_name == 'show',
+         visible: true}
+    ])
+  end
+
   def live_dropdown_menu(view_object)
     build_dropdown_menu('Live', [
-      {
-        name: 'Live Entry',
-        link: live_entry_live_event_group_path(view_object.event_group),
-        active: action_name == 'live_entry',
-        visible: true
-      },
-      {
-        name: 'Drops',
-        link: drop_list_event_path(view_object.event),
-        active: action_name == 'drop_list',
-        visible: true
-      },
-      {
-        name: 'Progress',
-        link: progress_report_live_event_path(view_object.event),
-        active: action_name == 'progress_report',
-        visible: true
-      },
-      {
-        name: 'Aid Stations',
-        link: aid_station_report_live_event_path(view_object.event),
-        active: action_name == 'aid_station_report',
-        visible: true
-      }
+        {name: 'Live Entry',
+         link: live_entry_live_event_group_path(view_object.event_group),
+         active: action_name == 'live_entry',
+         visible: true},
+        {name: 'Drops',
+         link: drop_list_event_path(view_object.event),
+         active: action_name == 'drop_list',
+         visible: true},
+        {name: 'Progress',
+         link: progress_report_live_event_path(view_object.event),
+         active: action_name == 'progress_report',
+         visible: true},
+        {name: 'Aid Stations',
+         link: aid_station_report_live_event_path(view_object.event),
+         active: action_name == 'aid_station_report',
+         visible: true}
     ])
   end
 
   def results_dropdown_menu(view_object)
-    build_dropdown_menu('Results',
-                        [{name: 'List', link: event_path(view_object.event), active: controller_name == 'events' && action_name == 'show', visible: true},
-                         {name: 'Spread', link: spread_event_path(view_object.event), active: action_name == 'spread', visible: true},
-                        {name: 'Podium', link: podium_event_path(view_object.event), active: action_name == 'podium', visible: true}])
+    build_dropdown_menu('Results', [
+        {name: 'List',
+         link: event_path(view_object.event),
+         active: controller_name == 'events' && action_name == 'show',
+         visible: true},
+        {name: 'Spread',
+         link: spread_event_path(view_object.event),
+         active: action_name == 'spread',
+         visible: true},
+        {name: 'Podium',
+         link: podium_event_path(view_object.event),
+         active: action_name == 'podium',
+         visible: true},
+        {name: 'Traffic',
+         link: traffic_event_group_path(view_object.event_group),
+         active: action_name == 'traffic',
+         visible: true}
+    ])
   end
 
   def raw_times_dropdown_menu(view_object)
     build_dropdown_menu('Raw Times', [
-      {
-        name: 'Raw Times',
-        link: raw_times_event_group_path(view_object.event_group),
-        active: action_name == 'raw_times',
-        visible: true
-      },
-      {
-        name: 'Split Raw Times',
-        link: split_raw_times_event_group_path(view_object.event_group),
-        active: action_name == 'split_raw_times',
-        visible: true
-      }
+        {name: 'List',
+         link: raw_times_event_group_path(view_object.event_group),
+         active: action_name == 'raw_times',
+         visible: true},
+        {name: 'Splits',
+         link: split_raw_times_event_group_path(view_object.event_group),
+         active: action_name == 'split_raw_times',
+         visible: true}
     ])
   end
 
@@ -84,22 +103,26 @@ module DropdownHelper
   def check_in_filter_dropdown_menu(items)
     dropdown_items = items.map do |item|
       {
-        name: content_tag(:i, nil, class: "glyphicon glyphicon-#{item[:icon]}") + ' ' + item[:name],
-        link: request.params.merge(
-            checked_in: item[:checked_in],
-            started: item[:started],
-            unreconciled: item[:unreconciled],
-            problem: item[:problem],
-            filter: {search: ''},
-            page: nil
-        ),
-        active: params[:checked_in]&.to_boolean == item[:checked_in] && 
-                params[:started]&.to_boolean == item[:started] && 
-                params[:unreconciled]&.to_boolean == item[:unreconciled] && 
-                params[:problem]&.to_boolean == item[:problem],
-        visible: true
+          name: content_tag(:i, nil, class: "glyphicon glyphicon-#{item[:icon]}") + ' ' + item[:name],
+          link: request.params.merge(
+              checked_in: item[:checked_in],
+              started: item[:started],
+              unreconciled: item[:unreconciled],
+              problem: item[:problem],
+              filter: {search: ''},
+              page: nil
+          ),
+          active: params[:checked_in]&.to_boolean == item[:checked_in] &&
+              params[:started]&.to_boolean == item[:started] &&
+              params[:unreconciled]&.to_boolean == item[:unreconciled] &&
+              params[:problem]&.to_boolean == item[:problem],
+          visible: true
       }
     end
     build_dropdown_menu(nil, dropdown_items, class: 'pull-right', button: true)
+  end
+
+  def event_staging_app_page(view_object)
+    (view_object.respond_to?(:display_style)) && (view_object.display_style == 'splits') ? 'splits' : 'entrants'
   end
 end

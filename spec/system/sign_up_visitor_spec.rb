ac5bb080f33d0dce76d5f0a71d4b9fb8cbf3fdfd
Mark Oveson <mark.oveson@gmail.com>
2018-08-28 23:39:54 -0600

    Update more system tests to find text in new growl-type notifications.

--- spec/system/sign_up_visitor_spec.rb
+++ spec/system/sign_up_visitor_spec.rb
@@ -12,37 +12,37 @@ RSpec.describe 'Visitor signs up' do
   scenario 'without first name' do
     sign_up_with '', 'Example', 'valid@example.com', 'password'
 
-    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content(:all, "First name can't be blank")
   end
 
   scenario 'without last name' do
     sign_up_with 'Joe', '', 'valid@example.com', 'password'
 
-    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content(:all, "Last name can't be blank")
   end
 
   scenario 'with invalid email' do
     sign_up_with 'Joe', 'Example', 'invalid_email', 'password'
 
-    expect(page).to have_content('Email is invalid')
+    expect(page).to have_content(:all, 'Email is invalid')
   end
 
   scenario 'with invalid phone number' do
     sign_up_with 'Joe', 'Example', 'valid@example.com', 'password', '1234'
 
-    expect(page).to have_content('Phone must be a valid US or Canada phone number')
+    expect(page).to have_content(:all, 'Phone must be a valid US or Canada phone number')
   end
 
   scenario 'with blank password' do
     sign_up_with 'Joe', 'Example', 'valid@example.com', ''
 
-    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content(:all, "Password can't be blank")
   end
 
   scenario 'with too short password' do
     sign_up_with 'Joe', 'Example', 'valid@example.com', '1234'
 
-    expect(page).to have_content("Password is too short")
+    expect(page).to have_content(:all, "Password is too short")
   end
 
   def sign_up_with(first_name, last_name, email, password, phone = nil)

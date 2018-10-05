a69caade155d3fab97b0665493384b9a1ead92c2
Mark Oveson <mark.oveson@gmail.com>
2018-10-04 23:04:24 -0600

    Update Gemfile to Bootstrap 4; update application.scss and application.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery-ui/effects/effect-highlight
 //= require rails-ujs
-//= require twitter/bootstrap
+//= require popper
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require jquery-readyselector
 //= require jquery-fileupload/basic

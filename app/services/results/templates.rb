c38eff670d3bc2a937bc4175e553e34ca98186b5
moveson <mark.oveson@gmail.com>
2018-10-09 14:47:19 -0600

    Add "Overall, 30s, 40s, 50s, Seniors" podium template.

--- app/services/results/templates.rb
+++ app/services/results/templates.rb
@@ -7,23 +7,37 @@ module Results
     end
 
     def self.all
-      {ramble: Results::Template.new('Ramble', :inclusive, 3,
-                                     [:men_overall, :women_overall, :men_masters, :women_masters,
-                                      :men_under_20, :women_under_20, :men_20s, :women_20s,
-                                      :men_30s, :women_30s, :men_40s, :women_40s,
-                                      :men_50s, :women_50s, :men_60s, :women_60s]
-                                         .map { |name| Results::Categories.find(name) }),
-       blue_sky: Results::Template.new('Blue Sky', :inclusive, 3,
-                                       [:men_overall, :women_overall, :men_under_40, :women_under_40,
-                                        :men_40s_masters, :women_40s_masters, :men_grandmasters, :women_grandmasters]
-                                           .map { |name| Results::Categories.find(name) }),
-       masters_and_grandmasters: Results::Template.new('Masters and Grandmasters', :inclusive, 3,
-                                       [:men_overall, :women_overall, :men_40s_masters, :women_40s_masters,
-                                        :men_grandmasters, :women_grandmasters]
-                                           .map { |name| Results::Categories.find(name) }),
-       simple: Results::Template.new('Simple', :inclusive, 3,
-                                     [:men_overall, :women_overall]
-                                         .map { |name| Results::Categories.find(name) })
+      {ramble:
+           Results::Template.new('Ramble', :inclusive, 3,
+                                 [:men_overall, :women_overall, :men_masters, :women_masters,
+                                  :men_under_20, :women_under_20, :men_20s, :women_20s,
+                                  :men_30s, :women_30s, :men_40s, :women_40s,
+                                  :men_50s, :women_50s, :men_60s, :women_60s]
+                                     .map { |name| Results::Categories.find(name) }),
+
+       overall_30s_40s_50s_seniors:
+           Results::Template.new('Overall, 30s, 40s, 50s, Seniors', :inclusive, 3,
+                                 [:men_overall, :women_overall, :men_under_30, :women_under_30,
+                                  :men_30s, :women_30s, :men_40s, :women_40s,
+                                  :men_50s, :women_50s, :men_seniors, :women_seniors]
+                                     .map { |name| Results::Categories.find(name) }),
+
+       blue_sky:
+           Results::Template.new('Blue Sky', :inclusive, 3,
+                                 [:men_overall, :women_overall, :men_under_40, :women_under_40,
+                                  :men_40s_masters, :women_40s_masters, :men_grandmasters, :women_grandmasters]
+                                     .map { |name| Results::Categories.find(name) }),
+
+       masters_and_grandmasters:
+           Results::Template.new('Masters and Grandmasters', :inclusive, 3,
+                                 [:men_overall, :women_overall, :men_40s_masters, :women_40s_masters,
+                                  :men_grandmasters, :women_grandmasters]
+                                     .map { |name| Results::Categories.find(name) }),
+
+       simple:
+           Results::Template.new('Simple', :inclusive, 3,
+                                 [:men_overall, :women_overall]
+                                     .map { |name| Results::Categories.find(name) })
       }.with_indifferent_access
     end
 

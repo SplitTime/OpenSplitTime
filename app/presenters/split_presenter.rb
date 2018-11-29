a1dd35a4866388b7f55a4aa718fcb5188c30702c
Development <dev@Marks-Mac-Pro.local>
2018-11-29 15:00:32 -0700

    Update Split Views

--- app/presenters/split_presenter.rb
+++ app/presenters/split_presenter.rb
@@ -5,11 +5,16 @@ class SplitPresenter < BasePresenter
   delegate :id, :course, :base_name, :description, :distance_from_start, :vert_gain_from_start, :vert_loss_from_start,
            :latitude, :longitude, :elevation, :to_param, to: :split
   delegate :track_points, to: :course
+  delegate :organization, to: :event
 
   def initialize(split, params, current_user)
     @split = split
     @params = params
     @current_user = current_user
+  end
+
+  def event
+    @event ||= course.events.visible.latest
   end
 
   def course_splits

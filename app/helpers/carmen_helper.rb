c04b05daa678a393116efe7214b1e11665690bff
Abram Early <abram.early@gmail.com>
2018-12-08 15:18:04 -0700

    Select style fix

--- app/helpers/carmen_helper.rb
+++ app/helpers/carmen_helper.rb
@@ -6,11 +6,11 @@ module CarmenHelper
     priority = args[:priority]
     prompt = args[:prompt]
     country_table = Geodata.sorted_countries(priority).map { |country| [country.name, country.code] }
-    select(model, field, country_table, prompt: prompt)
+    select(model, field, country_table, {prompt: prompt}, {class: "form-control"})
   end
 
   def carmen_subregion_select(model, field, country)
     subregion_table = Geodata.included_subregions(country).map { |subregion| [subregion.name, subregion.code] }
-    select(model, field, subregion_table)
+    select(model, field, subregion_table, {}, {class: "form-control"})
   end
 end

41961049afe5859dca41305d5b814ec3bbeff45e
Mark Oveson <mark.oveson@gmail.com>
2019-01-02 14:49:33 -0700

    Tie homepage email to sign_up form.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,13 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
 
+    # GET /resource/sign_up
+    def new
+      build_resource(pre_filled_params)
+      yield resource if block_given?
+      respond_with resource
+    end
+
     protected
 
     def after_sign_up_path_for(resource)
@@ -14,6 +21,10 @@ module Users
     end
 
     private
+
+    def pre_filled_params
+      params[:user]&.permit(*UserParameters.permitted) || {}
+    end
 
     def sign_up_params
       params.require(:user).permit(*UserParameters.permitted)

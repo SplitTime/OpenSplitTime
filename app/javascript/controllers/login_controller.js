200a75a1595cced34a27e9195282bfcd64637f5b
Mark Oveson <mark.oveson@gmail.com>
2019-01-14 03:26:03 -0700

    Move reloadWithTurbolinks() function out of login_controller.

--- app/javascript/controllers/login_controller.js
+++ app/javascript/controllers/login_controller.js
@@ -16,21 +16,3 @@ export default class extends Controller {
         $(this.errorTarget).fadeIn()
     }
 }
-
-var reloadWithTurbolinks = (function () {
-    var scrollPosition;
-
-    function reload() {
-        scrollPosition = [window.scrollX, window.scrollY];
-        Turbolinks.visit(window.location.toString(), {action: 'replace'})
-    }
-
-    document.addEventListener('turbolinks:load', function () {
-        if (scrollPosition) {
-            window.scrollTo.apply(window, scrollPosition);
-            scrollPosition = null
-        }
-    });
-
-    return reload
-})();

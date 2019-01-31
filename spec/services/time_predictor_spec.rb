bb743507aca21088a3898e8b927ca92be9ff3a58
moveson <mark.oveson@gmail.com>
2019-01-31 11:32:12 -0700

    Update final specs for fixture compatibility. Tests now run in less than half the time as compared to pure factories.

--- spec/services/time_predictor_spec.rb
+++ spec/services/time_predictor_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe TimePredictor do
   let(:event) { events(:sum_55k) }
   let(:effort) { efforts(:sum_55k_progress_rolling) }
   let(:subject_split_times) { effort.split_times }
-  let(:ordered_splits) { event.ordered_splits}
+  let(:ordered_splits) { event.ordered_splits }
   let(:start) { ordered_splits.first }
   let(:aid_1) { ordered_splits.second }
   let(:aid_2) { ordered_splits.third }
@@ -27,26 +27,26 @@ RSpec.describe TimePredictor do
   let(:aid_1_in_to_aid_1_in) { build(:segment, begin_lap: 1, begin_split: aid_1, begin_in_out: 'in',
                                      end_lap: 1, end_split: aid_1, end_in_out: 'in') }
   let(:in_aid_2) { build(:segment, begin_lap: 1, begin_split: aid_2, begin_in_out: 'in',
-                               end_lap: 1, end_split: aid_2, end_in_out: 'out') }
+                         end_lap: 1, end_split: aid_2, end_in_out: 'out') }
   let(:start_to_aid_1) { build(:segment, begin_lap: 1, begin_split: start, begin_in_out: 'in',
-                                           end_lap: 1, end_split: aid_1, end_in_out: 'in') }
+                               end_lap: 1, end_split: aid_1, end_in_out: 'in') }
   let(:start_to_aid_2) { build(:segment, begin_lap: 1, begin_split: start, begin_in_out: 'in',
-                                            end_lap: 1, end_split: aid_2, end_in_out: 'in') }
+                               end_lap: 1, end_split: aid_2, end_in_out: 'in') }
   let(:aid_1_to_aid_2_inclusive) { build(:segment, begin_lap: 1, begin_split: aid_1, begin_in_out: 'in',
-                                                     end_lap: 1, end_split: aid_2, end_in_out: 'out') }
+                                         end_lap: 1, end_split: aid_2, end_in_out: 'out') }
   let(:aid_1_to_aid_5) { build(:segment, begin_lap: 1, begin_split: aid_1, begin_in_out: 'out',
-                                            end_lap: 1, end_split: aid_5, end_in_out: 'in') }
+                               end_lap: 1, end_split: aid_5, end_in_out: 'in') }
   let(:aid_2_to_aid_5) { build(:segment, begin_lap: 1, begin_split: aid_2, begin_in_out: 'out',
-                                            end_lap: 1, end_split: aid_5, end_in_out: 'in') }
+                               end_lap: 1, end_split: aid_5, end_in_out: 'in') }
   let(:aid_1_to_aid_2) { build(:segment, begin_lap: 1, begin_split: aid_1, begin_in_out: 'out',
-                                            end_lap: 1, end_split: finish, end_in_out: 'in') }
+                               end_lap: 1, end_split: finish, end_in_out: 'in') }
   let(:start_to_aid_5) { build(:segment, begin_lap: 1, begin_split: start, begin_in_out: 'in',
-                                           end_lap: 1, end_split: aid_5, end_in_out: 'in') }
+                               end_lap: 1, end_split: aid_5, end_in_out: 'in') }
   let(:start_to_finish) { build(:segment, begin_lap: 1, begin_split: start, begin_in_out: 'in',
-                                            end_lap: 1, end_split: finish, end_in_out: 'in') }
+                                end_lap: 1, end_split: finish, end_in_out: 'in') }
   let(:start_to_completed) { build(:segment, begin_lap: 1, begin_split: start, begin_in_out: 'in',
-                                         end_lap: completed_split_time.lap, end_split: completed_split_time.split,
-                                         end_in_out: SubSplit.kind(completed_split_time.bitkey)) }
+                                   end_lap: completed_split_time.lap, end_split: completed_split_time.split,
+                                   end_in_out: SubSplit.kind(completed_split_time.bitkey)) }
 
   let(:pace_factor) { subject.send(:pace_factor) }
 
@@ -179,51 +179,53 @@ RSpec.describe TimePredictor do
     let(:completed_segment) { start_to_completed }
     let(:limit_factors) { DataStatus::LIMIT_FACTORS }
     let(:typical_time_in_aid) { DataStatus::TYPICAL_TIME_IN_AID }
+    let(:typical_time) { segment.distance * distance_factor + segment.vert_gain * vert_gain_factor }
+    let(:course) { event.course }
 
-    it 'for a zero segment, sends to DataStatus a limits hash containing all zeros' do
-      segment = zero_start
-      expected = {low_bad: 0, low_questionable: 0, high_questionable: 0, high_bad: 0}
-      verify_data_status(segment, expected)
+    context 'for a zero segment' do
+      let(:segment) { zero_start }
+
+      it 'sends to DataStatus a limits hash containing all zeros' do
+        expected = {low_bad: 0, low_questionable: 0, high_questionable: 0, high_bad: 0}
+        verify_data_status(segment, expected)
+      end
     end
 
-    it 'for an in_aid segment, sends to DataStatus a limits hash containing zeros for low limits and pace-adjusted times for high limits' do
-      imputed_pace = imputed_pace(completed_segment)
-      segment = in_aid_2
-      typical_time = typical_time_in_aid
-      expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
-                     .map { |limit| [limit, (typical_time * limit_factors[:in_aid][limit] * imputed_pace).to_i] }
-                     .to_h
-      verify_data_status(segment, expected)
+    context 'for an in_aid segment' do
+      let(:segment) { in_aid_2 }
+      let(:typical_time) { typical_time_in_aid }
+
+      it 'sends to DataStatus a limits hash containing zeros for low limits and pace-adjusted times for high limits' do
+        expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
+                       .map { |limit| [limit, (typical_time * limit_factors[:in_aid][limit] * imputed_pace).to_i] }
+                       .to_h
+        verify_data_status(segment, expected)
+      end
     end
 
-    it 'for an inter-split segment, sends to DataStatus a limits hash containing pace-adjusted times for all limits' do
-      imputed_pace = imputed_pace(completed_segment)
-      segment = start_to_aid_2
-      typical_time = segment.distance * distance_factor + segment.vert_gain * vert_gain_factor
-      expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
-                     .map { |limit| [limit, (typical_time * limit_factors[:terrain][limit] * imputed_pace).to_i] }
-                     .to_h
-      verify_data_status(segment, expected)
+    context 'for an inter-split segment' do
+      let(:segment) { start_to_aid_2 }
+
+      it 'sends to DataStatus a limits hash containing pace-adjusted times for all limits' do
+        expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
+                       .map { |limit| [limit, (typical_time * limit_factors[:terrain][limit] * imputed_pace).to_i] }
+                       .to_h
+        verify_data_status(segment, expected)
+      end
     end
 
-    it 'for a segment covering multiple laps, sends to DataStatus a limits hash containing pace-adjusted times for all limits' do
-      imputed_pace = imputed_pace(completed_segment)
-      segment = start_to_finish
-      typical_time = segment.distance * distance_factor + segment.vert_gain * vert_gain_factor
-      expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
-                     .map { |limit| [limit, (typical_time * limit_factors[:terrain][limit] * imputed_pace).to_i] }
-                     .to_h
-      verify_data_status(segment, expected)
+    context 'for a segment covering the entire course' do
+      let(:segment) { start_to_finish }
+
+      it 'sends to DataStatus a limits hash containing pace-adjusted times for all limits' do
+        expected = [:low_bad, :low_questionable, :high_questionable, :high_bad]
+                       .map { |limit| [limit, (typical_time * limit_factors[:terrain][limit] * imputed_pace).to_i] }
+                       .to_h
+        verify_data_status(segment, expected)
+      end
     end
 
     def verify_data_status(segment, expected)
-      course = event.course
-      allow(course).to receive(:distance).and_return(finish.distance_from_start)
-      allow(course).to receive(:vert_gain).and_return(finish.vert_gain_from_start)
-      allow(course).to receive(:vert_loss).and_return(finish.vert_loss_from_start)
-      lap_splits, _ = lap_splits_and_time_points(event)
-      [segment.end_lap_split, segment.begin_lap_split]
-          .each { |lap_split| allow(lap_split).to receive(:course).and_return(course) }
       seconds = 999
       allow(DataStatus).to receive(:determine)
       TimePredictor.new(segment: segment,
@@ -231,16 +233,10 @@ RSpec.describe TimePredictor do
                         lap_splits: lap_splits,
                         completed_split_time: completed_split_time,
                         calc_model: :terrain).data_status(seconds)
-      expect(DataStatus).to have_received(:determine).with(expected, seconds)
+      expect(DataStatus).to have_received(:determine).with(a_hash_approximating(expected, 10), seconds)
     end
 
-    def imputed_pace(segment)
-      course = event.course
-      allow(course).to receive(:distance).and_return(finish.distance_from_start)
-      allow(course).to receive(:vert_gain).and_return(finish.vert_gain_from_start)
-      allow(course).to receive(:vert_loss).and_return(finish.vert_loss_from_start)
-      [segment.end_lap_split, segment.begin_lap_split]
-          .each { |lap_split| allow(lap_split).to receive(:course).and_return(course) }
+    def imputed_pace
       completed_typical_time = completed_segment.distance * distance_factor + completed_segment.vert_gain * vert_gain_factor
       completed_split_time.time_from_start / completed_typical_time
     end

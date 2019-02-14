2229115fb6d53b9649a55c9bc6ee546d927722c0
moveson <mark.oveson@gmail.com>
2019-02-13 23:58:23 -0700

    Remove unnecessary migration.

--- db/migrate/20190214060920_drop_live_times.rb
+++
@@ -1,6 +0,0 @@
-class DropLiveTimes < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :live_times do
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_060920) do
+ActiveRecord::Schema.define(version: 2019_02_14_053703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"

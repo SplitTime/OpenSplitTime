680f44b0da07369a0bdad146d80d7f4ae897a14b
Mark Oveson <mark.oveson@gmail.com>
2019-03-01 00:16:42 -0700

    Generalize Paperclip/ActiveStorage migration.

--- app/services/migrate_to_active_storage.rb
+++ app/services/migrate_to_active_storage.rb
@@ -4,24 +4,14 @@ class MigrateToActiveStorage
   require 'open-uri'
   require 'aws-sdk-s3'
 
-  def self.perform(klass, attribute)
-    new(klass, attribute).perform
-  end
-
-  def initialize(klass, attribute)
-    @klass = klass
-    @attribute = attribute
-  end
-
   def perform
-    get_blob_id = "LASTVAL()" # last value grabbed in postgres database
+    get_blob_id = 'LASTVAL()'
 
     ActiveRecord::Base.connection.raw_connection.prepare("active_storage_blob_statement", <<-SQL)
       INSERT INTO active_storage_blobs (
         key, filename, content_type, metadata, byte_size, checksum, created_at
       ) VALUES ($1, $2, $3, '{}', $4, $5, $6)
     SQL
-    # With the values, SQL was complaining if I didn't have named variables ($1, etc.).
 
     ActiveRecord::Base.connection.raw_connection.prepare("active_storage_attachment_statement", <<-SQL)
       INSERT INTO active_storage_attachments (
@@ -29,36 +19,34 @@ class MigrateToActiveStorage
       ) VALUES ($1, $2, $3, #{get_blob_id}, $4)
     SQL
 
-    ActiveRecord::Base.transaction do
-      klass.find_each.each do |resource|
-        next if resource.send("#{attribute}_file_name").blank?
+    model_map = {Course => :gpx, Effort => :photo, Partner => :banner, Person => :photo}
 
-        make_active_storage_records(resource)
+    model_map.each do |model, attribute|
+      model.where.not("#{attribute}_file_name" => nil).find_each.each do |resource|
+        make_active_storage_records(resource, attribute, model)
       end
     end
   end
 
   private
 
-  attr_reader :klass, :attribute
-
-  def make_active_storage_records(resource)
-    blob_key = key(resource)
+  def make_active_storage_records(resource, attribute, model)
+    blob_key = key(resource, attribute)
     filename = resource.send("#{attribute}_file_name")
     content_type = resource.send("#{attribute}_content_type")
     file_size = resource.send("#{attribute}_file_size")
-    file_checksum = checksum(resource)
+    file_checksum = checksum(resource, attribute)
     created_at = resource.updated_at.iso8601
 
     blob_values = [blob_key, filename, content_type, file_size, file_checksum, created_at]
+
     ActiveRecord::Base.connection.raw_connection.exec_prepared(
         "active_storage_blob_statement",
         blob_values
     )
 
-    # This will allow `klass.resource` calls to return an asset.
     blob_name = attribute
-    record_type = klass.name
+    record_type = model.name
     record_id = resource.id
 
     attachment_values = [blob_name, record_type, record_id, created_at]
@@ -68,16 +56,11 @@ class MigrateToActiveStorage
     )
   end
 
-  def key(resource)
-    # This differs from the standard key mentioned in the
-    # migration guide, because on S3 our file is
-    # located under several nested folders. Just
-    # putting the filename as the key means Active Storage
-    # searches the root directory and the file is not there.
-    resource.send(attribute).path
+  def key(resource, attribute)
+    resource.send("#{attribute}").path
   end
 
-  def checksum(resource)
+  def checksum(resource, attribute)
     resource_url = resource.send(attribute).expiring_url(60)
     Rails.logger.info "Copy metadata for file: #{resource.send(attribute).path}"
     uri = URI.parse(resource_url)

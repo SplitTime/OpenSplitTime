702d81c109ea0421ecd5bee9efbf0534f7b22189
Mark Oveson <mark.oveson@gmail.com>
2019-04-17 14:54:33 -0600

    Remove combinedSplitAttributes key from EventGroupSerializer.

--- app/serializers/event_group_serializer.rb
+++ app/serializers/event_group_serializer.rb
@@ -2,17 +2,11 @@
 
 class EventGroupSerializer < BaseSerializer
   attributes :id, :name, :organization_id, :concealed, :available_live, :monitor_pacers, :slug,
-             :multi_lap, :maximum_laps, :combined_split_attributes, :data_entry_groups, :unpaired_data_entry_groups
+             :multi_lap, :maximum_laps, :data_entry_groups, :unpaired_data_entry_groups
   link(:self) { api_v1_event_group_path(object) }
 
   has_many :events
   belongs_to :organization
-
-  def combined_split_attributes
-    CombineEventGroupSplitAttributes.perform(object,
-                                             pair_by_location: pair_by_location?,
-                                             node_attributes: [:event_split_ids, :sub_split_kind, :label, :split_name, :display_split_name])
-  end
 
   def data_entry_groups
     CombineEventGroupSplitAttributes.perform(object,

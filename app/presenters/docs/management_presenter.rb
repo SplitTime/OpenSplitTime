d412335a91979cb5bc9ab3be0e4a7b6c0aa186e9
moveson <mark.oveson@gmail.com>
2019-05-15 10:56:34 -0600

    Create all remaining documentation pages (substantive text to come).

--- app/presenters/docs/management_presenter.rb
+++ app/presenters/docs/management_presenter.rb
@@ -15,7 +15,7 @@ module Docs
           overview: {display_topic: 'Overview', pages: ['Summary of Steps']},
           prep: {display_topic: 'Preparation', pages: ['Going Public and Live', 'Explaining How to Follow', 'Preparing Equipment', 'Training Crews']},
           start: {display_topic: 'Starting', pages: ['Overview', 'Checking In', 'Changing Entrants', 'Starting Entrants']},
-          monitor: {display_topic: 'Monitoring', pages: ['Tools', 'Looking for Holes', 'Watching Progress', 'Aid Station Summary', 'Aid Station Detail']},
+          monitor: {display_topic: 'Monitoring', pages: ['Tools', 'Full Results', 'Watching Progress', 'Aid Station Summary', 'Aid Station Detail', 'Problem Efforts']},
           enter: {display_topic: 'Entering and Editing Times', pages: ['Live Entry', 'Direct Edit', 'Rebuilding an Effort', 'Finding Problems', 'Sleuthing']},
           wind_up: {display_topic: 'Winding Up', pages: ['Setting Stops', 'Disabling Live Entry', 'Exporting Data']},
           support: {display_topic: 'Support', pages: ['Getting Help']}

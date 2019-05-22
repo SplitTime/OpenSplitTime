510450550441e86232a9c5d644a2f2dc37a42d06
Mark Oveson <mark.oveson@gmail.com>
2019-05-22 10:11:58 -0600

    Finish documentation for Event Management/Preparation.

--- app/presenters/docs/management_presenter.rb
+++ app/presenters/docs/management_presenter.rb
@@ -13,7 +13,7 @@ module Docs
     def items
       {
           overview: {display_topic: 'Overview', pages: ['Summary of Steps']},
-          prep: {display_topic: 'Preparation', pages: ['Going Public and Live', 'Explaining How to Follow', 'Preparing Equipment', 'Training Crews']},
+          prep: {display_topic: 'Preparation', pages: ['Going Public and Live', 'Explaining How to Follow', 'Obtaining Equipment', 'Caring for Equipment', 'Training Crews']},
           start: {display_topic: 'Starting', pages: ['Overview', 'Checking In', 'Changing Entrants', 'Starting Entrants']},
           monitor: {display_topic: 'Monitoring', pages: ['Tools', 'Full Results', 'Watching Progress', 'Aid Station Overview', 'Aid Station Detail', 'Problem Efforts']},
           enter: {display_topic: 'Entering and Editing Times', pages: ['Live Entry', 'Direct Edit', 'Rebuilding an Effort', 'Finding Problems', 'Sleuthing']},

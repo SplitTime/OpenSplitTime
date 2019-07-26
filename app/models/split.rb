4ac334948bbfde02eb37a52073e23057006019f6
Mark Oveson <mark.oveson@gmail.com>
2019-07-25 21:47:01 -0600

    Improve formatting in Split model.

--- app/models/split.rb
+++ app/models/split.rb
@@ -3,12 +3,9 @@
 class Split < ApplicationRecord
   DISTANCE_THRESHOLD = 50 # Distance (in meters) below which split locations are deemed equivalent
 
-  include Auditable
-  include Delegable
-  include Locatable
-  include GuaranteedFindable
-  include UnitConversions
+  include Auditable, Delegable, Locatable, GuaranteedFindable, UnitConversions
   extend FriendlyId
+
   strip_attributes collapse_spaces: true
   friendly_id :course_split_name, use: [:slugged, :history]
   enum kind: [:start, :finish, :intermediate]

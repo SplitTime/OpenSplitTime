60c5673ea26bed2e7b703b9009c412478af87fa5
Mark Oveson <mark.oveson@gmail.com>
2019-09-09 07:30:03 -0600

    Begin building audit_list view

--- app/view_models/effort_audit_view.rb
+++ app/view_models/effort_audit_view.rb
@@ -5,5 +5,11 @@ class EffortAuditView < EffortWithLapSplitRows
   delegate :event_name, :person, :start_time, :has_start_time?, :stopped?, to: :loaded_effort
   delegate :simple?, :multiple_sub_splits?, :multiple_laps?, :laps_unlimited?, :event_group, to: :event
 
-
+  def audit_rows
+    lap_splits.flat_map do |lap_split|
+      lap_split.bitkeys.map do |bitkey|
+        OpenStruct.new(name: lap_split.name(bitkey), time_point: lap_split.time_point(bitkey))
+      end
+    end
+  end
 end

0b9301e773b020d72c8ef8db102f977725324fc8
Mark Oveson <mark.oveson@gmail.com>
2019-10-13 23:10:36 -0600

    Remove multiple lap compatibility from audit view

--- app/view_models/effort_audit_view.rb
+++ app/view_models/effort_audit_view.rb
@@ -5,11 +5,9 @@ class EffortAuditView < EffortWithLapSplitRows
   DISCREPANCY_THRESHOLD = 1.minute
 
   delegate :event_name, :person, :start_time, :has_start_time?, :stopped?, to: :loaded_effort
-  delegate :simple?, :multiple_sub_splits?, :multiple_laps?, :laps_unlimited?, :event_group, to: :event
+  delegate :simple?, :multiple_sub_splits?, :laps_unlimited?, :event_group, to: :event
 
   def audit_rows
-    name_method = multiple_laps? ? :name : :name_without_lap
-
     lap_splits.flat_map do |lap_split|
       lap_split.bitkeys.map do |bitkey|
         time_point = lap_split.time_point(bitkey)
@@ -17,7 +15,7 @@ class EffortAuditView < EffortWithLapSplitRows
         matched_raw_times = raw_times.select { |rt| rt.split_time_id && rt.split_time_id == split_time.id }
         unmatched_raw_times = raw_times.select { |rt| rt.split_time_id.nil? && rt.time_point == time_point }
 
-        EffortAuditRow.new(name: lap_split.public_send(name_method, bitkey),
+        EffortAuditRow.new(name: lap_split.public_send(:name_without_lap, bitkey),
                            parameterized_split_name: lap_split.split.parameterized_base_name,
                            sub_split_kind: SubSplit.kind(bitkey).downcase,
                            time_point: time_point,
@@ -48,10 +46,6 @@ class EffortAuditView < EffortWithLapSplitRows
     adjusted_times = times_in_seconds.map { |seconds| (seconds - times_in_seconds.first) > 12.hours ? (seconds - 24.hours).to_i : seconds }.sort
     largest_discrepancy = (adjusted_times.last - adjusted_times.first).to_i
 
-    single_lap? && largest_discrepancy > DISCREPANCY_THRESHOLD
-  end
-
-  def single_lap?
-    !multiple_laps?
+    largest_discrepancy > DISCREPANCY_THRESHOLD
   end
 end

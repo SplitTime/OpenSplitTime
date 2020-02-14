99eb56c399781008e635ce2b2ae29e3162f5b67e
Mark Oveson <mark.oveson@mx.com>
2020-02-14 00:08:46 -0700

    Update migration to populate matchable_bib_number

--- db/migrate/20200214063659_add_matchable_bib_number_to_raw_times.rb
+++ db/migrate/20200214063659_add_matchable_bib_number_to_raw_times.rb
@@ -1,5 +1,10 @@
 class AddMatchableBibNumberToRawTimes < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :raw_times, :matchable_bib_number, :integer
+    RawTime.find_each(&:save!)
+  end
+
+  def down
+    remove_column :raw_times, :matchable_bib_number, :integer
   end
 end

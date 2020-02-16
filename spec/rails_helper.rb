6b5d3671cf3d33f22e70133f28471701dd166b93
Mark Oveson <mark.oveson@mx.com>
2020-02-16 10:43:16 -0700

    Incorporate paper_trail helper into RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,8 @@ require 'rack/test'
 require 'webmock/rspec'
 require 'aws-sdk-s3'
 require 'aws-sdk-sns'
+require 'paper_trail/frameworks/rspec'
+
 WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

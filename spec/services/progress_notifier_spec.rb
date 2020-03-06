a785ae528dad991fe2a4b073d1eb651d03dc6057
Mark Oveson <mark.oveson@mx.com>
2020-03-05 23:48:27 -0700

    Generate shortened url in advance in spec

--- spec/services/progress_notifier_spec.rb
+++ spec/services/progress_notifier_spec.rb
@@ -18,21 +18,24 @@ RSpec.describe ProgressNotifier do
   end
 
   describe '#publish' do
-    it 'sends a message to an SNS client containing the expected information' do
-      sns_client.stub_data(:publish)
-      stubbed_response = OpenStruct.new(successful?: true)
-      subject = ProgressNotifier.new(topic_arn: topic_arn, effort_data: effort_data, sns_client: sns_client)
-      expected_subject = 'Update for Joe LastName 1 at Test Event 1 from OpenSplitTime'
-      full_path = subject.send(:effort_path)
-      expected_key = Shortener::ShortenedUrl.find_by(url: full_path).unique_key
-      expected_shortened_url = "#{OST::SHORTENED_URI}/#{expected_key}"
-      expected_message = <<~MESSAGE
+    let(:expected_subject) { 'Update for Joe LastName 1 at Test Event 1 from OpenSplitTime' }
+    let(:expected_message) {
+      <<~MESSAGE
         Joe LastName 1 made progress at Test Event 1:
         Split 1 In (Mile 6.2), Friday  7:40AM, elapsed: 01:40:00
         Split 1 Out (Mile 6.2), Friday  7:50AM, elapsed: 01:50:00 and stopped there
         Results on OpenSplitTime: #{expected_shortened_url}
       MESSAGE
+    }
+    let(:expected_shortened_url) { "#{OST::SHORTENED_URI}/#{expected_key}" }
+    let(:expected_key) { Shortener::ShortenedUrl.find_by(url: effort_path).unique_key }
+    let(:effort_path) { subject.send(:effort_path) }
+    let(:stubbed_response) { OpenStruct.new(successful?: true) }
 
+    before { Shortener::ShortenedUrl.generate!(effort_path) }
+
+    it 'sends a message to an SNS client containing the expected information' do
+      sns_client.stub_data(:publish)
       expect(sns_client).to receive(:publish).with(topic_arn: topic_arn, subject: expected_subject, message: expected_message).and_return(stubbed_response)
       subject.publish
     end

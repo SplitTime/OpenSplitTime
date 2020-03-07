29058bd782e02e7178b6eb28829f29d938d80ad2
Mark Oveson <mark.oveson@mx.com>
2020-03-07 15:59:54 -0700

    Add keyboard shortcut for refresh button

--- app/helpers/refresh_helper.rb
+++ app/helpers/refresh_helper.rb
@@ -2,13 +2,14 @@
 
 module RefreshHelper
   def link_to_refresh
-    tooltip_title = 'Refresh page'
+    tooltip_title = 'Refresh page [CTRL-R]'
     content_tag(:span, data: {controller: :refresh}) do
       link_to fa_icon(:redo, data: {target: 'refresh.icon'}),
               request.params,
               id: 'refresh-button',
               class: 'btn btn-primary has-tooltip',
-              data: {action: 'click->refresh#spin',
+              data: {action: 'click->refresh#spin keyup@document->refresh#evaluate',
+                     target: 'refresh.button',
                      toggle: 'tooltip',
                      placement: :bottom,
                      'original-title' => tooltip_title}
--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -2,9 +2,15 @@ import {Controller} from "stimulus"
 
 export default class extends Controller {
 
-    static targets = ["icon"];
+    static targets = ["button", "icon"];
 
     spin() {
         this.iconTarget.classList.add("fa-spin");
+    }
+
+    evaluate(e) {
+        if((e.ctrlKey === true || e.target.tagName !== 'INPUT') && e.altKey === false && e.key === 'r') {
+            this.buttonTarget.click()
+        }
     }
 }

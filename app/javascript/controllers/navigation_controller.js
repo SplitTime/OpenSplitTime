45bf51d9d70812e32944b0e19c7907c8026c63d8
Mark Oveson <mark.oveson@mx.com>
2020-03-08 17:37:13 -0600

    Keyboard shortcuts for prior and next buttons

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -2,16 +2,24 @@ import {Controller} from "stimulus"
 
 export default class extends Controller {
 
-    static targets = ["refreshButton"];
+    static targets = ['refreshButton', 'priorButton', 'nextButton'];
 
     spinIcon(e) {
         let icon = e.currentTarget.getElementsByTagName('I')[0];
-        icon.classList.add("fa-spin");
+        icon.classList.add('fa-spin');
     }
 
     evaluateKeyup(e) {
-        if((e.ctrlKey === true || e.target.tagName !== 'INPUT') && e.altKey === false && e.key === 'r') {
-            this.refreshButtonTarget.click()
+        let shortcutInvoked = (e.ctrlKey === true || e.target.tagName !== 'INPUT') && e.altKey === false;
+
+        if (shortcutInvoked) {
+            if (e.key === 'r' && this.hasRefreshButtonTarget) {
+                this.refreshButtonTarget.click()
+            } else if (e.key === 'p' && this.hasPriorButtonTarget) {
+                this.priorButtonTarget.click()
+            } else if (e.key === 'n' && this.hasNextButtonTarget) {
+                this.nextButtonTarget.click()
+            }
         }
     }
 }

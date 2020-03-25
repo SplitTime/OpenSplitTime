5689c8f45c77cb44dc10d797d7f40749137f970c
Mark Oveson <mark.oveson@gmail.com>
2020-03-24 23:10:30 -0600

    OrderedEffortsAtTimePoint#time_point method

--- app/models/ordered_efforts_at_time_point.rb
+++ app/models/ordered_efforts_at_time_point.rb
@@ -6,4 +6,8 @@ class OrderedEffortsAtTimePoint
   attribute :split_id, :integer
   attribute :sub_split_bitkey, :integer
   attribute :effort_ids, :integer_array_from_string
+
+  def time_point
+    TimePoint.new(lap, split_id, sub_split_bitkey)
+  end
 end

a539c2fed5b8d29ab9cf7b33b4b17713680c6e77
Mark Oveson <mark.oveson@gmail.com>
2020-03-28 05:04:55 -0600

    Docs for custom type

--- app/queries/integer_array_from_string_type.rb
+++ app/queries/integer_array_from_string_type.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+# Postgres returns an array_agg field as a comma-delimited String
+# surrounded by curly braces, like "{1,2,3}".
+#
+# This Type converts such a String to an array of integers.
+#
 class IntegerArrayFromStringType < ::ActiveModel::Type::Value
+  # @param [String, Array, nil] value
+  # @return [Array<Integer>]
   def cast(value)
     return super if value.is_a?(Enumerable)
 

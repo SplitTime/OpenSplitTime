781a88e9650d1eb9a12edcb8b13b538d2856b85f
Mark Oveson <mark.oveson@gmail.com>
2020-03-28 02:33:37 -0600

    Use correct query for OEATP

--- app/models/query/ordered_efforts_at_time_point.rb
+++ app/models/query/ordered_efforts_at_time_point.rb
@@ -2,39 +2,28 @@
 
 module Query
   class OrderedEffortsAtTimePoint
-    def self.sql(effort_id)
+    def self.sql(event_id)
       <<~SQL.squish
-        with effort_time_intervals as
-          (select ist.effort_id, ist.lap, ist.split_id, ist.absolute_time as in_time, ost.absolute_time as out_time, events.event_group_id
-          from split_times ist
-            join split_times ost on ist.effort_id = ost.effort_id and ist.lap = ost.lap and ist.split_id = ost.split_id and ost.sub_split_bitkey = 64
-            join efforts on efforts.id = ist.effort_id
-            join events on efforts.event_id = events.id
-          where ist.effort_id = #{effort_id} and ist.sub_split_bitkey = 1),
+      with start_times as
+        (select effort_id, absolute_time as start_time
+        from split_times
+           join efforts on efforts.id = split_times.effort_id
+           join splits on splits.id = split_times.split_id
+        where efforts.event_id = #{event_id} and split_times.lap = 1 and split_times.sub_split_bitkey = 1 and splits.kind = 0),
 
-        group_split_times as
-          (select effort_id, lap, split_id, 
-              case when sub_split_bitkey = 1 then absolute_time else null end as in_time,
-              case when sub_split_bitkey = 64 then absolute_time else null end as out_time
-          from split_times
-            join efforts on efforts.id = split_times.effort_id
-            join events on efforts.event_id = events.id
-          where events.event_group_id = (select event_group_id from effort_time_intervals limit 1)),
-            
-        group_time_intervals as
-          (select effort_id, lap, split_id, max(in_time) as in_time, max(out_time) as out_time
-           from group_split_times
-           group by effort_id, lap, split_id)
-
-        select eti.effort_id, eti.lap, eti.split_id, array_agg(gti.effort_id) as together_effort_ids
-        from effort_time_intervals eti
-            left join group_time_intervals gti using (lap, split_id)
-        where ((gti.in_time between eti.in_time and eti.out_time)
-           or (gti.out_time between eti.in_time and eti.out_time)
-           or (gti.in_time < eti.in_time and gti.out_time > eti.out_time))
-           and eti.effort_id != gti.effort_id
-        group by eti.effort_id, eti.lap, eti.split_id
-        order by eti.effort_id, eti.lap, eti.split_id;
+      elapsed_times as	
+        (select lap, ast.split_id, ast.sub_split_bitkey, ast.effort_id, distance_from_start, ast.absolute_time - start_times.start_time as elapsed_time
+        from split_times ast
+           join efforts on efforts.id = ast.effort_id
+           join splits on splits.id = ast.split_id
+           join start_times on start_times.effort_id = ast.effort_id
+        where efforts.event_id = #{event_id}
+        order by lap, distance_from_start, sub_split_bitkey, elapsed_time)
+        
+      select lap, split_id, sub_split_bitkey, array_agg(effort_id) as effort_ids
+      from elapsed_times
+      group by lap, split_id, sub_split_bitkey, distance_from_start
+      order by lap, distance_from_start, sub_split_bitkey
       SQL
     end
   end

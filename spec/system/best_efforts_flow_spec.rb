bdf98c443213a99e6897db34492080cb9fbecacf
Mark Oveson <mark.oveson@mx.com>
2020-03-28 13:30:16 -0600

    Proper scoping of events in best efforts display

--- spec/system/best_efforts_flow_spec.rb
+++ spec/system/best_efforts_flow_spec.rb
@@ -8,8 +8,11 @@ require 'rails_helper'
 # rails db:structure:load RAILS_ENV=test
 
 RSpec.describe 'Visit the best efforts page and search for an effort' do
-  let(:course) { courses(:hardrock_ccw) }
-  let(:event) { events(:hardrock_2015) }
+  let(:course) { courses(:hardrock_cw) }
+  let(:event) { events(:hardrock_2016) }
+  let(:event_group) { event.event_group }
+  let(:organization) { event_group.organization }
+
   let(:effort_1) { event.efforts.ranked_with_status.first }
   let(:other_efforts) { event.efforts.where.not(id: effort_1.id) }
 
@@ -27,5 +30,61 @@ RSpec.describe 'Visit the best efforts page and search for an effort' do
 
     verify_link_present(effort_1)
     other_efforts.each { |effort| verify_content_absent(effort) }
+  end
+
+  context 'when hidden efforts exist for the course' do
+    let(:hidden_event) { events(:hardrock_2014) }
+    let(:hidden_event_group) { hidden_event.event_group }
+    let(:hidden_effort_1) { hidden_event.efforts.ranked_with_status.first }
+    let(:other_hidden_efforts) { hidden_event.efforts.where.not(id: hidden_effort_1.id) }
+
+    let(:user) { users(:third_user) }
+    let(:owner) { users(:fourth_user) }
+    let(:steward) { users(:fifth_user) }
+    let(:admin) { users(:admin_user) }
+
+    before do
+      hidden_event_group.update(concealed: true)
+      organization.update(created_by: owner.id)
+      organization.stewards << steward
+    end
+
+    scenario 'The user is a visitor' do
+      visit best_efforts_course_path(course)
+      verify_link_present(effort_1)
+      verify_content_absent(hidden_effort_1)
+    end
+
+    scenario 'The user is not the owner and not a steward' do
+      login_as user, scope: :user
+
+      visit best_efforts_course_path(course)
+      verify_link_present(effort_1)
+      verify_content_absent(hidden_effort_1)
+    end
+
+    scenario 'The user owns the organization' do
+      login_as owner, scope: :user
+
+      visit best_efforts_course_path(course)
+      verify_link_present(effort_1)
+      verify_link_present(hidden_effort_1)
+    end
+
+    scenario 'The user is a steward of the organization' do
+      login_as steward, scope: :user
+
+      visit best_efforts_course_path(course)
+      verify_link_present(effort_1)
+      verify_link_present(hidden_effort_1)
+    end
+
+    scenario 'The user is an admin' do
+      login_as admin, scope: :user
+
+      visit best_efforts_course_path(course)
+      verify_link_present(effort_1)
+      verify_link_present(hidden_effort_1)
+    end
   end
 end

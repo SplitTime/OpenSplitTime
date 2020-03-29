5a9ac3c2bb3a62f9f0df46717213ce2d0551ec44
Mark Oveson <mark.oveson@mx.com>
2020-03-29 12:39:47 -0600

    System spec for follow redirect

--- spec/system/follow_view_spec.rb
+++ spec/system/follow_view_spec.rb
@@ -25,4 +25,28 @@ RSpec.describe 'visit the follow page' do
     visit follow_event_group_path(event_group)
     expect(page).to have_content(event_group.name)
   end
+
+  context 'The event group is completed' do
+    before do
+      event_group.events.each do |event|
+        Interactors::UpdateEffortsStop.perform!(event.efforts, stop_status: true)
+      end
+    end
+
+    context 'The event group has a single event' do
+      let(:event) { event_group.events.first }
+      scenario 'A visitor views the follow page and is redirected to the spread' do
+        visit follow_event_group_path(event_group)
+        expect(current_path).to eq spread_event_path(event)
+      end
+    end
+
+    context 'The event group has multiple events' do
+      let(:event_group) { event_groups(:sum) }
+      scenario 'A visitor views the follow page and is redirected to the event group page' do
+        visit follow_event_group_path(event_group)
+        expect(current_path).to eq event_group_path(event_group)
+      end
+    end
+  end
 end

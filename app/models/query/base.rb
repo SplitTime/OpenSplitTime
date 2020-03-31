ea6cb1fcfe4322bd86061e41a84d88a984e48f48
Mark Oveson <mark.oveson@mx.com>
2020-03-30 22:34:51 -0600

    Revert "Move over_segment query to new ::Query module and namespace"
    This reverts commit b1ac098dcaa3919546544897564066948d76dde4.

--- app/models/query/base.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Query
-  class Base
-    def self.sql_for_existing_scope(scope)
-      scope.connection.unprepared_statement { scope.reorder(nil).select('id').to_sql }
-    end
-  end
-end

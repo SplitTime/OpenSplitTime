d1c87d43b33f3cf78c09f5be97995312ac1bb76b
Mark Oveson <mark.oveson@mx.com>
2020-03-31 15:14:19 -0600

    Improve docs in SplitTimeData model

--- app/models/split_time_data.rb
+++ app/models/split_time_data.rb
@@ -4,16 +4,16 @@
 # The time zone parsing methods in Rails are slow and create an unacceptable delay
 # when working with many objects, particularly in a full spread view.
 
-# SplitTimeData is designed to receive both an absolute_time string
-# and a localized (day_and_time) string from the database query.
+# SplitTimeData receives both a non-localized absolute_time_string and a localized
+# absolute_time_local_string from the database query.
 
-SplitTimeData = Struct.new(:id, :effort_id, :lap, :split_id, :bitkey, :stopped_here, :pacer, :data_status_numeric, :absolute_time_string,
-                           :absolute_time_local_string, :time_from_start, :segment_time, :military_time, keyword_init: true) do
+SplitTimeData = Struct.new(:id, :effort_id, :lap, :split_id, :bitkey, :stopped_here, :pacer, :data_status_numeric,
+                           :absolute_time_string, :absolute_time_local_string, :time_from_start, :segment_time,
+                           :military_time, keyword_init: true) do
 
   include TimePointMethods
 
   # absolute_time is an ActiveSupport::TimeWithZone for compatibility and useful math operations.
-
   def absolute_time
     absolute_time_string&.in_time_zone('UTC')
   end
@@ -21,7 +21,6 @@ SplitTimeData = Struct.new(:id, :effort_id, :lap, :split_id, :bitkey, :stopped_h
   # absolute_time_local is a Ruby DateTime object for speed of conversion.
   # The events/spread view relies on this attribute and slows unacceptably
   # when it is parsed into an ActiveSupport::TimeWithZone object.
-
   def absolute_time_local
     absolute_time_local_string&.to_datetime
   end

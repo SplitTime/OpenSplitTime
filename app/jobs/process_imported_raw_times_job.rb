d5088fa1d52f411562f269e6f9fca5908a694a1c
Mark Oveson <mark.oveson@mx.com>
2020-04-05 06:14:27 -0600

    New class to set absolute time and lap

--- app/jobs/process_imported_raw_times_job.rb
+++ app/jobs/process_imported_raw_times_job.rb
@@ -6,7 +6,8 @@ class ProcessImportedRawTimesJob < ApplicationJob
   queue_as :default
 
   def perform(event_group, raw_times)
-    match_response = Interactors::MatchRawTimesToSplitTimes.perform!(event_group: event_group, raw_times: raw_times)
+    loaded_raw_times = ::RawTimes::SetAbsoluteTimeAndLap.perform(event_group, raw_times)
+    match_response = Interactors::MatchRawTimesToSplitTimes.perform!(event_group: event_group, raw_times: loaded_raw_times)
 
     if match_response.successful?
       unmatched_raw_times = match_response.resources[:unmatched]

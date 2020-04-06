2cd30147505850d0c11c5867020da5d8a9d990bc
Mark Oveson <mark.oveson@mx.com>
2020-04-05 21:21:55 -0600

    RawTimes::VerifyWithinEffort (will replace VerifyRawTimeRow

---
+++ app/services/raw_times/verify_within_effort.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+# raw_times must be complete with relation ids, lap, and absolute_time
+module RawTimes
+  class VerifyWithinEffort
+    def self.perform(raw_times, effort, options = {})
+      new(raw_times, effort, options).perform
+    end
+
+    def initialize(raw_times, effort, options = {})
+      @raw_times = raw_times
+      @effort = effort
+      @times_container = options[:times_container] || SegmentTimesContainer.new(calc_model: :stats)
+      @errors = []
+      validate_setup
+    end
+
+    def perform
+      set_split_time_exists
+      append_split_times
+      set_data_status
+    end
+
+    private
+
+    attr_reader :raw_times, :effort, :times_container, :errors
+
+    def set_split_time_exists
+      raw_times.each do |raw_time|
+        raw_time.split_time_exists = existing_split_times.any? { |st| st.time_point == raw_time.time_point }
+      end
+    end
+
+    def append_split_times
+      raw_times.each do |raw_time|
+        raw_time.new_split_time = ::SplitTimeFromRawTime.build(raw_time, effort: effort, event: event)
+      end
+    end
+
+    def set_data_status
+      ::Interactors::SetEffortStatus.perform(effort, ordered_split_times: combined_split_times, lap_splits: effort_lap_splits, times_container: times_container)
+    end
+
+    def combined_split_times
+      existing_split_times.each { |st| st.data_status = :confirmed if st.good? }
+      indexed_existing_split_times = existing_split_times.index_by(&:time_point)
+      indexed_new_split_times = new_split_times.index_by(&:time_point)
+      indexed_split_times = indexed_existing_split_times.merge(indexed_new_split_times)
+      indexed_split_times.values_at(*effort_time_points)
+    end
+
+    def existing_split_times
+      effort.ordered_split_times.map(&:dup)
+    end
+
+    def new_split_times
+      raw_times.map(&:new_split_time).compact
+    end
+
+    def event
+      effort.event
+    end
+
+    def effort_time_points
+      @effort_time_points ||= effort_lap_splits.flat_map(&:time_points)
+    end
+
+    def effort_lap_splits
+      @effort_lap_splits ||= event.required_lap_splits.presence || event.lap_splits_through(max_raw_time_lap)
+    end
+
+    def max_raw_time_lap
+      raw_times.max_by(&:lap).lap
+    end
+
+    def validate_setup
+      raw_times.each do |raw_time|
+        raise ArgumentError, "#{raw_time} is incomplete" unless raw_time.complete?
+        raise ArgumentError, "#{raw_time} does not match the provided effort #{effort}" unless raw_time.effort_id == effort.id
+      end
+    end
+  end
+end

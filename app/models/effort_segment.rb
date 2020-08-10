274fa41c004c3f24a34ee775f1c3393de64df706
Mark Oveson <mark.oveson@mx.com>
2020-08-10 00:06:26 -0600

    Set effort segments before best efforts spec

--- app/models/effort_segment.rb
+++ app/models/effort_segment.rb
@@ -3,6 +3,10 @@
 class EffortSegment < ApplicationRecord
   readonly
 
+  def self.delete_all
+    Effort.find_each { |effort| delete_for_effort(effort) }
+  end
+
   def self.delete_for_effort(effort)
     query = EffortSegmentQuery.delete_for_effort(effort)
     ActiveRecord::Base.connection.execute(query)
@@ -11,6 +15,10 @@ class EffortSegment < ApplicationRecord
   def self.delete_for_split_time(split_time)
     query = EffortSegmentQuery.delete_for_split_time(split_time)
     ActiveRecord::Base.connection.execute(query)
+  end
+
+  def self.set_all
+    Effort.find_each { |effort| set_for_effort(effort) }
   end
 
   def self.set_for_effort(effort)

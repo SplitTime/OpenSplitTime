abb4fd944773d87c30977696dd6ca0f7a2f15521
Mark Oveson <mark.oveson@mx.com>
2020-08-29 09:45:45 -0600

    Remove prefix from sidekiq queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = "open_split_time_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 

421de5164efb2fd19b162d37777e1e481985037e
Mark Oveson <mark.oveson@mx.com>
2020-08-30 07:52:51 -0600

    Comment out line that would otherwise require the `listen` gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.assets.quiet = true
 

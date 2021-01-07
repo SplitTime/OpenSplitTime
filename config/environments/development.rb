b753549172625b8b0b92d97f6aff4a6c0fc33cc7
Mark Oveson <mark.oveson@mx.com>
2021-01-06 20:51:35 -0700

    Update config for Rails 6.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :dalli_store, 'localhost', {:namespace => 'ost-dev', :expires_in => 1.day, :compress => true}
 

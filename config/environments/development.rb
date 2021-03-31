4a1c94bf5b78df4956d8ce8b0037ec9f0c97a83a
Mark Oveson <mark.oveson@mx.com>
2021-03-30 20:35:27 -0600

    Fix devise email sending in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Do care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: ENV["BASE_URI"], port: 3000}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

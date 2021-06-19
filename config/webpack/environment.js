7dac7c6e6dafc708a96e6abc8dc802337fe7a7f1
Mark Oveson <mark.oveson@mx.com>
2021-06-18 19:10:13 -0600

    Reinstall webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 module.exports = environment
-
-environment.config.merge({
-    externals: {
-        jquery: 'jQuery',
-    },
-})

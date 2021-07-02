3a6e1b76bf7e2af1fa1e32e3ae3b9c7ec6864db5
Mark Oveson <mark.oveson@mx.com>
2021-07-01 21:57:38 -0600

    enum status

--- app/models/import_job.rb
+++ app/models/import_job.rb
@@ -1,6 +1,13 @@
 class ImportJob < ApplicationRecord
   belongs_to :user
 
+  enum :status => {
+    :waiting => 0,
+    :processing => 1,
+    :finished => 2,
+    :failed => 3
+  }
+
   def parent_slug
     parent_type.constantize.find(parent_id).slug
   end

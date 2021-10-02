96b526c33e17890482d3f7c0047aae1ea88365d2
Mark Oveson <mark.oveson@mx.com>
2021-10-02 09:14:20 -0600

    Clean up and specs for ticket and draw logic

--- spec/models/lottery_division_spec.rb
+++ spec/models/lottery_division_spec.rb
@@ -5,4 +5,50 @@ require "rails_helper"
 RSpec.describe LotteryDivision, type: :model do
   it { is_expected.to strip_attribute(:name) }
   it { is_expected.to capitalize_attribute(:name) }
+
+  describe "#draw_ticket!" do
+    subject { lottery.divisions.find_by(name: "Slow People") }
+    let(:lottery) { lotteries(:another_new_lottery) }
+    let(:result) { subject.draw_ticket! }
+
+    context "when no tickets have been created" do
+      it "does not create a draw" do
+        expect { result }.not_to change { LotteryDraw.count }
+      end
+
+      it "returns nil" do
+        expect(result).to be_nil
+      end
+    end
+
+    context "when there are tickets available" do
+      before { lottery.delete_and_insert_tickets! }
+      it "creates a draw" do
+        expect { result }.to change { LotteryDraw.count }.by(1)
+      end
+
+      it "returns the created draw with expected attributes" do
+        expect(result).to be_a(LotteryDraw)
+        expect(result.lottery).to eq(lottery)
+        expect(result.ticket.entrant.division).to eq(subject)
+      end
+    end
+
+    context "when tickets have been created but have all been drawn" do
+      before do
+        lottery.delete_and_insert_tickets!
+        number_of_tickets = subject.tickets.count
+        expect(number_of_tickets).not_to eq(0)
+        number_of_tickets.times { subject.draw_ticket! }
+      end
+
+      it "does not create a draw" do
+        expect { result }.not_to change { LotteryDraw.count }
+      end
+
+      it "returns nil" do
+        expect(result).to be_nil
+      end
+    end
+  end
 end
--- spec/models/lottery_spec.rb
+++ spec/models/lottery_spec.rb
@@ -3,13 +3,14 @@
 require "rails_helper"
 
 RSpec.describe Lottery, type: :model do
+  subject { lotteries(:another_new_lottery) }
+
   it { is_expected.to strip_attribute(:name) }
   it { is_expected.to capitalize_attribute(:name) }
 
   describe "#generate_ticket_hashes" do
-    subject { lotteries(:another_new_lottery) }
     let(:result) { subject.generate_ticket_hashes }
-    let(:default_reference_number) { 100000 }
+    let(:default_reference_number) { 10_000 }
 
     it "returns an array the size of the aggregate sum of all tickets" do
       expect(result.size).to eq(4)
@@ -18,6 +19,40 @@ RSpec.describe Lottery, type: :model do
     it "returns hashes with expected information" do
       expect(result.first[:lottery_id]).to eq(subject.id)
       expect(result.first[:reference_number]).to eq(default_reference_number)
+    end
+  end
+
+  describe "#delete_and_insert_tickets!" do
+    let(:execute_method) { subject.delete_and_insert_tickets! }
+
+    context "when no tickets exist" do
+      before { expect(subject.tickets.count).to eq(0) }
+      it "creates the expected number of tickets" do
+        expected_ticket_count = subject.entrants.sum(:number_of_tickets)
+        execute_method
+        expect(subject.tickets.count).to eq(expected_ticket_count)
+      end
+    end
+
+    context "when tickets exist" do
+      before do
+        expected_ticket_count = subject.entrants.sum(:number_of_tickets)
+        execute_method
+        expect(subject.tickets.count).to eq(expected_ticket_count)
+      end
+
+      it "deletes existing tickets" do
+        existing_tickets = subject.tickets.to_a
+        execute_method
+        subject.reload
+        existing_tickets.each { |ticket| expect(subject.tickets).not_to include(ticket) }
+      end
+
+      it "creates new tickets" do
+        expected_ticket_count = subject.entrants.sum(:number_of_tickets)
+        execute_method
+        expect(subject.tickets.count).to eq(expected_ticket_count)
+      end
     end
   end
 end

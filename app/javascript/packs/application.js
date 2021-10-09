263ac1177df9bea96f835d0e39bc797894485b79
Mark Oveson <mark.oveson@mx.com>
2021-10-09 02:25:01 -0600

    Use stimulus webpack helpers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,7 @@ import TurboLinksAdapter from 'vue-turbolinks';
 Vue.use(TurboLinksAdapter);
 
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)

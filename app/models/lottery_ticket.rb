90ec4fa34519f5e92975242b5c61145331136cbc
Mark Oveson <mark.oveson@mx.com>
2021-10-11 23:55:22 -0600

    Spec for LotteryTicket model

--- app/models/lottery_ticket.rb
+++ app/models/lottery_ticket.rb
@@ -7,13 +7,13 @@ class LotteryTicket < ApplicationRecord
   belongs_to :entrant, class_name: "LotteryEntrant", foreign_key: "lottery_entrant_id"
   has_one :draw, class_name: "LotteryDraw", dependent: :destroy
 
+  scope :drawn, -> { with_drawn_at_attribute.where.not(drawn_at: nil) }
+  scope :undrawn, -> { with_drawn_at_attribute.where(drawn_at: nil) }
+  scope :with_drawn_at_attribute, -> { from(select("lottery_tickets.*, lottery_draws.created_at as drawn_at").left_joins(:draw), :lottery_tickets) }
   scope :with_sortable_entrant_attributes, -> do
     from(select("lottery_tickets.*, lottery_divisions.name as division_name, first_name, last_name, gender, city, state_code, state_name, country_code, country_name")
            .joins(entrant: :division), :lottery_tickets)
   end
-  scope :with_drawn_at_attribute, -> { from(select("lottery_tickets.*, lottery_draws.created_at as drawn_at").left_joins(:draw), :lottery_tickets) }
-  scope :drawn, -> { with_drawn_at_attribute.where.not(drawn_at: nil) }
-  scope :undrawn, -> { with_drawn_at_attribute.where(drawn_at: nil) }
 
   pg_search_scope :search_against_entrants,
                   against: :reference_number,

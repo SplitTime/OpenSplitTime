7c51c0704fa87e5908e19168c855547078e702ba
Mark Oveson <mark.oveson@gmail.com>
2021-10-18 23:50:52 -0600

    Update from sentry-raven to sentry-ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
   before_action :set_paper_trail_whodunnit
-  before_action :set_raven_context
+  before_action :set_sentry_context
   after_action :store_user_location!, if: :storable_location?
   helper_method :prepared_params
 
@@ -107,9 +107,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def set_raven_context
-    Raven.user_context(id: current_user&.id)
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  def set_sentry_context
+    Sentry.set_user(id: current_user&.id)
+    Sentry.set_extras(params: params.to_unsafe_h, url: request.url)
   end
 
   def jsonapi_error_object(record)

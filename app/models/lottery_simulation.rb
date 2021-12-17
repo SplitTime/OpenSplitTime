d873d181fa96cf677000ef551659e1e7ee0531b3
moveson <mark.oveson@gmail.com>
2021-12-17 07:36:35 -0700

    Spec for Lottery Simulation

--- app/models/lottery_simulation.rb
+++ app/models/lottery_simulation.rb
@@ -3,8 +3,6 @@
 class LotterySimulation < ApplicationRecord
   belongs_to :simulation_run, class_name: "LotterySimulationRun", foreign_key: "lottery_simulation_run_id"
 
-  delegate :lottery, to: :simulation_run
-
   def build
     self.ticket_ids = lottery.draws.in_drawn_order.pluck(:lottery_ticket_id)
     self.results = simulation_run.divisions.ordered_by_name.map do |division|
@@ -21,4 +19,6 @@ class LotterySimulation < ApplicationRecord
       }
     end
   end
+
+  delegate :lottery, to: :simulation_run, private: true
 end

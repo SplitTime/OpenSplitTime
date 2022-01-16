e3978ef1b92cc5824613eab54b3158fe5e81d0db
moveson <mark.oveson@gmail.com>
2022-01-15 21:09:29 -0500

    Move pretty_duration into format helper

--- app/helpers/import_jobs_helper.rb
+++ app/helpers/import_jobs_helper.rb
@@ -19,11 +19,4 @@ module ImportJobsHelper
       icon + label
     end
   end
-
-  def pretty_duration(seconds)
-    return "--:--" unless seconds.present?
-
-    parse_string = seconds < 3600 ? "%M:%S" : "%H:%M:%S"
-    Time.at(seconds).utc.strftime(parse_string)
-  end
 end

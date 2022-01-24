650bf64a0a6ef53eb89cad7569e7b34fd378cc75
moveson <mark.oveson@gmail.com>
2022-01-24 12:33:36 -0600

    Add percentages and totals to lottery simulation run show view

--- app/models/lottery_simulation_run.rb
+++ app/models/lottery_simulation_run.rb
@@ -19,6 +19,44 @@ class LotterySimulationRun < ApplicationRecord
 
   delegate :divisions, :organization, to: :lottery
 
+  def division_percent(attr, *keys)
+    keys = keys.map(&:to_s)
+    numerator = division_sum(attr, *keys)
+    return 0.0 if numerator == 0
+
+    keys.pop
+    child_keys = send(attr).values.first.dig(*keys).keys
+    key_sets_for_denominator = child_keys.map { |child_key| keys + [child_key] }
+    denominator = key_sets_for_denominator.sum { |set| division_sum(attr, *set) }
+    return 0.0 if denominator == 0
+
+
+    (numerator / denominator.to_f * 100).round(1)
+  end
+
+  def division_sum(attr, *keys)
+    keys = keys.map(&:to_s)
+    send(attr).sum { |_, division_values| division_values.dig(*keys) }
+  end
+
+  def single_division_percent(attr, division_name, *keys)
+    keys = keys.map(&:to_s)
+    numerator = single_division_sum(attr, division_name, *keys)
+    return 0.0 if numerator == 0
+
+    keys.pop
+    child_keys = send(attr)[division_name].dig(*keys).keys
+    key_sets_for_denominator = child_keys.map { |child_key| keys + [child_key] }
+    denominator = key_sets_for_denominator.sum { |set| single_division_sum(attr, division_name, *set) }
+    return 0.0 if denominator == 0
+
+    (numerator / denominator.to_f * 100).round(1)
+  end
+
+  def single_division_sum(attr, division_name, *keys)
+    send(attr)[division_name].dig(*keys)
+  end
+
   def parsed_errors
     JSON.parse(error_message || "[\"None\"]")
   end
@@ -64,7 +102,7 @@ class LotterySimulationRun < ApplicationRecord
         female_accepted_average = simulations.map { |simulation| simulation.results.dig(division_name, "accepted", "female") }.average
         male_wait_list_average = simulations.map { |simulation| simulation.results.dig(division_name, "wait_list", "male") }.average
         female_wait_list_average = simulations.map { |simulation| simulation.results.dig(division_name, "wait_list", "female") }.average
-        
+
         [
           division_name,
           {
@@ -72,7 +110,7 @@ class LotterySimulationRun < ApplicationRecord
               "male" => male_accepted_average.round(1),
               "female" => female_accepted_average.round(1),
             },
-            "accepted_%" => {
+            "accepted_percent" => {
               "male" => (male_accepted_average / (male_accepted_average + female_accepted_average) * 100).round(1),
               "female" => (female_accepted_average / (male_accepted_average + female_accepted_average) * 100).round(1),
             },
@@ -80,7 +118,7 @@ class LotterySimulationRun < ApplicationRecord
               "male" => male_wait_list_average.round(1),
               "female" => female_wait_list_average.round(1),
             },
-            "wait_list_%" => {
+            "wait_list_percent" => {
               "male" => (male_wait_list_average / (male_wait_list_average + female_wait_list_average) * 100).round(1),
               "female" => (female_wait_list_average / (male_wait_list_average + female_wait_list_average) * 100).round(1),
             },

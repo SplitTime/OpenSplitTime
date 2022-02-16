13ab93f791303c733e1aad133be6751d6819960a
moveson <mark.oveson@gmail.com>
2022-02-16 00:27:49 -0700

    Bump application defaults to Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module OpenSplitTime
   class Application < Rails::Application
     # Initialize configuration defaults for a specific Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

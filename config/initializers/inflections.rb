15f7144a7a0c4f3e5a46e3f43942794bbdaae764
moveson <mark.oveson@gmail.com>
2022-02-19 10:52:31 -0700

    Rails 7 configuration

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -15,5 +15,5 @@ end
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
+#   inflect.acronym "RESTful"
 # end

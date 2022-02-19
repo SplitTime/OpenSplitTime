b0a8a50a76b8feca77727c01abd64ddb6bd495ad
moveson <mark.oveson@gmail.com>
2022-02-18 21:10:15 -0700

    Additional randomness for factory-created event group names

--- spec/factories/event_group.rb
+++ spec/factories/event_group.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event_group do
-    name { "#{rand(2010..2020)} #{FFaker::Company.name} #{rand(1..10) * 25}" }
+    name { "#{rand(2010..2020)} #{FFaker::Company.name} #{rand(1000)}" }
     organization
 
     # Samoa causes Capybara to throw ambiguous match errors, so remove it before picking

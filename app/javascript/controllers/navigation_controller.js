4dcdcd16c81be251dbae3894967e23f8505daa4f
moveson <mark.oveson@gmail.com>
2022-02-21 23:26:14 -0700

    Remove monitor/setup shortcut keys

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -2,11 +2,7 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-    static targets = ["refreshButton", "priorButton", "nextButton", "monitorButton", "setupButton"];
-
-    connect() {
-        console.log("Connected to the navigation controller")
-    }
+    static targets = ["refreshButton", "priorButton", "nextButton"];
 
     evaluateKeyup(e) {
         let shortcutInvoked = (e.ctrlKey === true || e.target.tagName !== "INPUT") && e.altKey === false;
@@ -18,10 +14,6 @@ export default class extends Controller {
                 this.priorButtonTarget.click()
             } else if (e.key === "n" && this.hasNextButtonTarget) {
                 this.nextButtonTarget.click()
-            } else if (e.key === "m" && this.hasMonitorButtonTarget) {
-                this.monitorButtonTarget.click()
-            } else if (e.key === "s" && this.hasSetupButtonTarget) {
-                this.setupButtonTarget.click()
             }
         }
     }

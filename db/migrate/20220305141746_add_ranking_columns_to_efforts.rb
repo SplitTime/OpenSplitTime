af313301f8bb60debc0dfe57637e3cc75580f675
moveson <mark.oveson@gmail.com>
2022-03-05 16:47:40 -0700

    Change migration and add some columns

--- db/migrate/20220305141746_add_ranking_columns_to_efforts.rb
+++ db/migrate/20220305141746_add_ranking_columns_to_efforts.rb
@@ -1,13 +1,15 @@
 class AddRankingColumnsToEfforts < ActiveRecord::Migration[7.0]
   def change
-    add_column :efforts, :started, :boolean
-    add_column :efforts, :beyond_start, :boolean
-    add_column :efforts, :dropped, :boolean
-    add_column :efforts, :stopped, :boolean
-    add_column :efforts, :finished, :boolean
-    add_column :efforts, :laps_started, :integer
-    add_column :efforts, :laps_finished, :integer
     add_column :efforts, :overall_rank, :integer
     add_column :efforts, :gender_rank, :integer
+    add_column :efforts, :prior_effort_id, :integer
+    add_column :efforts, :next_effort_id, :integer
+    add_column :efforts, :stopped_split_time_id, :integer
+    add_column :efforts, :final_split_time_id, :integer
+    add_column :efforts, :started, :boolean
+    add_column :efforts, :beyond_start, :boolean
+    add_column :efforts, :stopped, :boolean
+    add_column :efforts, :dropped, :boolean
+    add_column :efforts, :finished, :boolean
   end
 end

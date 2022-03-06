1a1139d0b87f78249da26f01a63ed77603fac881
moveson <mark.oveson@gmail.com>
2022-03-06 07:46:54 -0700

    Callback to set ranking status after an event is touched

--- app/models/event.rb
+++ app/models/event.rb
@@ -39,6 +39,7 @@ class Event < ApplicationRecord
   before_validation :conform_changed_course
   before_save :add_all_course_splits
   after_save :validate_event_group
+  after_touch :set_ranking_data
 
   scope :name_search, -> (search_param) { where("events.name ILIKE ?", "%#{search_param}%") }
   scope :select_with_params, lambda { |search_param|
@@ -171,6 +172,10 @@ class Event < ApplicationRecord
 
   def add_all_course_splits
     splits << course.splits if splits.empty?
+  end
+
+  def set_ranking_data
+    ::Results::SetEventRankingData.perform!(id)
   end
 
   def validate_event_group

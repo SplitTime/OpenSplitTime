fa12388b091920107007689321471f2f5ff0e293
moveson <mark.oveson@gmail.com>
2022-03-09 00:29:15 -0700

    Fix Effort Show View spec

--- spec/view_models/effort_show_view_spec.rb
+++ spec/view_models/effort_show_view_spec.rb
@@ -1,42 +1,41 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe EffortShowView do
   subject { EffortShowView.new(effort) }
-  let(:event) { build_stubbed(:event, efforts: efforts) }
-  let(:efforts) { other_efforts + [effort] }
-
-  before do
-    allow(subject).to receive(:event).and_return(event)
-    allow(subject).to receive(:effort).and_return(effort)
-  end
+  let(:effort) { efforts(:hardrock_2014_progress_sherman) }
+  let(:other_effort_1) { efforts(:hardrock_2014_ken_bradtke) }
+  let(:other_effort_2) { efforts(:hardrock_2014_major_green) }
 
   describe "#next_problem_effort" do
     context "when the current effort is a problem effort" do
-      let(:effort) { build_stubbed(:effort, data_status: :questionable) }
+      before { effort.questionable! }
 
       context "when other problem efforts exist" do
-        let(:other_efforts) { build_stubbed_list(:effort, 2, data_status: :bad).sort_by(&:last_name) }
+        before do
+          other_effort_1.bad!
+          other_effort_2.bad!
+        end
 
         context "when the current effort is not last alphabetically" do
-          before { effort.last_name = "Aaron" }
+          before { effort.update(last_name: "Carlisle") }
 
           it "returns the next problem effort alphabetically by last name" do
-            expect(subject.next_problem_effort.last_name).to eq(other_efforts.first.last_name)
+            expect(subject.next_problem_effort).to eq(other_effort_2)
           end
         end
 
         context "when the current effort is last alphabetically" do
-          before { effort.last_name = "Zyzyx" }
+          before { effort.update(last_name: "Zyzyx") }
 
           it "returns the first problem effort" do
-            expect(subject.next_problem_effort).to eq(other_efforts.first)
+            expect(subject.next_problem_effort).to eq(other_effort_1)
           end
         end
       end
 
       context "when no other problem efforts exist" do
-        let(:other_efforts) { build_stubbed_list(:effort, 2, data_status: :good).sort_by(&:last_name) }
-
         it "returns nil" do
           expect(subject.next_problem_effort).to be_nil
         end
@@ -44,19 +43,18 @@ RSpec.describe EffortShowView do
     end
 
     context "when the current effort is not a problem effort" do
-      let(:effort) { build_stubbed(:effort, data_status: :good) }
-
       context "when other problem efforts exist" do
-        let(:other_efforts) { build_stubbed_list(:effort, 2, data_status: :bad).sort_by(&:last_name) }
+        before do
+          other_effort_1.bad!
+          other_effort_2.bad!
+        end
 
         it "returns the first problem effort alphabetically by last name" do
-          expect(subject.next_problem_effort).to eq(other_efforts.first)
+          expect(subject.next_problem_effort).to eq(other_effort_1)
         end
       end
 
       context "when no other problem efforts exist" do
-        let(:other_efforts) { build_stubbed_list(:effort, 2, data_status: :good).sort_by(&:last_name) }
-
         it "returns nil" do
           expect(subject.next_problem_effort).to be_nil
         end

ea3eeb4435562504f2c935f8d1abadd08d3fc9c8
moveson <mark.oveson@gmail.com>
2022-03-10 02:12:09 -0700

    Ground-up rewrite of the overall performance setter query

--- app/services/results/set_effort_performance_data.rb
+++ app/services/results/set_effort_performance_data.rb
@@ -2,12 +2,12 @@
 
 module Results
   class SetEffortPerformanceData
-    def self.perform!(*effort_ids)
-      new(*effort_ids).perform!
+    def self.perform!(effort_id)
+      new(effort_id).perform!
     end
 
-    def initialize(*effort_ids)
-      @effort_ids = effort_ids.flatten.presence || [0]
+    def initialize(effort_id)
+      @effort_id = effort_id
     end
 
     def perform!
@@ -16,153 +16,69 @@ module Results
 
     private
 
-    attr_reader :effort_ids
+    attr_reader :effort_id
 
     def query
-      effort_ids_string = effort_ids.join(",")
-
       <<~SQL.squish
-        with scoped_efforts as (
-            select id, event_id
-            from efforts
-            where efforts.id in (#{effort_ids_string})
-        ),
+        with relevant_effort as (select * from efforts where id = #{effort_id}),
 
-             course_subquery as (
-                 select courses.id                 as course_id,
-                        splits.distance_from_start as course_distance
-                 from courses
-                          join splits on splits.course_id = courses.id
-                          join events on events.course_id = courses.id
-                 where splits.kind = 1
-                   and events.id in (select distinct on (event_id) event_id from scoped_efforts)
+             starting_split_time as (
+                 select st.id, st.absolute_time
+                 from relevant_effort e
+                          left join split_times st on st.effort_id = e.id
+                          left join splits s on st.split_id = s.id
+                 where st.lap = 1
+                   and s.kind = 0
              ),
 
-             start_times as (
-                 select effort_id, absolute_time
-                 from split_times
-                          join splits on splits.id = split_times.split_id
-                 where split_times.effort_id in (select id from scoped_efforts)
-                   and split_times.lap = 1
-                   and splits.kind = 0
-                 order by split_times.effort_id
-             ),
-
-             beyond_start_efforts as (
-                 select distinct on (effort_id) effort_id
-                 from split_times
-                          join splits on splits.id = split_times.split_id
-                 where split_times.effort_id in (select id from scoped_efforts)
-                   and (kind != 0 or lap != 1)
-                 order by effort_id
-             ),
-
-             stopped_split_times as (
-                 select split_times.effort_id,
-                        split_times.id as stopped_split_time_id
-                 from split_times
-                 where split_times.effort_id in (select id from scoped_efforts)
-                   and split_times.stopped_here
-             ),
-
-             final_split_times as (
-                 select distinct on (split_times.effort_id) split_times.effort_id,
-                                                            split_times.id               as final_split_time_id,
-                                                            splits.distance_from_start   as final_lap_distance,
-                                                            split_times.lap              as final_lap,
-                                                            split_times.split_id         as final_split_id,
-                                                            split_times.sub_split_bitkey as final_bitkey,
-                                                            split_times.absolute_time    as final_absolute_time,
-                                                            split_times.elapsed_seconds  as final_elapsed_seconds,
-                                                            splits.kind = 1              as final_lap_complete
-                 from split_times
-                          join splits on splits.id = split_times.split_id
-                 where split_times.effort_id in (select id from scoped_efforts)
-                 order by split_times.effort_id,
-                          final_lap desc,
-                          final_lap_distance desc,
-                          final_bitkey desc
-             ),
-
-             base_subquery as (
-                 select scoped_efforts.id                                                  as effort_id,
-                        events.laps_required,
-                        stopped_split_time_id,
-                        course_distance,
-                        final_split_time_id,
-                        final_lap,
-                        final_lap_distance,
-                        final_bitkey,
-                        final_elapsed_seconds,
-                        (final_lap - 1) * course_distance + final_lap_distance             as final_distance,
-                        final_lap is not null                                              as started,
-                        bse.effort_id is not null                                          as beyond_start,
-                        case when final_lap is null then 0
-                             when final_lap_complete then final_lap 
-                             else final_lap - 1 
-                        end as laps_finished
-                 from scoped_efforts
-                          left join events on events.id = scoped_efforts.event_id
-                          left join course_subquery on events.course_id = course_subquery.course_id
-                          left join start_times on start_times.effort_id = scoped_efforts.id
-                          left join beyond_start_efforts bse on bse.effort_id = scoped_efforts.id
-                          left join stopped_split_times on stopped_split_times.effort_id = scoped_efforts.id
-                          left join final_split_times on final_split_times.effort_id = scoped_efforts.id
-                 order by effort_id
-             ),
-
-             base_with_finished as (
-                 select *,
+             last_split_time as (
+                 select st.id,
+                        st.lap,
+                        st.sub_split_bitkey,
+                        st.absolute_time,
+                        s.distance_from_start,
                         case
-                            when laps_required = 0
-                                then stopped_split_time_id is not null
-                            else
-                                laps_finished >= laps_required
-                            end
-                            as finished
-                 from base_subquery
+                            when ev.laps_required = 0 then null
+                            else ((s.kind = 1 and st.lap = ev.laps_required) or st.lap > ev.laps_required) end as fixed_lap_finish
+                 from relevant_effort e
+                          join events ev on ev.id = e.event_id
+                          left join split_times st on st.effort_id = e.id
+                          left join splits s on st.split_id = s.id
+                 order by st.lap desc, s.distance_from_start desc, st.sub_split_bitkey desc
+                 limit 1
              ),
 
-             base_with_status as (
-                 select *,
-                        (finished or stopped_split_time_id is not null)                  as stopped,
-                        (finished or stopped_split_time_id is not null) and not finished as dropped
-                 from base_with_finished
-             ),
-
-             update_data as (
-                 select effort_id,
-                        stopped_split_time_id,
-                        final_split_time_id,
-                        started,
-                        beyond_start,
-                        stopped,
-                        dropped,
-                        finished,
-                        case
-                            when started
-                                then (not dropped)::int::bit(1) || final_lap::bit(14) || final_distance::bit(30) ||
-                                     final_bitkey::bit(7) || ~(coalesce(final_elapsed_seconds * 1000, -1))::bigint::bit(44)
-                            else
-                                0::bit(96)
-                            end
-                            as overall_performance
-
-                 from base_with_status
-                 order by effort_id
+             stopped_split_time as (
+                 select st.id
+                 from relevant_effort e
+                          left join split_times st on st.effort_id = e.id
+                          left join splits s on st.split_id = s.id
+                 where st.stopped_here
+                 order by st.lap desc, s.distance_from_start desc, st.sub_split_bitkey desc
+                 limit 1
              )
 
         update efforts
-        set stopped_split_time_id = update_data.stopped_split_time_id,
-            final_split_time_id   = update_data.final_split_time_id,
-            started               = update_data.started,
-            beyond_start          = update_data.beyond_start,
-            stopped               = update_data.stopped,
-            dropped               = update_data.dropped,
-            finished              = update_data.finished,
-            overall_performance   = update_data.overall_performance
-        from update_data
-        where efforts.id = update_data.effort_id;
+        set stopped_split_time_id = stop_st.id,
+            final_split_time_id   = last_st.id,
+            started               = last_st.id is not null,
+            beyond_start          = coalesce(start_st.id <> last_st.id, last_st.id is not null),
+            stopped               = stop_st.id is not null or last_st.fixed_lap_finish is true,
+            dropped               = stop_st.id is not null and last_st.fixed_lap_finish is false,
+            finished              = coalesce(last_st.fixed_lap_finish, stop_st.id is not null),
+            overall_performance   = case
+                                        when last_st.id is not null then
+                                                            (stop_st.id is null or last_st.fixed_lap_finish is not false)::int::bit(1) ||
+                                                            last_st.lap::bit(14) ||
+                                                            last_st.distance_from_start::bit(30) ||
+                                                            last_st.sub_split_bitkey::bit(7) ||
+                                                            coalesce(~(extract(epoch from (last_st.absolute_time - start_st.absolute_time)) * 1000)::bigint, 0)::bigint::bit(44)
+                                        else 0::bit(96) end
+        from relevant_effort ef
+                 left join starting_split_time start_st on true
+                 left join last_split_time last_st on true
+                 left join stopped_split_time stop_st on true
+        where efforts.id = ef.id
       SQL
     end
   end

7e40454cb5b6c96afbd5d7962d878ebe7dfa60e2
moveson <mark.oveson@gmail.com>
2022-05-31 23:15:26 -0600

    Final tweaks to environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require_relative("../initializers/01_ost_config")
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.

84e411a805fe2fd31849fb432ae2ccf61c7d744a
moveson <mark.oveson@gmail.com>
2022-06-22 08:25:15 -0600

    Update the stimulus manifest

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,56 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import AnimationController from "./animation_controller.js"
+application.register("animation", AnimationController)
+
+import ApplicationController from "./application_controller.js"
+application.register("application", ApplicationController)
+
+import CarmenController from "./carmen_controller.js"
+application.register("carmen", CarmenController)
+
+import ConfirmController from "./confirm_controller.js"
+application.register("confirm", ConfirmController)
+
+import EventSetupController from "./event_setup_controller.js"
+application.register("event-setup", EventSetupController)
+
+import FinishLineController from "./finish_line_controller.js"
+application.register("finish-line", FinishLineController)
+
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
+
+import HighlightController from "./highlight_controller.js"
+application.register("highlight", HighlightController)
+
+import InfiniteScrollController from "./infinite_scroll_controller.js"
+application.register("infinite-scroll", InfiniteScrollController)
+
+import InputmaskController from "./inputmask_controller.js"
+application.register("inputmask", InputmaskController)
+
+import LoginController from "./login_controller.js"
+application.register("login", LoginController)
+
+import MapsController from "./maps_controller.js"
+application.register("maps", MapsController)
+
+import NavigationController from "./navigation_controller.js"
+application.register("navigation", NavigationController)
+
+import NotificationController from "./notification_controller.js"
+application.register("notification", NotificationController)
+
+import PopoverController from "./popover_controller.js"
+application.register("popover", PopoverController)
+
+import ResultsTemplateController from "./results_template_controller.js"
+application.register("results-template", ResultsTemplateController)
+
+import RosterController from "./roster_controller.js"
+application.register("roster", RosterController)
+
+import SidebarController from "./sidebar_controller.js"
+application.register("sidebar", SidebarController)

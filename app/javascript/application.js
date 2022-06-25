43bbba84fd7a09e16c1a849867db23aec0446274
moveson <mark.oveson@gmail.com>
2022-06-24 21:05:23 -0600

    Remove unnecessary requires from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-// import "./vue_controllers"
-// import * as bootstrap from "bootstrap"
 
 require("@hotwired/turbo-rails")
 
 import "./src/jquery"
-// import "./src/jquery-ui"
-
-$(function () {
-    console.log("hello world")
-})
+import "./src/jquery-ui"
 
 import './src/utils/growl';
 import "chartkick/chart.js";
-
-import {Application} from "@hotwired/stimulus"
-
-const application = Application.start()
-// const context = require.context("controllers", true, /.js$/)

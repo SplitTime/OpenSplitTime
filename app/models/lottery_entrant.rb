107d8114dc8d863babe02672b2eaef75d6388034
moveson <mark.oveson@gmail.com>
2022-06-30 19:27:36 -0600

    Exclude withdrawn entrants from accepted list and wait list

--- app/models/lottery_entrant.rb
+++ app/models/lottery_entrant.rb
@@ -23,6 +23,9 @@ class LotteryEntrant < ApplicationRecord
     from(select("distinct on (lottery_tickets.lottery_entrant_id) lottery_entrants.*, lottery_draws.created_at as drawn_at")
            .left_joins(tickets: :draw).order("lottery_tickets.lottery_entrant_id, drawn_at"), :lottery_entrants)
   }
+  scope :not_withdrawn, -> { where(withdrawn: [false, nil]) }
+  scope :withdrawn, -> { where(withdrawn: true) }
+
   scope :ordered, -> { order(:drawn_at) }
   scope :ordered_for_export, -> { with_division_name.order("division_name, last_name") }
   scope :pre_selected, -> { where(pre_selected: true) }

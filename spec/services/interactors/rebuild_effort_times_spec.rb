4c3b8c1e078f20d692f95716f6eeac22ec7aab23
moveson <mark.oveson@gmail.com>
2022-10-09 00:25:06 -0600

    Use matchable bib number when rebuilding efforts

--- spec/services/interactors/rebuild_effort_times_spec.rb
+++ spec/services/interactors/rebuild_effort_times_spec.rb
@@ -113,6 +113,35 @@ RSpec.describe Interactors::RebuildEffortTimes do
         end
       end
 
+      context "when raw_times have a leading 0" do
+        before do
+          raw_time = RawTime.last
+          raw_time.update(bib_number: "0#{effort.bib_number}")
+        end
+
+        it "reorders the split_times, retaining sub_split integrity" do
+          old_split_times = ordered_split_times.dup
+          response = subject.perform!
+          expect(response).to be_successful
+          expect(effort.ordered_split_times.map(&:absolute_time)).to eq(disordered_absolute_times.sort)
+          expect(effort.ordered_split_times.map(&:sub_split)).to match_array(old_split_times.map(&:sub_split))
+          expect(effort.ordered_split_times.map(&:lap)).to eq([1, 2, 2, 2, 3, 3])
+          expect(effort.ordered_split_times.map(&:split_id)).to eq([id_1, id_1, id_2, id_3, id_2, id_3])
+        end
+
+        it "matches raw_times with the newly created split_times" do
+          subject.perform!
+          raw_times = RawTime.where(source: "rebuild_effort_test")
+          expect(effort.ordered_split_times[1..-1].map(&:id)).to eq(raw_times.sort_by(&:absolute_time).map(&:split_time_id))
+        end
+
+        it "sets data status for the effort and all split times" do
+          subject.perform!
+          expect(effort.data_status).to eq("bad")
+          expect(effort.ordered_split_times.map(&:data_status)).to eq(%w[good good good good good bad])
+        end
+      end
+
       context "when raw_times have no absolute_time" do
         before do
           st = ordered_split_times[3]

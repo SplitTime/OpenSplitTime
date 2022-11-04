718ead3d101e8bfb32243156d50a4d79277a1a23
moveson <mark.oveson@gmail.com>
2022-11-03 22:40:27 -0600

    Course group best efforts view

--- app/parameters/course_group_parameters.rb
+++ app/parameters/course_group_parameters.rb
@@ -4,4 +4,8 @@ class CourseGroupParameters < BaseParameters
   def self.permitted
     [:id, :name, :slug, :organization_id, {course_ids: []}]
   end
+
+  def self.permitted_query
+    permitted + EffortParameters.permitted_query
+  end
 end

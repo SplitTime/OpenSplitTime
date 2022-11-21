db9853a4eefaf8f458a3a5a1591972ad9a85db0f
moveson <mark.oveson@gmail.com>
2022-11-20 20:04:33 -0700

    Extend sort param into the export

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -10,7 +10,7 @@ module ExportHelper
 
   def link_to_course_group_finishers_export_async(view_object)
     link_to fa_icon("file-csv", text: "Export CSV"),
-            export_async_organization_course_group_finishers_path(view_object.organization, view_object.course_group, filter: view_object.request[:filter]),
+            export_async_organization_course_group_finishers_path(view_object.organization, view_object.course_group, filter: view_object.request[:filter], sort: view_object.request[:sort]),
             method: :post,
             class: "btn btn-md btn-success"
   end

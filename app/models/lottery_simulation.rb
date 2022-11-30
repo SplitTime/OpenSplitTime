81de14b647ec23599338ea97784dabafc441ab09
moveson <mark.oveson@gmail.com>
2022-11-29 19:13:27 -0700

    Add entrant ids to lottery simulation results

--- app/models/lottery_simulation.rb
+++ app/models/lottery_simulation.rb
@@ -5,16 +5,25 @@ class LotterySimulation < ApplicationRecord
 
   def build
     results_array = simulation_run.divisions.ordered_by_name.map do |division|
+      accepted_male_ids = ::LotteryEntrant.from(division.accepted_entrants, :lottery_entrants).male.order(:drawn_at).pluck(:id)
+      accepted_female_ids = ::LotteryEntrant.from(division.accepted_entrants, :lottery_entrants).female.order(:drawn_at).pluck(:id)
+      wait_list_male_ids = ::LotteryEntrant.from(division.wait_list_entrants, :lottery_entrants).male.order(:drawn_at).pluck(:id)
+      wait_list_female_ids = ::LotteryEntrant.from(division.wait_list_entrants, :lottery_entrants).female.order(:drawn_at).pluck(:id)
+
       [
         division.name,
         {
           accepted: {
-            male: ::LotteryEntrant.from(division.accepted_entrants, :lottery_entrants).male.count,
-            female: ::LotteryEntrant.from(division.accepted_entrants, :lottery_entrants).female.count,
+            male: accepted_male_ids.size,
+            male_entrant_ids: accepted_male_ids,
+            female: accepted_female_ids.size,
+            female_entrant_ids: accepted_female_ids,
           },
           wait_list: {
-            male: ::LotteryEntrant.from(division.wait_list_entrants, :lottery_entrants).male.count,
-            female: ::LotteryEntrant.from(division.wait_list_entrants, :lottery_entrants).female.count,
+            male: wait_list_male_ids.size,
+            male_entrant_ids: wait_list_male_ids,
+            female: wait_list_female_ids.size,
+            female_entrant_ids: wait_list_female_ids,
           }
         }
       ]

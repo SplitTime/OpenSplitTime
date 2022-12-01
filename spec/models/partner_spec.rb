5292fc4689e02975f337ede79a38fa54a72d9e20
moveson <mark.oveson@gmail.com>
2022-12-01 08:20:02 -0700

    Remove initialization test from Partner spec

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -5,14 +5,4 @@ require "rails_helper"
 RSpec.describe Partner, type: :model do
   it { is_expected.to strip_attribute(:name).collapse_spaces }
   it { is_expected.to strip_attribute(:banner_link).collapse_spaces }
-
-  describe "#initialize" do
-    it "initializes with an event_id, a name, and a weight" do
-      partner = build_stubbed(:partner)
-      expect(partner.event_group_id).to be_present
-      expect(partner.name).to be_present
-      expect(partner.weight).to be_present
-      expect(partner).to be_valid
-    end
-  end
 end

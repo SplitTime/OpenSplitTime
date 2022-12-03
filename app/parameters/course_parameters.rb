12173841d70698e0d14d839db2000b8bef56b9c4
moveson <mark.oveson@gmail.com>
2022-12-03 00:48:21 -0700

    Restore organization_id to permitted params (for API)

--- app/parameters/course_parameters.rb
+++ app/parameters/course_parameters.rb
@@ -2,7 +2,7 @@
 
 class CourseParameters < BaseParameters
   def self.permitted
-    [:id, :slug, :name, :description, :next_start_time, :next_start_time_local, :gpx, :delete_gpx,
+    [:id, :slug, :name, :description, :next_start_time, :next_start_time_local, :gpx, :delete_gpx, :organization_id,
      {splits_attributes: [*SplitParameters.permitted]}]
   end
 

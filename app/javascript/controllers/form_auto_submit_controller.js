b86d55765dfd75cf60abe158903b076311209325
moveson <mark.oveson@gmail.com>
2022-12-11 20:10:06 -0700

    Update service completed indicator with turbo stream

--- app/javascript/controllers/form_auto_submit_controller.js
+++ app/javascript/controllers/form_auto_submit_controller.js
@@ -3,6 +3,7 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
 
     connect() {
+        window.onbeforeunload = null
         const form = this.element
 
         Array.from(form).forEach(function (el) {

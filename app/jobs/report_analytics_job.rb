00ed09bd5d30188d39fe6b5996dfc2db38ab1417
moveson <mark.oveson@gmail.com>
2022-12-17 15:43:16 -0700

    Remove API reporting to google analytics

--- app/jobs/report_analytics_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ReportAnalyticsJob < ApplicationJob
-  queue_as :default
-
-  def perform(ga_params)
-    AnalyticsReporter.report_to_ga(ga_params: ga_params, http_client: RestClient)
-  end
-end

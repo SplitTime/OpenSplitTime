02340e54c70c4a4090f9e21e8b5d71b742da1250
moveson <mark.oveson@gmail.com>
2022-12-17 23:06:37 -0700

    System spec for madmin root page

---
+++ spec/system/madmin_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "visit the madmin root page" do
+  let(:admin) { users(:admin_user) }
+  let(:user) { users(:third_user) }
+
+  scenario "The user is a visitor" do
+    visit madmin_root_path
+    expect(page).to have_current_path(root_path)
+  end
+
+  scenario "The user is a non-admin user" do
+    login_as user, scope: :user
+
+    visit madmin_root_path
+    expect(page).to have_current_path(root_path)
+  end
+
+  scenario "The user is an admin" do
+    login_as admin, scope: :user
+
+    visit madmin_root_path
+    expect(page).to have_current_path(madmin_root_path)
+  end
+end

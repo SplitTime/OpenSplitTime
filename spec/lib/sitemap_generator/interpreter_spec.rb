63108c35ef0b5509cf7cf4f67065a9293a4cc1df
moveson <mark.oveson@gmail.com>
2023-03-10 09:06:47 -0700

    Don't create records before testing sitemap generator

--- spec/lib/sitemap_generator/interpreter_spec.rb
+++ spec/lib/sitemap_generator/interpreter_spec.rb
@@ -8,9 +8,6 @@ RSpec.describe SitemapGenerator::Interpreter do
       allow(SitemapGenerator::Sitemap).to receive(:ping_search_engines).and_return true
       allow(SitemapGenerator::Sitemap).to receive(:create).and_yield
 
-      FactoryBot.create_list(:organization, 5)
-      FactoryBot.create_list(:person, 3)
-
       expect { described_class.run }.not_to raise_error
     end
   end

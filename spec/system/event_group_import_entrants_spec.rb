94d1b81eac9a9511dd59a64b259b6fb0e70ddb48
moveson <mark.oveson@gmail.com>
2023-03-10 00:51:12 -0700

    System spec for entrant import

---
+++ spec/system/event_group_import_entrants_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Import entrants from the event group setup view", type: :system, js: true do
+  let(:user) { users(:third_user) }
+  let(:owner) { users(:fourth_user) }
+  let(:steward) { users(:fifth_user) }
+  let(:admin) { users(:admin_user) }
+
+  before do
+    organization.update(created_by: owner.id)
+    organization.stewards << steward
+  end
+
+  let(:event_group) { event_groups(:sum) }
+  let(:organization) { event_group.organization }
+
+  scenario "the user is a visitor" do
+    visit_page
+
+    expect(page).to have_current_path(root_path)
+    verify_alert("You need to sign in or sign up before continuing")
+  end
+
+  scenario "the user is not an owner or steward" do
+    login_as user, scope: :user
+    visit_page
+
+    expect(page).to have_current_path(root_path)
+    verify_alert("Access denied")
+  end
+
+  scenario "the user is a steward" do
+    login_as steward, scope: :user
+    visit_page
+
+    expect(page).to have_current_path(root_path)
+    verify_alert("Access denied")
+  end
+
+  scenario "the user is the organization owner" do
+    login_as owner, scope: :user
+    visit_page
+
+    validate_import_job_created
+  end
+
+  scenario "the user is an admin" do
+    login_as admin, scope: :user
+    visit_page
+
+    validate_import_job_created
+  end
+
+  private
+
+  def visit_page
+    params = {
+      import_job: {
+        format: :event_group_entrants,
+        parent_type: "EventGroup",
+        parent_id: event_group.id,
+      }
+    }
+
+    visit new_import_job_path(params)
+  end
+
+  def validate_import_job_created
+    attach_file("import_job[files]", file_fixture("test_efforts_utf_8.csv"))
+    expect { click_button "Import" }.to change(ImportJob, :count).by(1)
+    expect(current_path).to eq(import_jobs_path)
+  end
+end

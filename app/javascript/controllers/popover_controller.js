a3b80e1e0920d5cb10f96e75e28dddb62c0f5592
moveson <mark.oveson@gmail.com>
2023-03-21 00:43:54 -0600

    Get homepage popovers working

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -1,64 +1,60 @@
 import { Controller } from "@hotwired/stimulus"
-import { isMobileSafari } from "utils";
-
-function safeParse(str) {
-    try {
-        return JSON.parse(str);
-    } catch (e) {
-        return null;
-    }
-}
 
 export default class extends Controller {
-    connect() {
-        let effortIds = safeParse(this.element.dataset.effortIds);
-        let theme = this.element.dataset.theme;
-        let $self = $(this.element);
-        $self.attr('tabindex', $self.attr('tabindex') || '0')
-            .attr('role', 'button')
-            .data('ajax', null)
-            .css('cursor', 'pointer')
-            .popover({
-                'html': true,
-                'trigger': 'focus',
-                'container': 'body'
-            });
-        let popover = $self.data('bs.popover');
-        this.allowListAdditions(popover.config);
-        $self.on('show.bs.popover', (event) => {
-            if (effortIds) {
-                var ajax = $self.data('ajax');
-                if (!ajax || typeof ajax.status == 'undefined') {
-                    $(popover.tip).addClass('efforts-popover');
-                    var data = {effortIds};
-                    $self.data('ajax', $.post('/efforts/mini_table/', data)
-                        .done(function (response) {
-                            popover.config.content = response;
-                            popover.show();
-                        }).always(function () {
-                            $self.data('ajax', null);
-                        })
-                    );
-                    event.preventDefault();
-                }
-            } else {
-                $(popover.tip).addClass('static-popover');
-                if (theme) {
-                    $(popover.tip).addClass(`static-popover-${theme}`);
-                }
-            }
-        });
-        if (isMobileSafari()) {
-            $('body').css('cursor', 'pointer');
-        }
-    }
+  static values = {
+    theme: String
+  }
 
-    allowListAdditions(popoverConfig) {
-        popoverConfig.allowList['table'] = [];
-        popoverConfig.allowList['thead'] = [];
-        popoverConfig.allowList['tbody'] = [];
-        popoverConfig.allowList['tr'] = [];
-        popoverConfig.allowList['td'] = [];
-        popoverConfig.allowList['th'] = [];
-    }
+  connect() {
+    const theme = this.themeValue
+    const popover = window.bootstrap.Popover.getInstance(this.element)
+
+    this.element.addEventListener("inserted.bs.popover", function () {
+      popover.tip.classList.add("static-popover");
+
+      if (theme) {
+        popover.tip.classList.add(`static-popover-${theme}`);
+      }
+    })
+  }
+
+  // connect() {
+  //     let effortIds = safeParse(this.element.dataset.effortIds);
+  //     let theme = this.element.dataset.theme;
+  //     let $self = $(this.element);
+  //     $self.attr('tabindex', $self.attr('tabindex') || '0')
+  //         .attr('role', 'button')
+  //         .data('ajax', null)
+  //         .css('cursor', 'pointer')
+  //         .popover({
+  //             'html': true,
+  //             'trigger': 'focus',
+  //             'container': 'body'
+  //         });
+  //     let popover = $self.data('bs.popover');
+  //     this.allowListAdditions(popover.config);
+  //     $self.on('show.bs.popover', (event) => {
+  //         if (effortIds) {
+  //             var ajax = $self.data('ajax');
+  //             if (!ajax || typeof ajax.status == 'undefined') {
+  //                 $(popover.tip).addClass('efforts-popover');
+  //                 var data = {effortIds};
+  //                 $self.data('ajax', $.post('/efforts/mini_table/', data)
+  //                     .done(function (response) {
+  //                         popover.config.content = response;
+  //                         popover.show();
+  //                     }).always(function () {
+  //                         $self.data('ajax', null);
+  //                     })
+  //                 );
+  //                 event.preventDefault();
+  //             }
+  //         } else {
+  //             $(popover.tip).addClass('static-popover');
+  //             if (theme) {
+  //                 $(popover.tip).addClass(`static-popover-${theme}`);
+  //             }
+  //         }
+  //     });
+  // }
 }

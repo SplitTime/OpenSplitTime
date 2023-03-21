4ffde3d844371df9aff8e5f13ff2c2eac8103396
moveson <mark.oveson@gmail.com>
2023-03-21 01:10:57 -0600

    More tooltip and popover work

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = {
-    theme: String
+    effortIds: Array,
+    theme: String,
   }
 
   connect() {

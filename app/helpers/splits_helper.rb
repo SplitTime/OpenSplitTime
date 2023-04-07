2633ef8866baa9a93122500cd9095804798d4730
moveson <mark.oveson@gmail.com>
2023-04-07 17:24:01 -0600

    Begin adding split edit feature

--- app/helpers/splits_helper.rb
+++ app/helpers/splits_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module SplitsHelper
+  def link_to_split_edit(split)
+    url = edit_split_path(split)
+    tooltip = "Edit this split"
+    options = { data: { controller: :tooltip,
+                        bs_placement: :bottom,
+                        bs_original_title: tooltip },
+                class: "btn btn-primary" }
+    link_to fa_icon("pencil-alt"), url, options
+  end
+
   def set_course_and_event
     if params[:event_id]
       @event = Event.friendly.find(params[:event_id])

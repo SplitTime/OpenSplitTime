e36252685b7794d844f1ed3bd811edf48bd7387a
moveson <mark.oveson@gmail.com>
2023-04-08 15:25:45 -0600

    Add a separate large version of the form_modal turbo frame

--- app/helpers/splits_helper.rb
+++ app/helpers/splits_helper.rb
@@ -4,7 +4,7 @@ module SplitsHelper
   def link_to_event_split_edit(event, split)
     url = edit_event_group_event_split_path(event.event_group, event, split)
     tooltip = "Edit this split"
-    options = { data: { turbo_frame: "form_modal",
+    options = { data: { turbo_frame: "form_modal_large",
                         controller: "tooltip",
                         bs_placement: :bottom,
                         bs_original_title: tooltip },

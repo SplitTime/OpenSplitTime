bc3fd94ebb03989bac5af687e4d200377bfbd3fa
Mark Oveson <mark.oveson@gmail.com>
2023-04-12 07:42:50 -0600

    Fully working widget except for Summary view

--- app/helpers/event_group_setup_widget_helper.rb
+++ app/helpers/event_group_setup_widget_helper.rb
@@ -2,27 +2,12 @@
 
 module EventGroupSetupWidgetHelper
   def link_to_setup_widget_course(presenter, event)
-    if event.persisted?
-      type = presenter.controller_name == "events" && presenter.action_name == "setup_course" && presenter.event == event ? :solid : :regular
-      path = setup_course_event_group_event_path(event.event_group, event)
-      tooltip = event.course.name
-    else
-      type = :regular
-      path = nil
-      tooltip = "You'll create or associate a Course when you set up your new Event"
-    end
+    type = presenter.controller_name == "events" && presenter.action_name == "setup_course" && presenter.event == event ? :solid : :regular
+    path = setup_course_event_group_event_path(event.event_group, event)
+    tooltip = event.course.name
 
-    if event.course.present?
-      icon = fa_icon("check-circle", type: type, size: "2x", data: { controller: "tooltip", bs_original_title: tooltip })
-      link_to icon, path
-    else
-      fa_icon "question-circle",
-              type: type,
-              size: "2x",
-              class: "text-black",
-              style: "--bs-text-opacity: .3;",
-              data: { controller: "tooltip", bs_original_title: tooltip }
-    end
+    icon = fa_icon("check-circle", type: type, size: "2x", data: { controller: "tooltip", bs_original_title: tooltip })
+    link_to icon, path
   end
 
   def link_to_setup_widget_entrants(presenter)
@@ -43,17 +28,26 @@ module EventGroupSetupWidgetHelper
   end
 
   def link_to_setup_widget_event(presenter, event)
-    if event.persisted?
-      type = presenter.controller_name == "events" && presenter.action_name == "edit" && presenter.event == event ? :solid : :regular
-      path = edit_event_group_event_path(event.event_group, event)
-      tooltip = event.guaranteed_short_name
-    else
-      type = presenter.controller_name == "events" && presenter.action_name == "new" ? :solid : :regular
-      path = new_event_group_event_path(presenter.event_group)
-      tooltip = "New Event"
-    end
+    type = presenter.controller_name == "events" && presenter.action_name == "edit" && presenter.event == event ? :solid : :regular
+    path = edit_event_group_event_path(event.event_group, event)
+    tooltip = event.guaranteed_short_name
 
     icon = fa_icon("check-circle", type: type, size: "2x", data: { controller: "tooltip", bs_original_title: tooltip })
+    link_to icon, path
+  end
+
+  def link_to_setup_widget_new_event(presenter)
+    if presenter.controller_name == "events" && presenter.action_name == "new"
+      type = :solid
+      tooltip = ""
+    else
+      type = :regular
+      tooltip = "Add an Event"
+    end
+
+    path = new_event_group_event_path(presenter.event_group)
+
+    icon = fa_icon("plus-square", type: type, size: "2x", data: { controller: "tooltip", bs_original_title: tooltip })
     link_to icon, path
   end
 

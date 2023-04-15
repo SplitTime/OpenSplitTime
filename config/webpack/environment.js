f1f7f04b36b9960b82368dc24b26304db6b8b0e4
Mark Oveson <mark.oveson@gmail.com>
2023-04-15 01:47:23 -0400

    Add sass loader to webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,17 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+    loader: 'resolve-url-loader',
+    options: {
+        attempts: 1
+    }
+});
+
+// Add an additional plugin of your choosing : ProvidePlugin
+
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
         $: 'jquery',
         JQuery: 'jquery',
@@ -11,6 +22,7 @@ environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
 const aliasConfig = {
     'jquery': 'jquery/src/jquery',
     'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+
 };
 
 environment.config.set('resolve.alias', aliasConfig);

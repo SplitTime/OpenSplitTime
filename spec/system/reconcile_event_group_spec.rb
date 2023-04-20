b38199af3cd0fe10cb4c7c1c7f788783f91b2a58
moveson <mark.oveson@gmail.com>
2023-04-19 21:15:09 -0600

    Add widget methods to reconcile presenter and add reconcile system spec

---
+++ spec/system/reconcile_event_group_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "visit the edit event group page and make changes", type: :system, js: true do
+  let(:user) { users(:third_user) }
+  let(:owner) { users(:fourth_user) }
+  let(:steward) { users(:fifth_user) }
+  let(:admin) { users(:admin_user) }
+
+  before do
+    organization.update(created_by: owner.id)
+    organization.stewards << steward
+  end
+
+  let(:event_group) { event_groups(:sum) }
+  let(:organization) { event_group.organization }
+
+  scenario "The user is a visitor" do
+    visit_page
+
+    expect(page).to have_current_path(root_path)
+    verify_alert("You need to sign in or sign up before continuing")
+  end
+
+  scenario "The user is a user that is not authorized to edit the event group" do
+    login_as user, scope: :user
+
+    visit_page
+
+    expect(page).to have_current_path(root_path)
+    verify_alert("Access denied")
+  end
+
+  context "when all entrants have been reconciled" do
+    scenario "The user is a steward of the organization" do
+      login_as steward, scope: :user
+
+      visit_page
+      expect(page).to have_content("All entrants have been reconciled")
+    end
+
+    scenario "The user is the owner of the organization" do
+      login_as owner, scope: :user
+
+      visit_page
+      expect(page).to have_content("All entrants have been reconciled")
+    end
+
+    scenario "The user is an admin" do
+      login_as admin, scope: :user
+
+      visit_page
+      expect(page).to have_content("All entrants have been reconciled")
+    end
+  end
+
+  context "when some entrants have not been reconciled" do
+    before do
+      event_group.efforts.first.update(person: nil)
+    end
+
+    scenario "The user is a steward of the organization" do
+      login_as steward, scope: :user
+
+      visit_page
+      expect(page).to have_content(/Showing \d+ of \d+ unreconciled efforts/)
+    end
+
+    scenario "The user is the owner of the organization" do
+      login_as owner, scope: :user
+
+      visit_page
+      expect(page).to have_content(/Showing \d+ of \d+ unreconciled efforts/)
+    end
+
+    scenario "The user is an admin" do
+      login_as admin, scope: :user
+
+      visit_page
+      expect(page).to have_content(/Showing \d+ of \d+ unreconciled efforts/)
+    end
+  end
+
+  def visit_page
+    visit reconcile_event_group_path(event_group)
+  end
+end

a78d32a849d86324315d2fe132dc57c6c03714f7
moveson <mark.oveson@gmail.com>
2023-04-20 09:16:52 -0600

    Remove stimulus login controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,8 +6,6 @@ module Users
       respond_to do |format|
         format.html { super }
         format.turbo_stream do
-          # When sign in fails devise redirects to new path using turbo_stream format
-          # To update the sign in form with errors we use turbo_stream.replace
           locals = { resource: User.new, resource_name: :user }
           render turbo_stream: turbo_stream.replace("form_modal", partial: "devise/sessions/form", locals: locals)
         end

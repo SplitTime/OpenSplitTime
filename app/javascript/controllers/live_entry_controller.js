dd4ad4c237707d2178e473394ebbe9e6ef4bd12a
moveson <mark.oveson@gmail.com>
2023-04-27 10:29:57 -0600

    Use turbo for trigger_raw_times_push

---
+++ app/javascript/controllers/live_entry_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
+
+export default class extends Controller {
+
+  static values = {
+    eventGroupId: Number,
+  }
+
+  connect() {
+
+    console.log("LiveEntryController#connect")
+
+    this.triggerRawTimesPush();
+  }
+
+  triggerRawTimesPush() {
+    const url = `/live/event_groups/${this.eventGroupIdValue}/trigger_raw_times_push`
+    const options = {
+      responseKind: "turbo-stream"
+    }
+
+    get(url, options)
+  }
+}

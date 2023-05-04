7f7205848aa1c24fb73f29138c44fceba4a159cd
moveson <mark.oveson@gmail.com>
2023-05-04 09:50:30 -0600

    Get podium working again

--- app/models/results_category.rb
+++ app/models/results_category.rb
@@ -40,11 +40,11 @@ class ResultsCategory < ApplicationRecord
   end
 
   def genders
-    %w[male female nonbinary].select(&method(:send))
+    %w[male female].select(&method(:send))
   end
 
   def all_genders?
-    male? && female? && nonbinary?
+    male? && female?
   end
 
   def description
@@ -70,6 +70,6 @@ class ResultsCategory < ApplicationRecord
   private
 
   def gender_present?
-    errors.add(:base, "must include male or female or nonbinary entrants") unless male? || female? || nonbinary?
+    errors.add(:base, "must include male or female or nonbinary entrants") unless male? || female?
   end
 end

fea4fbfdc839ef21e3808dd57c090c7693c49c27
moveson <mark.oveson@gmail.com>
2023-05-07 12:37:55 -0600

    Tables opt into fixture rake task instead of opting out

---
+++ lib/tasks/db/fixture_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module FixtureHelper
+  FIXTURE_TABLES = [
+    :aid_stations,
+    :course_group_courses,
+    :course_groups,
+    :courses,
+    :credentials,
+    :efforts,
+    :event_groups,
+    :event_series,
+    :event_series_events,
+    :events,
+    :lotteries,
+    :lottery_divisions,
+    :lottery_draws,
+    :lottery_entrants,
+    :lottery_tickets,
+    :notifications,
+    :organizations,
+    :partners,
+    :people,
+    :raw_times,
+    :results_categories,
+    :results_template_categories,
+    :results_templates,
+    :split_times,
+    :splits,
+    :stewardships,
+    :subscriptions,
+    :syncable_relations,
+    :users,
+  ].freeze
+
+  ATTRIBUTES_TO_IGNORE = [
+    :created_at,
+    :confirmation_sent_at,
+    :confirmation_token,
+    :remember_created_at,
+    :reset_password_token,
+    :reset_password_sent_at,
+    :updated_at,
+  ].freeze
+end

718672677ddc8b4e514fcd4e3e17ec58b783609c
moveson <mark.oveson@gmail.com>
2023-05-09 23:58:10 -0600

    Fast highlighting

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -2,20 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["element"]
-  static values = {created: Number}
+  static values = {
+    created: Number,
+    fast: Boolean,
+  }
 
   connect() {
     const five_seconds_ago = Math.round(Date.now() / 1000) - 5
-    const subjectElement = this.elementTarget
+    const subjectElement = this.element
 
     if (this.createdValue > five_seconds_ago) {
+      const cssFadeClass = this.fastValue ? "bg-highlight-faded-fast" : "bg-highlight-faded"
+      const delay = this.fastValue ? 200 : 2000
       subjectElement.classList.add("bg-highlight")
 
       setTimeout(function () {
         subjectElement.classList.remove("bg-highlight");
-        subjectElement.classList.add("bg-highlight-faded");
-      }, 2000);
+        subjectElement.classList.add(cssFadeClass);
+      }, delay);
 
     }
   }

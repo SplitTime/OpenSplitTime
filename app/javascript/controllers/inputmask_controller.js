344fb5f463fcdee25ff38898af757aa4949fa8de
moveson <mark.oveson@gmail.com>
2023-05-13 16:53:18 -0600

    Add a comment to inputmask_controller.js

--- app/javascript/controllers/inputmask_controller.js
+++ app/javascript/controllers/inputmask_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static values = {
-    type: String
+    type: String // Must be an inputmask alias; see app/javascript/packs/application.js
   }
 
   connect() {

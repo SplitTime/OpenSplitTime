5b2bf3b7ddb8ac5aad3d0861342a23a0e275de52
Mark Oveson <mark.oveson@gmail.com>
2023-05-25 15:33:59 -0600

    Simplify the Carmen controller

--- app/javascript/controllers/carmen_controller.js
+++ app/javascript/controllers/carmen_controller.js
@@ -3,11 +3,10 @@ import { get } from "@rails/request.js"
 
 export default class extends Controller {
 
-  static targets = ["countrySelect"]
   static values = { model: String }
 
   getSubregions() {
-    const countryCode = this.countrySelectTarget.value
+    const countryCode = this.element.value
     const model = this.modelValue
     const url = "/carmen/subregion_options?model=" + model + "&parent_region=" + countryCode
     const options = { responseKind: "turbo-stream" }

00fd02035de4a5cdd56dc515411d57685694f44e
moveson <mark.oveson@gmail.com>
2023-05-26 22:04:13 -0600

    Migration to add slug to results_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_025555) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_27_040014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
@@ -486,6 +486,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_025555) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.boolean "nonbinary", default: false
+    t.string "slug"
     t.index ["organization_id"], name: "index_results_categories_on_organization_id"
   end
 

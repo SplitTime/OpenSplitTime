93d25cb425f381f4a6338c78290402443a97a114
moveson <mark.oveson@gmail.com>
2023-06-09 08:03:31 -0600

    Convert user delete and impersonate buttons to Turbo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,24 +3,26 @@
 module UsersHelper
   def link_to_delete_user(user)
     url = user_path(user, referrer_path: request.params)
-    options = {method: :delete,
-               data: {confirm: "This cannot be undone. Are you sure you want to delete this record?",
-                      controller: :tooltip,
-                      bs_placement: :bottom,
-                      bs_original_title: "Delete user"},
-               id: "delete_user_#{user.id}",
-               class: "btn btn-danger"}
-    link_to fa_icon("trash"), url, options
+    options = { method: :delete,
+                data: { turbo_confirm: "This cannot be undone. Are you sure you want to delete this record?",
+                        controller: "tooltip",
+                        bs_placement: :bottom,
+                        bs_original_title: "Delete user" },
+                id: "delete_user_#{user.id}",
+                class: "btn btn-danger" }
+
+    button_to(url, options) { fa_icon("trash") }
   end
 
   def link_to_become_user(user)
     url = admin_impersonate_start_path(user)
-    options = {method: :post,
-               data: {controller: :tooltip,
-                      bs_placement: :bottom,
-                      bs_original_title: "Impersonate user"},
-               id: "impersonate_user_#{user.id}",
-               class: "btn btn-warning"}
-    link_to fa_icon("theater-masks"), url, options
+    options = { method: :post,
+                data: { controller: "tooltip",
+                        bs_placement: :bottom,
+                        bs_original_title: "Impersonate user" },
+                id: "impersonate_user_#{user.id}",
+                class: "btn btn-warning" }
+
+    button_to(url, options) { fa_icon("theater-masks") }
   end
 end

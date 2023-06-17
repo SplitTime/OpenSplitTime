f9af1a250b871b9eec96076e07fe2e0a105c3070
moveson <mark.oveson@gmail.com>
2023-06-16 18:37:38 -0600

    Update the datepicker and datetimepicker form helpers

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -2,12 +2,21 @@ import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
 export default class extends Controller {
+  static values = {
+    enableTime: {
+      type: Boolean,
+      default: false,
+    },
+  }
+
   connect() {
+    const selector = `#${this.element.id}`
+    const dateFormat = this.enableTimeValue ? "m/d/Y H:i:S" : "m/d/Y"
     const datetime = new Date(this.element.value)
 
-    flatpickr("#event_scheduled_start_time_local", {
-      enableTime: true,
-      dateFormat: "m/d/Y H:i:S",
+    flatpickr(selector, {
+      enableTime: this.enableTimeValue,
+      dateFormat: dateFormat,
       defaultDate: datetime,
     });
   }

82d25e6a523f1cc6003b019f3ee85a76d7b52107
moveson <mark.oveson@gmail.com>
2023-06-17 22:53:36 -0600

    Fix exports for course group views

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 module ExportHelper
-  def link_to_course_group_best_efforts_export_async(view_object)
-    link_to fa_icon("file-csv", text: "Export CSV"),
-            export_async_organization_course_group_best_efforts_path(view_object.organization, view_object.course_group, filter: view_object.request[:filter]),
-            method: :post,
-            class: "btn btn-md btn-success"
+  def button_to_course_group_best_efforts_export_async(view_object)
+    path = export_async_organization_course_group_best_efforts_path(
+      view_object.organization,
+      view_object.course_group,
+      filter: view_object.request[:filter]
+    )
+
+    button_to(path, method: :post, class: "btn btn-md btn-success") do
+      fa_icon("file-csv", text: "Export CSV")
+    end
   end
 
-  def link_to_course_group_finishers_export_async(view_object)
-    link_to fa_icon("file-csv", text: "Export CSV"),
-            export_async_organization_course_group_finishers_path(view_object.organization, view_object.course_group, filter: view_object.request[:filter], sort: view_object.request[:sort]),
-            method: :post,
-            class: "btn btn-md btn-success"
+  def button_to_course_group_finishers_export_async(view_object)
+    path = export_async_organization_course_group_finishers_path(
+      view_object.organization,
+      view_object.course_group,
+      filter: view_object.request[:filter],
+      sort: view_object.request[:sort]
+    )
+
+    button_to(path, method: :post, class: "btn btn-md btn-success") do
+      fa_icon("file-csv", text: "Export CSV")
+    end
   end
 end

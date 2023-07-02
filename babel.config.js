f49ddd5d0282b12f9dbcf2c881d56db9d27f23d6
moveson <mark.oveson@gmail.com>
2023-07-02 10:52:03 -0600

    Another attempt to remove babel warnings in Heroku CI

--- babel.config.js
+++ babel.config.js
@@ -49,6 +49,12 @@ module.exports = function(api) {
                 }
             ],
             [
+                '@babel/plugin-proposal-private-methods',
+                {
+                    loose: true
+                }
+            ],
+            [
                 '@babel/plugin-proposal-private-property-in-object',
                 {
                     loose: true

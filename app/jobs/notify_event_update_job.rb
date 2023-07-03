7b3fa369d0f8173db9b7247789847fe1045551f6
moveson <mark.oveson@gmail.com>
2023-07-03 09:45:46 -0600

    Don't attempt to create a Notification record for event update notifications

--- app/jobs/notify_event_update_job.rb
+++ app/jobs/notify_event_update_job.rb
@@ -3,19 +3,12 @@
 class NotifyEventUpdateJob < ApplicationJob
   def perform(event_id)
     @event_id = event_id
-
     return unless event_id.present? && event.present?
 
     response = EventUpdateNotifier.publish(topic_arn: topic_resource_key, event: event)
 
-    if response.successful?
-      notification = Notification.new(kind: :event_update, event: event, follower_ids: event.followers.ids,
-                                      subject: response.resources[:subject], notice_text: response.resources[:notice_text],
-                                      topic_resource_key: topic_resource_key)
-      unless notification.save
-        logger.error "  Unable to create notification for #{event}"
-        logger.error "  #{notification.errors.full_messages}"
-      end
+    unless response.successful?
+      raise "Failed to send event update notification for #{event.name} (#{event.id}): #{response.message_with_error_report}"
     end
   end
 

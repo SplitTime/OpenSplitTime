622378e1ee5a0e424f1718102493f8992b2cdd68
moveson <mark.oveson@gmail.com>
2023-07-03 09:37:24 -0600

    Add event_update as a valid kind for the Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,7 +3,7 @@
 class Notification < ApplicationRecord
   include Auditable
 
-  enum kind: [:participation, :progress]
+  enum kind: [:participation, :progress, :event_update]
 
   belongs_to :effort
   has_one :event, through: :effort

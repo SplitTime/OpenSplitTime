371b489d091be51ca9cf91bb95f5ac33822999a6
moveson <mark.oveson@gmail.com>
2023-07-04 07:15:54 -0600

    Simplify endpoint logic

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,38 +6,27 @@ class SubscriptionsController < ApplicationController
   before_action :set_subscription, except: [:create]
   after_action :verify_authorized
 
-  PROTOCOL_WARNINGS = {
-    "sms" => "Please add a mobile phone number to receive sms text notifications.",
-  }.freeze
-
   def create
     @subscription = @subscribable.subscriptions.new(permitted_params)
     @subscription.user = current_user
     protocol = permitted_params[:protocol]
     @subscription.endpoint = case protocol
-                             when "email"
-                               current_user.email
-                             when "sms"
-                               current_user.sms
-                             when "http", "https"
-                               params[:endpoint]
-                             else
-                               nil
+                             when "email" then current_user.email
+                             when "sms" then current_user.sms
+                             else params[:endpoint]
                              end
     authorize @subscription
 
-    if @subscription.endpoint.present?
-      if @subscription.save
-        flash.now[:success] = "You have subscribed to #{protocol} notifications for #{@subscribable.full_name}. " +
-          "Messages will be sent to #{@subscription[:endpoint]}."
-        render "replace_button", locals: { subscribable: @subscribable, protocol: protocol }
-      else
-        flash.now[:danger] = @subscription.errors.full_messages.to_sentence
-        render turbo_stream: turbo_stream.replace("flash", partial: "layouts/flash")
-      end
-    else
-      flash_protocol_warning
+    if protocol == "sms" && current_user.sms.blank?
+      flash[:warning] = "Please add a mobile phone number to receive sms text notifications."
       redirect_to user_settings_preferences_path
+    elsif @subscription.save
+      flash.now[:success] = "You have subscribed to #{protocol} notifications for #{@subscribable.full_name}. " +
+        "Messages will be sent to #{@subscription[:endpoint]}."
+      render "replace_button", locals: { subscribable: @subscribable, protocol: protocol }
+    else
+      flash.now[:danger] = @subscription.errors.full_messages.to_sentence
+      render turbo_stream: turbo_stream.replace("flash", partial: "layouts/flash")
     end
   end
 
@@ -51,10 +40,6 @@ class SubscriptionsController < ApplicationController
   end
 
   private
-
-  def flash_protocol_warning
-    flash[:warning] = PROTOCOL_WARNINGS[permitted_params[:protocol]] || "Protocol does not exist."
-  end
 
   def set_subscribable
     raise NotImplementedError, "set_subscribable must be implemented"

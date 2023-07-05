0cbb22499243e3d0c73c6ca0fa1a3fd2085caf0a
moveson <mark.oveson@gmail.com>
2023-07-05 16:43:33 -0600

    Handle case where subscription has no resource key

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -2,12 +2,20 @@
 
 module SubscriptionsHelper
   def subscription_status_badge(subscription)
-    pending = subscription.resource_key.start_with?("pending")
-    text = pending ? "Pending" : "Confirmed"
-    color = pending ? "warning" : "success"
-    tooltip_text = pending ?
-                     "This subscription is pending confirmation. If you have confirmed the endpoint, click Actions > Refresh to change its status here." :
-                     "This subscription is confirmed."
+    if subscription.resource_key.blank?
+      text = "No topic"
+      color = "danger"
+      tooltip_text = "This event does not have a topic resource key. Click Actions > Refresh to attempt to generate one, or delete this subscription and create a new one."
+    elsif subscription.pending?
+      text = "Pending"
+      color = "warning"
+      tooltip_text = "This subscription is pending confirmation. If you have confirmed the endpoint, click Actions > Refresh to change its status here."
+    else
+      text = "Confirmed"
+      color = "success"
+      tooltip_text = "This subscription is confirmed."
+    end
+
     badge_with_text(text, color: color, tooltip_text: tooltip_text)
   end
 end

330ee19387f79f03eafe1e6c76d276de07c1f443
moveson <mark.oveson@gmail.com>
2023-07-05 21:13:42 -0600

    Better subscription tooltips and move them to I18n

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -5,15 +5,15 @@ module SubscriptionsHelper
     if subscription.resource_key.blank?
       text = "No topic"
       color = "danger"
-      tooltip_text = "This event does not have a topic resource key. Click Actions > Refresh to attempt to generate one, or delete this subscription and create a new one."
+      tooltip_text = t("subscriptions.tooltips.no_topic")
     elsif subscription.pending?
       text = "Pending"
       color = "warning"
-      tooltip_text = "This subscription is pending confirmation. If you have confirmed the endpoint, click Actions > Refresh to change its status here."
+      tooltip_text = t("subscriptions.tooltips.pending")
     else
       text = "Confirmed"
       color = "success"
-      tooltip_text = "This subscription is confirmed."
+      tooltip_text = t("subscriptions.tooltips.confirmed")
     end
 
     badge_with_text(text, color: color, tooltip_text: tooltip_text)

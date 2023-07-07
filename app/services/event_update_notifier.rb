a7bdf2bc4c76c5f481031ae0adc6605ed2db7ef6
moveson <mark.oveson@gmail.com>
2023-07-06 21:31:04 -0600

    Include a properly formatted JSON message within the webhook

--- app/services/event_update_notifier.rb
+++ app/services/event_update_notifier.rb
@@ -27,20 +27,22 @@ class EventUpdateNotifier < BaseNotifier
 
   def message
     {
-      default: "#{event.name} has been updated as of #{event.updated_at}.",
+      default: "#{event.slug} (#{event.id}) was updated at #{event.updated_at}",
       http: http_message,
       https: http_message,
     }.to_json
   end
 
+  # http_message has to be converted to JSON individually and then wrapped in JSON again within the message method
   def http_message
     {
       data: {
-        event: {
-          id: event.id,
+        type: "events",
+        id: event.id,
+        attributes: {
           updated_at: event.updated_at,
         }
       }
-    }
+    }.to_json
   end
 end

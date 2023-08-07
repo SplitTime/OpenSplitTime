51cb09e0d16c54ab2a46c2961cbb52b564ff0f2b
moveson <mark.oveson@gmail.com>
2023-08-07 01:50:14 -0600

    Clean up string monkey patches

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -13,20 +13,4 @@ class String
     ActiveRecord::Type::Boolean.new.cast(self)
   end
   alias to_bool to_boolean
-
-  # Tests if string is a valid UUID v4
-  def uuid?
-    (self =~ /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i).present?
-  end
-
-  def self.longest_common_phrase(strings)
-    n = strings.min_by { |string| string.split.size }.size
-    while n > 0
-      cons_word_sets = strings.map { |string| string.downcase.split.each_cons(n).to_a }
-      matching_words = cons_word_sets.reduce(:&).first
-      return matching_words&.join(" ") if matching_words
-
-      n -= 1
-    end
-  end
 end
--- spec/lib/core_ext/string_spec.rb
+++ spec/lib/core_ext/string_spec.rb
@@ -4,127 +4,27 @@ require_relative "../../../lib/core_ext/string"
 require "active_record"
 
 RSpec.describe String do
-  describe ".longest_common_phrase" do
-    subject { String.longest_common_phrase(strings) }
+  describe "#numeric?" do
+    let(:result) { subject.numeric? }
 
-    context "when two strings have one word in common at the start of the string" do
-      let(:strings) { ["hello world", "hello curl"] }
-
-      it "returns the common word" do
-        expect(subject).to eq("hello")
-      end
+    context "when the string is entirely digits" do
+      subject { "1234" }
+      it { expect(result).to eq(true) }
     end
 
-    context "when two strings have multiple words in common at the start of the string" do
-      let(:strings) { ["hello there world how are you?", "hello there world what is happening?"] }
-
-      it "returns the common phrase" do
-        expect(subject).to eq("hello there world")
-      end
+    context "when the string represents a decimal" do
+      subject { "1234.56" }
+      it { expect(result).to eq(true) }
     end
 
-    context "when two strings match but for inconsistent case" do
-      let(:strings) { ["Hello World", "hello world how are you"] }
-
-      it "matches case-insensitive and returns a downcased result" do
-        expect(subject).to eq("hello world")
-      end
+    context "when the string is partially digits" do
+      subject { "hello1234" }
+      it { expect(result).to eq(false) }
     end
 
-    context "when two strings include a matching phrase at the end of the string" do
-      let(:strings) { ["Race of the Century", "Kids Race of the Century"] }
-
-      it "returns the matching phrase" do
-        expect(subject).to eq("race of the century")
-      end
-    end
-
-    context "when two strings include a matching phrase in the middle of the string" do
-      let(:strings) { ["Adult Race of the Century Get Ready", "Kids Race of the Century They Are So Cute"] }
-
-      it "returns the matching phrase" do
-        expect(subject).to eq("race of the century")
-      end
-    end
-
-    context "when two strings include numbers" do
-      let(:strings) { ["2017 Rattlesnake Ramble", "2017 Rattlesnake Ramble Kids Race"] }
-
-      it "includes the numbers" do
-        expect(subject).to eq("2017 rattlesnake ramble")
-      end
-    end
-
-    context "when two strings include distance designations that differ" do
-      let(:strings) { ["2017 Double Dirty 30 100K", "2017 Double Dirty 30 55K"] }
-
-      it "returns the expected result" do
-        expect(subject).to eq("2017 double dirty 30")
-      end
-    end
-
-    context "when the longest matching phrase is in the middle between shorter matching phrases" do
-      let(:strings) { ["Hello world, please come to my house next week", "Hello friends, please come to my party next week"] }
-
-      it "returns the longest matching phrase" do
-        expect(subject).to eq("please come to my")
-      end
-    end
-
-    context "when multiple strings are provided with a matching phrase" do
-      let(:strings) { ["2017 Ramble Boys", "2017 Ramble Girls", "2017 Ramble Men", "2017 Ramble Women"] }
-
-      it "returns the common phrase" do
-        expect(subject).to eq("2017 ramble")
-      end
-    end
-
-    context "when multiple strings are provided with imperfectly matching phrases" do
-      let(:strings) { ["2017 Ramble Boys", "2017 Ramble Girls", "Rattlesnake Ramble Men", "Rattlesnake Ramble Women"] }
-
-      it "returns the only common phrase" do
-        expect(subject).to eq("ramble")
-      end
-    end
-
-    context "when multiple strings are provided with no matching phrases" do
-      let(:strings) { ["2017 Ramble Boys", "2017 Ramble Girls", "Another Race"] }
-
-      it "returns the only common phrase" do
-        expect(subject).to be_nil
-      end
-    end
-
-    context "when the two strings have no words in common" do
-      let(:strings) { %w[hello world] }
-
-      it "returns nil" do
-        expect(subject).to be_nil
-      end
-    end
-
-    context "when one string is an empty string" do
-      let(:strings) { ["", "hello world"] }
-
-      it "returns nil" do
-        expect(subject).to be_nil
-      end
-    end
-
-    context "when all strings are empty strings" do
-      let(:strings) { ["", ""] }
-
-      it "returns nil" do
-        expect(subject).to be_nil
-      end
-    end
-
-    context "when only one string is given" do
-      let(:strings) { ["Hello World"] }
-
-      it "returns a downcased version of the given string" do
-        expect(subject).to eq("hello world")
-      end
+    context "when the string contains no digits" do
+      subject { "hello" }
+      it { expect(result).to eq(false) }
     end
   end
 
@@ -165,20 +65,6 @@ RSpec.describe String do
 
     it "returns false when called on an unknown string value" do
       expect("hello".to_boolean).to eq(true)
-    end
-  end
-
-  describe "#uuid?" do
-    it "returns true when the object is a valid UUID v4" do
-      expect(SecureRandom.uuid.uuid?).to eq(true)
-    end
-
-    it "returns false when the object is not a valid UUID v4" do
-      expect("Hello".uuid?).to eq(false)
-    end
-
-    it "returns false when the object is an empty string" do
-      expect("".uuid?).to eq(false)
     end
   end
 end

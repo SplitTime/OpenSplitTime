9412b0592e24d28a0612aced7c7519a26c140dfc
moveson <mark.oveson@gmail.com>
2023-08-16 21:31:13 -0600

    Lint

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,15 +1,15 @@
-import {Controller} from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-    static targets = ["wrapper"];
+  static targets = ["wrapper"];
 
-    toggle(e) {
-        e.preventDefault();
-        if (this.wrapperTarget.classList.contains("toggled")) {
-            this.wrapperTarget.classList.remove("toggled");
-        } else {
-            this.wrapperTarget.classList.add("toggled");
-        }
+  toggle(e) {
+    e.preventDefault();
+    if (this.wrapperTarget.classList.contains("toggled")) {
+      this.wrapperTarget.classList.remove("toggled");
+    } else {
+      this.wrapperTarget.classList.add("toggled");
     }
+  }
 }

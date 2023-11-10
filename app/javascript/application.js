5cf0ec289338db0c242d49eda599eb5bcc3443f5
moveson <mark.oveson@gmail.com>
2023-11-10 11:39:04 -0700

    Remove sentry JS reporting
    I never could get source maps to work, and the frontend error reporting often caused us to bust our rate limt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,31 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-// Sentry
-import * as Sentry from "@sentry/browser";
-
-Sentry.init({
-  dsn: "https://75503de427ae47638046edde0174a0ea@o361209.ingest.sentry.io/3805803",
-
-  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
-  // if your build tool supports it.
-  release: "opensplittime",
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/ost-stage\.herokuapp\.com/, /^https:\/\/opensplittime\.org/],
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
-
 // ActiveStorage
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()

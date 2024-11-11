bec798ed08a5e52ecc5dccd103124a3e5a4dbf21
moveson <mark.oveson@gmail.com>
2024-11-11 10:46:42 -0700

    HistoricalFact model and spec

---
+++ app/models/historical_fact.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class HistoricalFact < ApplicationRecord
+  enum gender: {
+    male: 0,
+    female: 1,
+    nonbinary: 2,
+  }
+
+  enum kind: {
+    dns: 0,
+    volunteer_minor: 1,
+    volunteer_major: 2,
+    volunteer_legacy: 3,
+    qualifier_finish: 4,
+  }
+
+  include Auditable
+  include CapitalizeAttributes
+
+  strip_attributes collapse_spaces: true
+  strip_attributes only: [:phone, :emergency_phone], regex: /[^0-9|+]/
+  capitalize_attributes :first_name, :last_name, :city, :emergency_contact
+end
---
+++ spec/models/historical_fact_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe HistoricalFact, type: :model do
+  it_behaves_like "auditable"
+  it { is_expected.to capitalize_attribute(:first_name) }
+  it { is_expected.to capitalize_attribute(:last_name) }
+  it { is_expected.to capitalize_attribute(:city) }
+  it { is_expected.to capitalize_attribute(:emergency_contact) }
+  it { is_expected.to strip_attribute(:first_name).collapse_spaces }
+  it { is_expected.to strip_attribute(:last_name).collapse_spaces }
+  it { is_expected.to strip_attribute(:state_code).collapse_spaces }
+  it { is_expected.to strip_attribute(:country_code).collapse_spaces }
+end

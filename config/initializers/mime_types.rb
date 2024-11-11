7713b7b965d04d47bf909eac5bf811cbadd8c8c6
moveson <mark.oveson@gmail.com>
2024-11-11 11:25:28 -0700

    Extend Marcel::MimeType definitions for active_storage_validations gem

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,3 +4,10 @@
 # Mime::Type.register "text/richtext", :rtf
 
 Mime::Type.register "application/vnd.api+json", :jsonapi
+
+# Extend Marcel::MimeType for active_storage_validations
+
+Marcel::MimeType.extend "application/xml", extensions: %w(gpx)
+Marcel::MimeType.extend "application/gpx+xml", extensions: %w(gpx), parents: "application/xml"
+Marcel::MimeType.extend "application/octet-stream", extensions: %w(gpx), parents: "application/xml"
+Marcel::MimeType.extend "text/xml", extensions: %w(gpx), parents: "application/xml"

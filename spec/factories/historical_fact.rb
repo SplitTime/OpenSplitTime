8fb7b9dd1981c6872de46a5d6ae95089a9b26801
moveson <mark.oveson@gmail.com>
2024-11-20 11:05:14 -0700

    Callback to fill personal_info_hash

--- spec/factories/historical_fact.rb
+++ spec/factories/historical_fact.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :historical_fact do
+    organization
     first_name { FFaker::Name.first_name }
     last_name { FFaker::Name.last_name }
     birthdate { FFaker::Time.date(years_back: 70, latest_year: 2005) }

3ff85bc44a35b609933f45fafcce2b92358f5967
moveson <mark.oveson@gmail.com>
2024-12-01 21:04:26 -0700

    Route, action, policy, and view for lottery calculations

--- app/models/lotteries/calculations/hardrock_2025.rb
+++ app/models/lotteries/calculations/hardrock_2025.rb
@@ -3,6 +3,12 @@
 class Lotteries::Calculations::Hardrock2025 < ApplicationRecord
   self.table_name = :lotteries_calculations_hardrock_2025s
 
+  enum gender: {
+    male: 0,
+    female: 1,
+    nonbinary: 2,
+  }
+
   belongs_to :organization
   belongs_to :person
 end
---
+++ app/presenters/lottery_calculations_presenter.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class LotteryCalculationsPresenter < LotteryPresenter
+  CalculatedGroup = Struct.new(:name, :entrants_count, :tickets_count)
+
+  delegate :calculation_class, to: :lottery
+
+  def initialize(lottery, view_context)
+    super
+    validate_setup
+  end
+
+  def division_calculations
+    @division_calculations ||=
+      lottery_calculation.group(:division).pluck(Arel.sql("division, count(*), sum(ticket_count)")).map do |row|
+        CalculatedGroup.new(*row)
+      end
+  end
+
+  def division_entrants_count
+    division_calculations.sum(&:entrants_count)
+  end
+
+  def division_tickets_count
+    division_calculations.sum(&:tickets_count)
+  end
+
+  def gender_calculations
+    @gender_calculations ||=
+      lottery_calculation.group(:gender).pluck(Arel.sql("gender, count(*), sum(ticket_count)")).map do |row|
+        CalculatedGroup.new(*row)
+      end
+  end
+
+  def gender_entrants_count
+    gender_calculations.sum(&:entrants_count)
+  end
+
+  def gender_tickets_count
+    gender_calculations.sum(&:tickets_count)
+  end
+
+  private
+
+  def calculated_division_names
+    @calculated_divisions ||= lottery_calculation.group(:division).pluck(:division)
+  end
+
+  def lottery_calculation
+    @lottery_calculation ||= lottery_calculation_class.where(organization: organization)
+  end
+
+  def lottery_calculation_class
+    "Lotteries::Calculations::#{calculation_class}".safe_constantize
+  end
+
+  def validate_setup
+    raise "Lottery must have an assigned calculation_class" unless calculation_class.present?
+    raise "Lottery calculation class does not exist" unless lottery_calculation_class.present?
+  end
+end

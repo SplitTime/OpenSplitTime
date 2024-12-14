bab5a6e62315d0a824a42206ddd4c5856196c163
moveson <mark.oveson@gmail.com>
2024-12-13 22:04:38 -0700

    UI tweaks for lottery service form downloads

--- app/presenters/lottery_presenter.rb
+++ app/presenters/lottery_presenter.rb
@@ -7,7 +7,7 @@ class LotteryPresenter < BasePresenter
   delegate :action_name, :controller_name, to: :view_context
 
   delegate :calculation_class?, :concealed?, :divisions, :draws, :entrants, :name, :organization, :scheduled_start_date, :status,
-           :tickets, :to_param, to: :lottery
+           :tickets, :to_param, :service_form, to: :lottery
   delegate :draws, :entrants, :tickets, to: :lottery, prefix: true
 
   def initialize(lottery, view_context)

7d11113ddd1be2601365e0ccf98b65cbfd7f5002
moveson <mark.oveson@gmail.com>
2024-12-18 20:30:27 -0700

    Set table_name for Analytics::FileDownload model

--- app/models/analytics/file_download.rb
+++ app/models/analytics/file_download.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Analytics::FileDownload < ApplicationRecord
+  self.table_name = "analytics_file_downloads"
+
   belongs_to :user
   belongs_to :record, polymorphic: true
 end

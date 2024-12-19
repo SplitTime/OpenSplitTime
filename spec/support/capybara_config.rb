f052e9e47eec780e3fc4d797afe4c6c73e0c8a76
moveson <mark.oveson@gmail.com>
2024-12-19 10:09:08 -0700

    Add download preference options to selenium webdrivers

--- spec/support/capybara_config.rb
+++ spec/support/capybara_config.rb
@@ -4,9 +4,16 @@ end
 
 # chrome_headless is the default driver for system tests
 Capybara.register_driver :chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[headless disable-gpu no-sandbox window-size=1536,1152 disable-dev-shm-usage],
-  )
+  download_path = Rails.root.join("tmp/downloads").to_s
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--window-size=1536,1152")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_preference(:download, directory_upgrade: true, prompt_for_download: false, default_directory: download_path)
+
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
                                  clear_session_storage: true,
@@ -18,9 +25,15 @@ end
 # passed to `driven_by` in spec/support/basic_configure.rb from
 # :chrome_headless to :chrome_visible
 Capybara.register_driver :chrome_visible do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[disable-gpu no-sandbox window-size=1536,1152 disable-dev-shm-usage],
-  )
+  download_path = Rails.root.join("tmp/downloads").to_s
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--window-size=1536,1152")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_preference(:download, directory_upgrade: true, prompt_for_download: false, default_directory: download_path)
+
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
                                  clear_session_storage: true,

57da8b0de7ed30549242a11eae71ed0008dd77b5
moveson <mark.oveson@gmail.com>
2024-12-22 03:55:25 -0700

    Fix module namespacing for UrlHelper monkey patch

--- lib/core_ext/action_view/helpers/url_helper.rb
+++ lib/core_ext/action_view/helpers/url_helper.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 module CoreExt
-  module UrlHelper
-    # Monkey Patch link_to to translate the boolean attribute "disabled" into a class
-    # Using the signature (*args, &blocks) makes this patch resistant to API changes
-    # since all args are captured as is
-    def link_to(*args, &block)
-      # Locate first hash in args with :disabled, and modify in place
-      options = args.find do |hash|
-        hash.key?(:disabled)
-      rescue StandardError
-        false
+  module ActionView
+    module Helpers
+      module UrlHelper
+        # Monkey Patch link_to to translate the boolean attribute "disabled" into a class
+        # Using the signature (*args, &blocks) makes this patch resistant to API changes
+        # since all args are captured as is
+        def link_to(*args, &block)
+          # Locate first hash in args with :disabled, and modify in place
+          options = args.find do |hash|
+            hash.key?(:disabled)
+          rescue StandardError
+            false
+          end
+          options[:class] = [options[:class], "disabled"].join(" ") if options && options[:disabled]
+          original_link_to(*args, &block) # Call original link_to
+        end
       end
-      options[:class] = [options[:class], "disabled"].join(" ") if options && options[:disabled]
-      original_link_to(*args, &block) # Call original link_to
     end
   end
 end
@@ -21,7 +25,7 @@ end
 module ActionView
   module Helpers
     module UrlHelper
-      include CoreExt::UrlHelper
+      include CoreExt::ActionView::Helpers::UrlHelper
 
       alias original_link_to link_to
     end

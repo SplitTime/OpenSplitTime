0a787c87ee9dca8b91c1d6afbccad7ef306aa205
Mark Oveson <mark.oveson@gmail.com>
2024-12-23 07:57:28 -0700

    Fix unknown local referred_by and add specs for course views (#1363)

---
+++ spec/system/visit_course_events_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Visit a course events page" do
+  let(:organization) { organizations(:hardrock) }
+  let(:course) { courses(:hardrock_cw) }
+  let(:events) { course.events }
+
+  let(:user) { users(:third_user) }
+  let(:owner) { users(:fourth_user) }
+  let(:steward) { users(:fifth_user) }
+  let(:admin) { users(:admin_user) }
+
+  before do
+    organization.update(created_by: owner.id)
+    organization.stewards << steward
+  end
+
+  scenario "Visitor visits the page" do
+    visit_page
+    verify_content_present
+  end
+
+  scenario "User visits the page" do
+    login_as user, scope: :user
+
+    visit_page
+    verify_content_present
+  end
+
+  scenario "Steward visits the page" do
+    login_as steward, scope: :user
+
+    visit_page
+    verify_content_present
+  end
+
+  scenario "Owner visits the page" do
+    login_as owner, scope: :user
+
+    visit_page
+    verify_content_present
+  end
+
+  scenario "Admin visits the page" do
+    login_as admin, scope: :user
+
+    visit_page
+    verify_content_present
+  end
+
+  def visit_page
+    visit organization_course_path(organization, course)
+  end
+
+  def verify_content_present
+    expect(page).to have_content(organization.name)
+    course.events.each { |event| expect(page).to have_text(event.name) }
+  end
+end

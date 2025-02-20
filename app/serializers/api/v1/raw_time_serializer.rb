0e1db6cd7f876317baa9b611ab8a3c5352045eb6
moveson <mark.oveson@gmail.com>
2025-02-20 15:13:58 -0700

    Expose event group raw times index and show to the API

--- app/serializers/api/v1/raw_time_serializer.rb
+++ app/serializers/api/v1/raw_time_serializer.rb
@@ -3,8 +3,23 @@ module Api
     class RawTimeSerializer < ::Api::V1::BaseSerializer
       set_type :raw_times
 
-      attributes :id, :source, :absolute_time, :entered_time, :bib_number, :lap, :split_name, :sub_split_kind,
-                 :data_status, :with_pacer, :remarks
+      attributes :id,
+                 :event_group_id,
+                 :source,
+                 :absolute_time,
+                 :entered_time,
+                 :bib_number,
+                 :lap,
+                 :split_name,
+                 :sub_split_kind,
+                 :data_status,
+                 :stopped_here,
+                 :with_pacer,
+                 :remarks
+
+      link :self, :api_v1_url
+
+      belongs_to :event_group
     end
   end
 end

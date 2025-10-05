7d74226b800710048b50ffaf36277373df4604c1
moveson <mark.oveson@gmail.com>
2025-10-04 18:16:48 -0600

    Fix puma deprecation warning

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ rackup      DefaultRackup if defined?(DefaultRackup)
 port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RACK_ENV", "development")
 
-on_worker_boot do
+before_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection

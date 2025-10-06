15f6b6c2f8f15f021689ca83cdee7104e7fc598b
moveson <mark.oveson@gmail.com>
2025-10-05 18:10:16 -0600

    Additional mapping fixes

--- config/initializers/timezone_patch.rb
+++ config/initializers/timezone_patch.rb
@@ -1,14 +1,18 @@
 # Rails maps timezone names to TZInfo names in ActiveSupport::TimeZone::MAPPING
 # This constant maps "Greenland" to "America/Godthab", even though that timezone
-# was renamed to "America/Nuuk" in 2020.
+# was renamed to "America/Nuuk" in 2020. The same is true of "Rangoon", which maps
+# to "Asia/Rangoon", even though this is now "Asia/Yangon".
 
 # Postgres recognizes TZInfo names, and newer versions of Postgres no longer
-# recognize "America/Godthab" as a valid timezone. As a result, we receive a
-# PG error if we attempt to set this timezone:
+# recognize the deprecated names as valid timezones. As a result, we receive a
+# PG error if we attempt to set timezone to a deprecated timezone, e.g.
 
 # set timezone='America/Godthab';
+# => PG::InvalidParameterValue
 
-# This file maps "Greenland" to the newer "America/Nuuk" TZInfo name,
+# This file maps certain time zone names to the newer TZInfo names,
 # which avoids these PG errors.
 
-ActiveSupport::TimeZone::MAPPING["Greenland"] = "America/Nuuk"
+ActiveSupport::TimeZone::MAPPING["Greenland"] = "America/Nuuk" # was "America/Godthab"
+ActiveSupport::TimeZone::MAPPING["Kyiv"]  = "Europe/Kyiv"   # was "Asia/Rangoon"
+ActiveSupport::TimeZone::MAPPING["Rangoon"]  = "Asia/Yangon"   # was "Asia/Rangoon"

eb0f356212dd615c5322a98d50876254dc33a312
Wilson <wilson.is.a.robot@gmail.com>
2026-03-05 18:52:51 -0700

    Fix PagyPresenter method name conflict with Pagy::Method
    Problem: Including Pagy::Method caused a conflict - all presenters have
    their own 'pagy' method that conflicts with Pagy::Method#pagy helper.
    
    This caused 'wrong number of arguments' errors in system specs because
    the local 'pagy' method was being called instead of the Pagy helper.
    
    Solution: Don't include Pagy::Method. Instead, call the paginators
    directly (Pagy::OffsetPaginator and Pagy::CountlessPaginator) and
    handle Pagy::Request wrapping ourselves.
    
    Changes:
    - Remove 'include Pagy::Method'
    - Require paginator modules explicitly
    - Call Pagy::OffsetPaginator.paginate directly
    - Call Pagy::CountlessPaginator.paginate directly
    - Wrap hash requests in Pagy::Request before passing to paginators
    - Merge Pagy::OPTIONS with our options to get proper defaults
    
    All 51 presenter specs pass locally.

--- app/presenters/concerns/pagy_presenter.rb
+++ app/presenters/concerns/pagy_presenter.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'pagy/classes/request'
+require 'pagy/toolbox/paginators/offset'
+require 'pagy/toolbox/paginators/countless'
+
 # Pagy pagination support for presenters
 # Include this module to add pagy pagination methods to presenters
 #
@@ -7,8 +11,6 @@
 # In production presenters, pass request from view_context.
 # In tests, pass a request hash with params.
 module PagyPresenter
-  include Pagy::Method
-
   # Paginate a scope using Pagy
   #
   # @param scope [ActiveRecord::Relation] The scope to paginate
@@ -20,7 +22,20 @@ module PagyPresenter
     count ||= scope.reorder(nil).count(:all)
     count = count.values.sum if count.is_a?(Hash)
 
-    pagy(:offset, scope, count: count, request: request, **options)
+    # Merge Pagy defaults with our options
+    pagy_options = Pagy::OPTIONS.merge(options)
+    
+    # Wrap request in Pagy::Request if it's a hash
+    pagy_options[:request] = if request.is_a?(Hash)
+                                Pagy::Request.new(pagy_options.merge(request: request))
+                              else
+                                request
+                              end
+    
+    # Add count to options
+    pagy_options[:count] = count
+    
+    Pagy::OffsetPaginator.paginate(scope, pagy_options)
   end
 
   # Paginate without running a COUNT query.
@@ -32,8 +47,20 @@ module PagyPresenter
   # @param options [Hash] Additional pagy options
   # @return [Array<Pagy::Countless, ActiveRecord::Relation>]
   def pagy_countless_from_scope(scope, request, **options)
-    # Pagy::Countless only supports :empty_page or :exception overflow options
+    # Merge Pagy defaults with our options
+    pagy_options = Pagy::OPTIONS.merge(options)
+    
+    # Wrap request in Pagy::Request if it's a hash
+    pagy_options[:request] = if request.is_a?(Hash)
+                                Pagy::Request.new(pagy_options.merge(request: request))
+                              else
+                                request
+                              end
+    
+    # Pagy::Offset::Countless only supports :empty_page or :exception overflow options
     # (not :last_page which is the global default)
-    pagy(:countless, scope, request: request, overflow: :empty_page, **options)
+    pagy_options[:overflow] = :empty_page
+    
+    Pagy::CountlessPaginator.paginate(scope, pagy_options)
   end
 end

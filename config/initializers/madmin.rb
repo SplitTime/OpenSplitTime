6a6bb6778c2d2fc506273323cdf494cb2d43ac29
Wilson <wilson.is.a.robot@gmail.com>
2026-03-06 09:02:06 -0700

    Update madmin initializer - remove redundant resource location
    The Madmin::Engine automatically adds resource locations in its
    before_configuration hook, so manually adding them in the initializer
    is redundant and was causing duplicates.

--- config/initializers/madmin.rb
+++ config/initializers/madmin.rb
@@ -1,6 +1,4 @@
 # Madmin Configuration
+# Resource locations are automatically added by the engine
 
 Madmin.site_name = "OpenSplitTime Admin"
-
-# Add app/madmin/resources to resource locations
-Madmin.resource_locations << Rails.root.join("app/madmin/resources")

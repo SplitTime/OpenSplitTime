8f7ff7f873e4ca380ce4dcf0cbd4d6d5a6f6ad0b
moveson <mark.oveson@gmail.com>
2026-03-08 01:07:58 -0700

    Revert sorted_analysis_rows method

--- app/view_models/effort_analysis_view.rb
+++ app/view_models/effort_analysis_view.rb
@@ -95,12 +95,7 @@ class EffortAnalysisView < EffortWithLapSplitRows
   end
 
   def sorted_analysis_rows
-    analysis_rows
-      .select do |row|
-        value = row.segment_over_under_percent
-        value.present? && !(value.respond_to?(:nan?) && value.nan?)
-      end
-      .sort_by(&:segment_over_under_percent)
+    analysis_rows.select(&:segment_over_under_percent).sort_by(&:segment_over_under_percent)
   end
 
   def course

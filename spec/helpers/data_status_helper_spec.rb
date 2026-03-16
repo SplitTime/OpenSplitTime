d426877a5ca8260ddef65921b235a046144639f0
Wilson <wilson.is.a.robot@gmail.com>
2026-03-16 16:16:19 -0600

    Refactor spec to use let blocks per style guide

--- spec/helpers/data_status_helper_spec.rb
+++ spec/helpers/data_status_helper_spec.rb
@@ -2,51 +2,59 @@ require "rails_helper"
 
 RSpec.describe DataStatusHelper do
   describe "#text_with_status_indicator" do
+    let(:result) { helper.text_with_status_indicator(text, status, options) }
+    let(:text) { "12:45:30 PM" }
+    let(:status) { nil }
+    let(:options) { {} }
+
     context "when status is nil" do
       it "returns the time without any indicator" do
-        result = helper.text_with_status_indicator("12:45:30 PM", nil)
         expect(result).to eq("12:45:30 PM")
       end
     end
 
     context "when status is good" do
+      let(:status) { :good }
+
       it "returns the time without any indicator" do
-        result = helper.text_with_status_indicator("12:45:30 PM", :good)
         expect(result).to eq("12:45:30 PM")
       end
     end
 
     context "when status is bad" do
+      let(:status) { :bad }
+
       it "returns an icon with the time wrapped in a nowrap span" do
-        result = helper.text_with_status_indicator("12:45:30 PM", :bad)
         expect(result).to have_css("span.text-nowrap")
         expect(result).to have_css("i.fa-circle-xmark.text-danger")
         expect(result).to have_css("span.fa6-text", text: "12:45:30 PM")
       end
 
       it "includes tooltip data attributes" do
-        result = helper.text_with_status_indicator("12:45:30 PM", :bad)
         expect(result).to have_css("i[data-controller='tooltip'][data-bs-original-title='Time Appears Bad']")
       end
     end
 
     context "when status is questionable" do
+      let(:status) { :questionable }
+
       it "returns an icon with the time wrapped in a nowrap span" do
-        result = helper.text_with_status_indicator("12:45:30 PM", :questionable)
         expect(result).to have_css("span.text-nowrap")
         expect(result).to have_css("i.fa-circle-question.text-warning")
         expect(result).to have_css("span.fa6-text", text: "12:45:30 PM")
       end
 
       it "includes tooltip data attributes" do
-        result = helper.text_with_status_indicator("12:45:30 PM", :questionable)
         expect(result).to have_css("i[data-controller='tooltip'][data-bs-original-title='Time Appears Questionable']")
       end
     end
 
     context "when data_type option is provided" do
+      let(:text) { "5.2" }
+      let(:status) { :bad }
+      let(:options) { { data_type: :pace } }
+
       it "uses the custom data type in the tooltip" do
-        result = helper.text_with_status_indicator("5.2", :bad, data_type: :pace)
         expect(result).to have_css("i[data-bs-original-title='Pace Appears Bad']")
       end
     end

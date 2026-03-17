4328f12b4d7d4285848220410b984a9f58861f3c
Wilson <wilson.is.a.robot@gmail.com>
2026-03-17 08:12:49 -0600

    Remove ArgsValidator: Services - Interactors Part 1
    Replace ArgsValidator with Ruby keyword arguments in 5 interactor services:
    - ChangeEffortEvent
    - ChangeEventCourse
    - DuplicateCourse
    - MatchRawTimesToSplitTimes
    - MatchTimeRecordsToSplitTimes
    
    Add nil validation for required parameters that was previously handled by ArgsValidator.
    Fix RuboCop offenses including Time.parse timezone issues.
    Update specs to test for missing keyword arguments rather than nil values.
    
    Part of #1639

--- app/services/interactors/match_raw_times_to_split_times.rb
+++ app/services/interactors/match_raw_times_to_split_times.rb
@@ -21,9 +21,9 @@ module Interactors
       if errors.present?
         Interactors::Response.new(errors, "Raw times could not be matched. ", {})
       else
-        args = { time_records: loaded_raw_times, split_times: split_times }
-        args[:tolerance] = tolerance if tolerance
-        Interactors::MatchTimeRecordsToSplitTimes.perform!(**args)
+        Interactors::MatchTimeRecordsToSplitTimes.perform!(time_records: loaded_raw_times, 
+                                                            split_times: split_times, 
+                                                            tolerance: tolerance)
       end
     end
 

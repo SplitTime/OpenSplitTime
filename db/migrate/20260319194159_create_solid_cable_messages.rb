2da0fc1b8217ab56a5faa839d4e4169afd8f29c4
moveson <mark.oveson@gmail.com>
2026-03-19 19:19:46 -0600

    Revert "Fix RuboCop offense: add underscores to large numeric literal"
    This reverts commit ff097af4bdf4828edd9981b9d40283bd1f7495f9.

--- db/migrate/20260319194159_create_solid_cable_messages.rb
+++ db/migrate/20260319194159_create_solid_cable_messages.rb
@@ -2,7 +2,7 @@ class CreateSolidCableMessages < ActiveRecord::Migration[8.1]
   def change
     create_table :solid_cable_messages do |t|
       t.binary :channel, null: false, limit: 1024
-      t.binary :payload, null: false, limit: 536_870_912
+      t.binary :payload, null: false, limit: 536870912
       t.datetime :created_at, null: false
       t.integer :channel_hash, null: false, limit: 8
 

957dfc9999698a6b1f887d1d3243f17c10bfad2f
moveson <mark.oveson@gmail.com>
2026-03-24 08:42:52 -0600

    Fix scout app names
    Staging and production both use "production" environments. We have to reference `CREDENTIALS_ENV` to distinguish between the two.

--- config/initializers/01_ost_config.rb
+++ config/initializers/01_ost_config.rb
@@ -97,6 +97,14 @@ module OstConfig
     3.days
   end
 
+  def self.scout_apm_app_name
+    if credentials_env == "production"
+      "OpenSplitTime"
+    elsif credentials_env == "staging"
+      "ost-staging"
+    end
+  end
+
   def self.scout_apm_key
     Rails.application.credentials.dig(:scout, :agent_key)
   end

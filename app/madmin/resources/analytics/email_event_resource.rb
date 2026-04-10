a0056fd7b7d491ab074fa2e27496520736a7b439
moveson <mark.oveson@gmail.com>
2026-04-10 08:07:44 -0400

    Move type to the right in the madmin index

--- app/madmin/resources/analytics/email_event_resource.rb
+++ app/madmin/resources/analytics/email_event_resource.rb
@@ -2,12 +2,12 @@ module Analytics
   class EmailEventResource < Madmin::Resource
     # Attributes
     attribute :id, form: false
-    attribute :type, index: true
     attribute :email, index: true
     attribute :event, index: true
     attribute :timestamp, index: true
     attribute :status, index: true
     attribute :reason, index: true
+    attribute :type, index: true
     attribute :response, index: false
     attribute :ip, index: false
     attribute :useragent, index: false

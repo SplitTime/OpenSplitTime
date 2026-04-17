4cca45067e3c069182b23e9a6e3fbdc531d72382
Wilson <wilson.is.a.robot@gmail.com>
2026-04-17 08:18:05 -0600

    Fix first entrant turbo update on setup page

--- spec/system/event_group_construction/create_entrants_spec.rb
+++ spec/system/event_group_construction/create_entrants_spec.rb
@@ -1,16 +1,15 @@
 require "rails_helper"
 
-RSpec.describe "create an entrant from the event group entrants view", js: true do
+RSpec.describe "create an entrant from the event group entrants view", :js do
   include ActionView::RecordIdentifier
   include ActiveJob::TestHelper
 
   let(:steward) { users(:fifth_user) }
-
-  before { organization.stewards << steward }
-
   let(:event_group) { event_groups(:rufa_2016) }
   let(:organization) { event_group.organization }
   let(:entrant) { event_group.efforts.find_by(first_name: "Finished", last_name: "First") }
+
+  before { organization.stewards << steward }
 
   scenario "Create an entrant" do
     login_as steward, scope: :user
@@ -30,6 +29,31 @@ RSpec.describe "create an entrant from the event group entrants view", js: true 
     end.to change { event_group.efforts.count }.by(1)
 
     expect(page).to have_content(entrant.reload.full_name)
+  end
+
+  scenario "Create the first entrant" do
+    login_as steward, scope: :user
+    event_group.events.flat_map(&:efforts).each(&:destroy)
+    visit_page
+
+    expect(page).to have_content("Add or Import Your Entrants")
+    expect(page).not_to have_css("tbody#entrants")
+
+    click_link "Add"
+    expect(page).to have_content("Add an Entrant")
+
+    fill_in "effort_first_name", with: "Barney"
+    fill_in "effort_last_name", with: "Rubble"
+    select "Male", from: "effort_gender"
+
+    expect do
+      click_button "Create Entrant"
+      expect(page).to have_css("tbody#entrants")
+      expect(page).to have_css(".bg-highlight")
+    end.to change { event_group.reload.efforts.count }.by(1)
+
+    expect(page).to have_content("Barney Rubble")
+    expect(page).not_to have_content("Add or Import Your Entrants")
   end
 
   def visit_page
